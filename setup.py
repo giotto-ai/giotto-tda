f52488e84c50a648b1ad80210d2a23df63ee9e79
Guillaume Tauzin <g.tauzin@l2f.ch>
2019-06-05 12:39:06 +0200

    [ISSUE 1] Restructed examples directory

--- setup.py
+++ setup.py
@@ -52,8 +52,7 @@ setup(name='topological_learning',
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
+        # Specify the Python versions you support here.
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
