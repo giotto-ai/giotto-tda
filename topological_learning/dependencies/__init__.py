a0a43eb6a1d94f25f34ab47f09e8b86392819253
Guillaume Tauzin <g.tauzin@l2f.ch>
2019-06-17 16:48:12 +0200

    Solved exception handling issue in dependencies import

--- topological_learning/dependencies/__init__.py
+++ topological_learning/dependencies/__init__.py
@@ -3,16 +3,14 @@ pyximport.install()
 from distutils.errors import CompileError
 
 try:
-    #from .gudhi_bottleneck import bottleneck_distance
-    raise ImportError()
-except ImportError:
+    from .gudhi_bottleneck import bottleneck_distance
+except:
     from gudhi import bottleneck_distance
     print("Using original gudhi bottleneck_distance.")
 
 try:
     from .hera_wasserstein import wasserstein
-    raise CompileError()
-except CompileError:
+except:
     def wasserstein(diagram_1, diagram_2, p = 1, delta = 0.01):
         pass
     print("Function wasserstein_distance not available.")
