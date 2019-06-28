59875e37dbfe0242798883f923038104229b7df6
Guillaume Tauzin <guillaumetauzin@l2fs-mbp.lan>
2019-06-28 18:18:17 +0200

    Added all modules to doc

--- doc/conf.py
+++ doc/conf.py
@@ -10,14 +10,13 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'topological_learning'
+project = 'Topological Learning'
 copyright = '2019, Guillaume Tauzin'
 author = 'Guillaume Tauzin'
 
@@ -31,6 +30,16 @@ release = '0.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'numpydoc',
+    # 'sphinx.ext.linkcode', # need to be setup, not sure what it does
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.imgconverter',
+    'sphinx_gallery.gen_gallery',
+    'sphinx_issues',
+    # 'custom_references_resolver' # custom for sklearn, not sure what it does
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,7 +56,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- doc/index.rst
+++ doc/index.rst
@@ -3,14 +3,20 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to topological_learning's documentation!
+Welcome to Topological Learning's documentation!
 ================================================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
-
+   modules/preprocessing
+   modules/compose
+   modules/homology
+   modules/diagram
+   modules/manifold
+   modules/neural_network
+   modules/model_Selection
 
 Indices and tables
 ==================
