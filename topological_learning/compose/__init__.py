33e7184b9c0fab01bdd0f4eb46d61ed87b230af4
Guillaume Tauzin <g.tauzin@l2f.ch>
2019-07-04 11:33:51 +0200

    Improved API ref

--- topological_learning/compose/__init__.py
+++ topological_learning/compose/__init__.py
@@ -1,7 +1,5 @@
 """The :mod:`topological_learning.compose` module is an extension of sk-learn's
-and implements meta-estimators for building composite models with transformers
-In addition to its current contents, this module will eventually be home to
-refurbished versions of Pipeline and FeatureUnion.
+and implements meta-estimators for building composite models with transformers.
 """
 
 from .target import TargetResamplingClassifier, TargetResamplingRegressor, TargetResampler
