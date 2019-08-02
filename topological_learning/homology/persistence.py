96c34e3c0289babe152e646ee3418b7d98054f0f
Guillaume Tauzin <g.tauzin@l2f.ch>
2019-08-02 17:11:11 +0200

    Implemented CubicalPersistence

--- topological_learning/homology/persistence.py
+++ topological_learning/homology/persistence.py
@@ -87,7 +87,7 @@ class VietorisRipsPersistence(BaseEstimator, TransformerMixin):
         implemented_metric_types = set(['precomputed'] + [met for i in VALID_METRICS.values() for met in i])
 
         if metric not in implemented_metric_types:
-            return ValueError('The metric you specified is not implemented.')
+            raise ValueError('The metric you specified is not implemented.')
 
     def _ripser_diagram(self, X, is_distance_matrix, metric):
         diagram = ripser(X, distance_matrix=is_distance_matrix, metric=metric, maxdim=max(self.homology_dimensions), thresh=self.max_edge_length)['dgms']
@@ -175,6 +175,164 @@ class VietorisRipsPersistence(BaseEstimator, TransformerMixin):
         is_distance_matrix = (self.metric == 'precomputed')
 
         X_transformed = Parallel(n_jobs=self.n_jobs) ( delayed(self._ripser_diagram)(X[i, :, :], is_distance_matrix, self.metric)
+                                                        for i in range(X.shape[0]) )
+
+        if self.pad:
+            max_length_list = [ max(1, np.max([ X_transformed[i][dimension].shape[0] for i in range(len(X_transformed)) ]))
+                                for dimension in self.homology_dimensions ]
+            X_transformed = Parallel(n_jobs = self.n_jobs) ( delayed(self._pad_diagram)(X_transformed[i], max_length_list)
+                                                            for i in range(len(X_transformed)) )
+            X_transformed = self._stack_padded_diagrams(X_transformed)
+        else:
+            X_transformed = { dimension: [diag[dimension] for diag in X_transformed] for dimension in self.homology_dimensions }
+
+        return X_transformed
+
+
+class CubicalPersistence(BaseEstimator, TransformerMixin):
+    """Transformer for the calculation of persistence diagrams resulting from images.
+    Given an image, information about the appearance and disappearance
+    of "topological holes" (technically, homology classes) of various dimensions and
+    at different distance scales is summarised in the corresponding persistence diagram.
+
+    Parameters
+    ----------
+    max_edge_length : float, optional, default: np.inf
+        Upper bound on the maximum value of the Vietoris-Rips filtration parameter.
+        Points whose distance is greater than this value will never be connected by an
+        edge, and topological features at scales larger than this value will not be
+        detected.
+
+    homology_dimensions : list, optional, default: [0, 1]
+        List of dimensions (non-negative integers). Topological holes of each of
+        these dimensions will be detected.
+
+    pad : boolean, optional, default: True
+        Whether or not some of the resulting persistence diagrams
+        should be padded with zeros so that the final collection of persistence
+        diagrams can be cast as a single numpy array.
+
+    n_jobs : int or None, optional, default: None
+        The number of jobs to use for the computation. ``None`` means 1 unless in
+        a :obj:`joblib.parallel_backend` context. ``-1`` means using all processors.
+
+    """
+
+    def __init__(self, max_edge_length=np.inf, homology_dimensions=[0, 1], pad=True, n_jobs=None):
+        self.max_edge_length = max_edge_length
+        self.homology_dimensions = homology_dimensions
+        self.pad = pad
+        self.n_jobs = n_jobs
+
+    def get_params(self, deep=True):
+        """Get parameters for this estimator.
+
+        Parameters
+        ----------
+        deep : boolean, optional, default: True
+            Behaviour not yet implemented.
+
+        Returns
+        -------
+        params : mapping of string to any
+            Parameter names mapped to their values.
+        """
+        return {'max_edge_length': self.max_edge_length,
+                'homology_dimensions': self.homology_dimensions,
+                'pad': self.pad,
+                'n_jobs': self.n_jobs}
+
+    @staticmethod
+    def _validate_params(homology_dimensions, n_dimensions):
+        """A class method that checks whether the hyperparameters and the input parameters
+        of the :meth:'fit' are valid.
+        """
+        try:
+            assert set(homology_dimensions).issubset(set(range(n_dimensions)))
+        except:
+            raise ValueError('The homology_dimensions specified contains element(s) that are not within the range 0 to the dimension of the images.')
+
+    def _gudhi_diagram(self, X, is_distance_matrix, metric):
+        diagram = gd.CubicalComplex(dimensions=X.shape, top_dimensional_cells=X.flatten(order="F"))
+
+        return { dimension : np.array([diagram[i][1] for i in range(len(diagram)) if diagram[i][0] == dimension ])
+                            for dimension in self.homology_dimensions }
+
+    def _pad_diagram(self, diagram, max_length_list):
+        padList = [ ((0, max(0, max_length_list[i] - diagram[dimension].shape[0])), (0,0)) for i, dimension in enumerate(self.homology_dimensions) ]
+        return { dimension: np.pad(diagram[dimension], padList[i], 'constant') for i, dimension in enumerate(self.homology_dimensions) }
+
+    def _stack_padded_diagrams(self, diagrams):
+        stacked_diagrams = { dimension: np.stack([diagrams[i][dimension] for i in range(len(diagrams))], axis=0) for dimension in self.homology_dimensions }
+
+        # for dimension in self.homology_dimensions:
+        #     if stackedDiagrams[dimension].size == 0:
+        #         del stackedDiagrams[dimension]
+        return stacked_diagrams
+
+    def fit(self, X, y=None):
+        """Do nothing and return the estimator unchanged.
+        This method is just there to implement the usual API and hence
+        work in pipelines.
+
+        Parameters
+        ----------
+        X : ndarray, shape (n_samples, n_points, n_points) or (n_samples, n_points, n_features)
+            Input data. If ``metric == 'precomputed'``, the input should be an ndarray
+            whose each entry along axis 0 is a distance matrix of shape
+            (n_points, n_points). Otherwise, each such entry will be interpreted as
+            an ndarray of n_points in Euclidean space of dimension n_features.
+
+        y : None
+            There is no need of a target in a transformer, yet the pipeline API
+            requires this parameter.
+
+        Returns
+        -------
+        self : object
+            Returns self.
+
+        """
+
+        self._validate_params(self.homology_dimensions, len(X.shape)-1)
+
+        self._is_fitted = True
+        return self
+
+    def transform(self, X, y=None):
+        """Computes, for each dimension in ``homology_dimensions`` and for each
+        image in X, the relevant persistence diagram as
+        an array of pairs (b, d) -- one per persistent topological hole -- where
+        b is the scale at which the topological hole first appears, and d the scale
+        at which the same hole disappears.
+
+        Parameters
+        ----------
+        X : ndarray, shape (n_samples, n_pixels_x, n_pixels_y) or (n_samples, n_pixels_x, n_pixels_y, n_pixels_z)
+            Input data. Array of 2D or 3D images.
+
+        y : None
+            There is no need of a target in a transformer, yet the pipeline API
+            requires this parameter.
+
+        Returns
+        -------
+        X_transformed : dict of int: ndarray or dict of int: list
+            Dictionary whose keys are the integers in ``self.homology_dimensions``,
+            and whose values are ndarrays if ``pad == True``, and lists otherwise.
+            In the former case, the ndarrays have shape (n_samples, M_d, 2) where,
+            if m_{d,i} is the number of persistent topological features in the relevant
+            dimension d found in sample i, then M_d = max {m_{d,i}: i = 1, ..., n_samples}.
+            If ``pad == False``, then each list has length n_samples and its i-th entry
+            is an ndarrays of shape (m_{d,i}, 2).
+
+        """
+        # Check is fit had been called
+        check_is_fitted(self, ['_is_fitted'])
+
+        is_distance_matrix = (self.metric == 'precomputed')
+
+        X_transformed = Parallel(n_jobs=self.n_jobs) ( delayed(self._gudhi_diagram)(X[i, :, :], is_distance_matrix, self.metric)
                                                         for i in range(X.shape[0]) )
 
         if self.pad:
