4ca760c8afee10bdce32f0369d460b60ea2b855b
philippenguyen <p.nguyen@l2f.ch>
2019-08-29 10:57:39 +0200

    remove warning filter

--- giotto/graphs/kneighbors.py
+++ giotto/graphs/kneighbors.py
@@ -128,9 +128,7 @@ class KNeighborsGraph(BaseEstimator, TransformerMixin):
     def _make_adjacency_matrix(self, X):
         A = self._nearest_neighbors(X)
         rows, cols = A.nonzero()
-        with warnings.catch_warnings():
-            warnings.simplefilter('ignore', SparseEfficiencyWarning)
-            A[cols, rows] = 1
+        A[cols, rows] = 1
         return A
 
     def fit(self, X, y=None):
