82c014a077f6d4f9d781b9c797696bb701782ee4
philippenguyen <p.nguyen@l2f.ch>
2019-09-02 14:09:06 +0200

    group resampler and stationarizer tests

--- giotto/time_series/tests/test_preprocessing.py
+++ giotto/time_series/tests/test_preprocessing.py
@@ -1,4 +1,4 @@
-"""Testing for Resampler"""
+"""Testing for Resampler and Stationarizer"""
 
 import pytest
 
@@ -9,10 +9,11 @@ import datetime as dt
 from sklearn.exceptions import NotFittedError
 from sklearn.utils.testing import assert_raise_message
 
-from giotto.time_series import Resampler
+from giotto.time_series import Resampler, Stationarizer
 
-signal = pd.DataFrame(np.asarray([np.sin(x / 2) + 2 for x in range(0, 20)]))
-signal.index = pd.to_datetime(signal.index, utc=True, unit='d')
+signal_array = np.asarray([np.sin(x / 2) + 2 for x in range(0, 20)])
+signal_df = pd.DataFrame(signal_array)
+signal_df.index = pd.to_datetime(signal_df.index, utc=True, unit='d')
 
 signal_resampled = np.array(
     [[2.],
@@ -75,7 +76,7 @@ def test_resampler_not_fitted(resampler):
     msg = ("This %s instance is not fitted yet. Call \'fit\'"
            " with appropriate arguments before using this method.")
     assert_raise_message(NotFittedError, msg % 'Resampler',
-                         resampler.transform, signal)
+                         resampler.transform, signal_df)
 
 
 def test_resampler_not_valid():
@@ -86,7 +87,7 @@ def test_resampler_not_valid():
                           remove_weekends=False)
     msg = "The sampling type %s is not supported"
     assert_raise_message(ValueError, msg % sampling_type,
-                         resampler.fit, signal)
+                         resampler.fit, signal_df)
 
 
 @pytest.mark.parametrize("remove_weekends, expected",
@@ -95,6 +96,86 @@ def test_resampler_not_valid():
 def test_resampler_transform(fixed_resampler, remove_weekends, expected):
     resampler = Resampler(sampling_type='periodic', sampling_period='2d',
                           sampling_times=None, remove_weekends=remove_weekends)
-    assert resampler.fit_transform(signal).all() == expected.all()
+    assert resampler.fit_transform(signal_df).all() == expected.all()
     assert fixed_resampler.fit_transform(
-        signal).all() == signal_resampled.all()
+        signal_df).all() == signal_resampled.all()
+
+
+signal = signal_array.reshape(-1, 1)
+
+signal_stationarized_return = np.array(
+    [[0.19336154],
+     [0.1274148],
+     [0.05205146],
+     [-0.03031576],
+     [-0.11961848],
+     [-0.21360416],
+     [-0.29826475],
+     [-0.32659273],
+     [-0.21587689],
+     [0.01787175],
+     [0.19574495],
+     [0.24766283],
+     [0.22325449],
+     [0.16630367],
+     [0.09564785],
+     [0.01718037],
+     [-0.06820511],
+     [-0.16017813],
+     [-0.25314694]])
+
+signal_stationarized_log_return = np.array(
+    [[0.21487972],
+     [0.13629497],
+     [0.05345506],
+     [-0.02986532],
+     [-0.11298798],
+     [-0.19359458],
+     [-0.26102857],
+     [-0.2826138],
+     [-0.19546554],
+     [0.01803338],
+     [0.21783884],
+     [0.28457069],
+     [0.25264251],
+     [0.18188605],
+     [0.10053645],
+     [0.01732966],
+     [-0.06597978],
+     [-0.14857355],
+     [-0.22565794]])
+
+
+@pytest.fixture
+def stationarizer():
+    return Stationarizer(stationarization_type='return')
+
+
+def test_stationarizer_init():
+    stationarization_type = 'return'
+    stationarizer = Stationarizer(stationarization_type=stationarization_type)
+    assert stationarizer.get_params()[
+               'stationarization_type'] == stationarization_type
+
+
+def test_stationarizer_not_fitted(stationarizer):
+    msg = ("This %s instance is not fitted yet. Call \'fit\'"
+           " with appropriate arguments before using this method.")
+    assert_raise_message(NotFittedError, msg % 'Stationarizer',
+                         stationarizer.transform, signal)
+
+
+def test_stationarizer_not_valid():
+    stationarization_type = 'not_defined'
+    stationarizer = Stationarizer(stationarization_type=stationarization_type)
+    msg = "The transformation type %s is not supported"
+    assert_raise_message(ValueError, msg % stationarization_type,
+                         stationarizer.fit, signal)
+
+
+@pytest.mark.parametrize("stationarization_type, expected",
+                         [('return', signal_stationarized_return),
+                          ('log-return', signal_stationarized_log_return)])
+def test_stationarizer_transform(stationarization_type, expected):
+    stationarizer = Stationarizer(stationarization_type=stationarization_type)
+    assert stationarizer.fit_transform(signal).all() == expected.all()
