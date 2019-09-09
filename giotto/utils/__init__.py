0d24cab5d658a6e998cc8cfcc144eff8943c09c4
Guillaume Tauzin <g.tauzin@l2f.ch>
2019-09-09 12:43:54 +0200

    [24] Added empty check_diagram funtion in utils/validation.py

---
+++ giotto/utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+The :mod:`giotto.utils` module includes various utilities.
+"""
+
+from .validation import check_diagram
+
+__all__ = [
+    'check_diagram'
+]
---
+++ giotto/utils/validation.py
@@ -0,0 +1,6 @@
+"""Utilities for input validation"""
+
+def check_diagram(diagram):
+    """Input validation on a diagram
+    """
+    pass
