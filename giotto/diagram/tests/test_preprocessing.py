1d9487a9c5acf22061f770a6f7b88ac3f85ab06b
philippenguyen <p.nguyen@l2f.ch>
2019-10-09 09:27:08 +0200

    [7] Remove test permutation, attempt to fix tests

--- giotto/diagram/tests/test_preprocessing.py
+++ giotto/diagram/tests/test_preprocessing.py
@@ -7,12 +7,12 @@ from sklearn.exceptions import NotFittedError
 
 from giotto.diagram import DiagramStacker, DiagramScaler
 
-X_1 = np.array([[[0., 0.36905774],
-                 [0., 0.37293977],
-                 [0., 0.38995215],
-                 [0., 3.00457644],
-                 [0., 3.04772496],
-                 [0., 3.32096624],
+X_1 = np.array([[[0., 0.36905774, 0],
+                 [0., 0.37293977, 0],
+                 [0., 0.38995215, 0],
+                 [0., 3.00457644, 0],
+                 [0., 3.04772496, 0],
+                 [0., 3.32096624, 0],
                  [7.97852135, 8.00382805, 1],
                  [1.79289687, 1.8224113, 1],
                  [1.69005811, 2.32093406, 1],
@@ -25,12 +25,12 @@ X_1 = np.array([[[0., 0.36905774],
                  [0., 0., 2],
                  [0., 0., 2],
                  [0., 0., 2]],
-                [[0., 0.36905774],
-                 [0., 0.37293977],
-                 [0., 0.38995215],
-                 [0., 3.00457644],
-                 [0., 3.04772496],
-                 [0., 3.32096624],
+                [[0., 0.36905774, 0],
+                 [0., 0.37293977, 0],
+                 [0., 0.38995215, 0],
+                 [0., 3.00457644, 0],
+                 [0., 3.04772496, 0],
+                 [0., 3.32096624, 0],
                  [15.27686119, 24.32133484, 1],
                  [1.79289687, 1.8224113, 1],
                  [1.69005811, 2.32093406, 1],
@@ -43,12 +43,12 @@ X_1 = np.array([[[0., 0.36905774],
                  [0., 0., 2],
                  [0., 0., 2],
                  [0., 0., 2]],
-                [[0., 1.32215321],
-                 [0., 1.48069561],
-                 [0., 1.62762213],
-                 [0., 3.18582344],
-                 [0., 3.25349188],
-                 [0., 3.28288555],
+                [[0., 1.32215321, 0],
+                 [0., 1.48069561, 0],
+                 [0., 1.62762213, 0],
+                 [0., 3.18582344, 0],
+                 [0., 3.25349188, 0],
+                 [0., 3.28288555, 0],
                  [3.32096624, 23.92891693, 1],
                  [2.93474603, 3.07139683, 1],
                  [2.83503842, 2.94497037, 1],
@@ -61,10 +61,10 @@ X_1 = np.array([[[0., 0.36905774],
                  [0., 0., 2],
                  [0., 0., 2],
                  [0., 0., 2]],
-                [[0., 0.13842253],
-                 [0., 0.13912651],
-                 [0., 0.15125643],
-                 [0., 3.30339432],
+                [[0., 0.13842253, 0],
+                 [0., 0.13912651, 0],
+                 [0., 0.15125643, 0],
+                 [0., 3.30339432, 0],
                  [0., 3.3078723, 0],
                  [0., 3.33029914, 0],
                  [16.82829285, 16.84351158, 1],
@@ -226,12 +226,11 @@ def test_not_fitted():
 def test_dst_transform(X):
     dst = DiagramStacker()
     X_res = dst.fit_transform(X)
-    assert X_res[None].shape == (X[0].shape[0],
-                                 len(X.keys()) * X[0].shape[1], 2)
+    assert X_res.shape == X.shape
 
 
 parameters = [('wasserstein', {'order': 2, 'delta': 0.1}),
-              ('betti', {'n_sampled_values': 10}),
+              ('betti', {'n_values': 10}),
               ('bottleneck', None)]
 
 
@@ -240,10 +239,8 @@ parameters = [('wasserstein', {'order': 2, 'delta': 0.1}),
 def test_dd_transform(X, metric, metric_params):
     dsc = DiagramScaler(metric=metric, metric_params=metric_params, n_jobs=1)
     X_res = dsc.fit_transform(X)
-    for i in range(len(X_1.keys())):
-        assert X_res[i].shape == X[i].shape
+    assert X_res.shape == X.shape
 
     dsc = DiagramScaler(metric=metric, metric_params=metric_params, n_jobs=1)
     X_inv_res = dsc.fit(X_res).inverse_transform(X_res)
-    for i in range(len(X_inv_res.keys())):
-        assert X_inv_res[i].shape == X[i].shape
+    assert X_inv_res.shape == X.shape
