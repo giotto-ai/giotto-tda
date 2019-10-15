c9514b78c6ac12797e6dfaedaab9817068551940
matteocao <38946719+matteocao@users.noreply.github.com>
2019-10-15 23:00:04 +0200

    Update docker_scripts.sh

--- azure-ci/docker_scripts.sh
+++ azure-ci/docker_scripts.sh
@@ -4,7 +4,7 @@ set -e -x
 
 echo 'folders: '
 ls
-/opt/python/*$(python.version)*/bin/pip install --upgrade pip setuptools
+/opt/python/*$(python.ver)*/bin/pip install --upgrade pip setuptools
 
 pip install -e /io/.['tests', 'doc']
 pip uninstall -y giotto-learn
