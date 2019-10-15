3762a3dc79fceaec739a9d3ad750656d31700052
matteocao <38946719+matteocao@users.noreply.github.com>
2019-10-15 22:25:27 +0200

    Update build_manylinux2010.sh

--- .ci/build_manylinux2010.sh
+++ .ci/build_manylinux2010.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+docker run -d -i -t --name manylinux10 -v `pwd`:/io quay.io/pypa/manylinux2010_x86_64 /bin/bash
+echo 'running dockers:'
+docker ps
+docker exec manylinux10 ls
+docker exec manylinux10 pwd
+docker exec manylinux10 sh -c "ls /io"
+docker exec manylinux10 sh -c "python --version"
+docker exec manylinux10 python --version
+docker exec manylinux10 sh -c "python3 --version"
+docker exec manylinux10 sh -c "sudo sh -c /io/.ci/build_manylinux2010.sh"
+docker exec manylinux10 sh -c "sudo sh /io/.ci/build_manylinux2010.sh"
+docker exec manylinux10 sh -c "sudo bash /io/.ci/build_manylinux2010.sh"
+
+
 set -e -x
 
 echo 'folders: '
