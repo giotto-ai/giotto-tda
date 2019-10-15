92ac0ef4504d97312dac9f11706a01551fd10c8d
matteocao <38946719+matteocao@users.noreply.github.com>
2019-10-15 22:46:54 +0200

    Update build_manylinux2010.sh

--- azure-ci/build_manylinux2010.sh
+++ azure-ci/build_manylinux2010.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+echo 'current directory: '
+pwd
+ls
 docker run -d -i -t --name manylinux10 -v `pwd`:/io quay.io/pypa/manylinux2010_x86_64 /bin/bash
 echo 'running dockers:'
 docker ps
@@ -9,7 +11,7 @@ docker exec manylinux10 sh -c "ls /io"
 docker exec manylinux10 sh -c "python --version"
 docker exec manylinux10 python --version
 docker exec manylinux10 sh -c "python3 --version"
-docker exec manylinux10 sh -c "sh -c /io/.ci/build_manylinux2010.sh"
-docker exec manylinux10 sh -c "sh /io/.ci/build_manylinux2010.sh"
-docker exec manylinux10 sh -c "/io/.ci/build_manylinux2010.sh"
-docker exec manylinux10 sudo sh -c "/io/.ci/build_manylinux2010.sh"
+docker exec manylinux10 sh -c "sh -c /io/azure-ci/docker_scripts.sh"
+docker exec manylinux10 sh -c "sh /io/azure-ci/docker_scripts.sh"
+docker exec manylinux10 sh -c "/io/azure-ci/docker_scripts.sh"
+docker exec manylinux10 sudo sh -c "/io/azure-ci/docker_scripts.sh"
