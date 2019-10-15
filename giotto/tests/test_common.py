749876e15076a2df23638dc4c19946b2a1209c7b
Guillaume Tauzin <g.tauzin@l2f.ch>
2019-10-15 10:19:52 +0200

    Delete test_common.py

--- giotto/tests/test_common.py
+++
@@ -1,7 +0,0 @@
-from sklearn.utils.estimator_checks import parametrize_with_checks
-from giotto.graphs import GraphGeodesicDistance
-
-
-@parametrize_with_checks([GraphGeodesicDistance])
-def test_sklearn_compatible_estimator(estimator, check):
-    check(estimator)
