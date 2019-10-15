8e16b487405385e0851eb66ac9194a258f6ee919
giotto-learn <55085396+giotto-learn@users.noreply.github.com>
2019-10-15 10:00:10 +0200

    Updated init file for new release

--- giotto/__init__.py
+++ giotto/__init__.py
@@ -1,3 +1,2 @@
-__all__ = ['compose', 'homology', 'model_selection', 'time_series',
-           'graphs', 'images', 'datasets', 'diagram', 'manifold',
-           'neural_network', 'meta_transformers']
+__all__ = ['homology', 'time_series', 'graphs', 
+           'diagrams', 'externals']
