3d96abd3f155b4f716f0e43c29fee0ce68179cfb
giotto-learn <55085396+giotto-learn@users.noreply.github.com>
2019-10-15 10:00:47 +0200

    Remove cubicals from init files

--- giotto/homology/__init__.py
+++ giotto/homology/__init__.py
@@ -3,11 +3,9 @@ to generate features from persistent homology.
 """
 
 from .consistent import ConsistentRescaling
-from .grids import CubicalPersistence
 from .point_clouds import VietorisRipsPersistence
 
 __all__ = [
     'VietorisRipsPersistence',
-    'CubicalPersistence',
     'ConsistentRescaling',
 ]
