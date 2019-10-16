b96187a2d8027aa1fefb973a88f035b96b0a65df
Guillaume Tauzin <g.tauzin@l2f.ch>
2019-10-16 21:26:26 +0200

    Fixed manylinux wheels script (#2)

--- azure-ci/docker_scripts.sh
+++ azure-ci/docker_scripts.sh
@@ -2,45 +2,45 @@
 
 set -e -x
 
-echo 'files and folders: '
-ls
-# update-alternatives --set python /opt/python/cp37-cp37m/
-/opt/python/*${python_ver}*/bin/pip install --upgrade pip setuptools
-#for PYBIN in /opt/python/*/bin; do "${PYBIN}/pip" install cmake; done
-/opt/python/*${python_ver}*/bin/pip install cmake
-ls /opt/python/*${python_ver}*/bin/
-CMAKE_BIN=/opt/python/*${python_ver}*/bin/cmake
-ln -sf ${CMAKE_BIN} /usr/bin/cmake
+# upgrading pip and setuptools
+PYTHON_PATH=$(eval find "/opt/python/*${python_ver}*" -print)
+export PATH=${PYTHON_PATH}/bin:${PATH}
+pip install --upgrade pip setuptools
 
-# install boost
+# installing cmake
+pip install cmake
 
-#yum list available
+# installing boost
 yum install -y wget tar
+wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz
+tar -zxvf /boost_1_69_0.tar.gz
+mkdir boost
+cd /boost_1_69_0
+./bootstrap.sh --prefix=/boost
+./b2 install
+cd ..
 
-pip install conda
-conda install boost
+# helping cmake find boost
+export BOOST_ROOT=/boost_1_69_0
+export Boost_INCLUDE_DIR=/boost_1_69_0/include
 
-#wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz
-#echo 'finish downloading boost.'
-#tar -zxvf /boost_1_69_0.tar.gz
-#cd /boost_1_69_0
-#./bootstrap.sh --prefix=/usr/local
-#./b2
-#ls /usr/local
-#ls /usr/local/lib
-#-sBOOST_ROOT=/usr/local/lib
+# installing pybind11
+git clone https://github.com/pybind/pybind11.git
+cd pybind11
+mkdir build
+cd build
+cmake ..
+make install
 
-#yum install -y boost148.x86_64
-#ls /usr/lib64/
-#echo 'other command'
-#-sBoost_INCLUDE_DIR=/usr/local/lib
+# installing and uninstalling giotto-learn
+cd /io
+pip install -e ".[tests]"
+pip uninstall -y giotto-learn
 
-/opt/python/cp37-cp37m/bin/pip install -e "/io/.[tests, doc]"
-/opt/python/cp37-cp37m/bin/pip uninstall -y giotto-learn
-/opt/python/cp37-cp37m/bin/pip install wheel twine
-
-pytest --cov /io/giotto/ --cov-report xml
+# testing, linting
+pytest --cov . --cov-report xml
 flake8 --exit-zero /io/
-displayName: 'Test with pytest and flake8'
 
-python /io/setup.py sdist bdist_wheel
+# building wheels
+pip install wheel twine
+python setup.py sdist bdist_wheel
