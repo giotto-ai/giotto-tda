giotto-ai/giotto-tda

commit e4c068640141118c8460d091766dd55ea95cd1f3

[#23] Fix and generalise TransitionGraph

giotto/graphs/transition.py
@@ -10,53 +10,116 @@ from joblib import Parallel, delayed
 from sklearn.utils.validation import check_array, check_is_fitted
 
 
+def _identity(X):
+    """The identity function.
+    """
+    return X
+
+
 class TransitionGraph(BaseEstimator, TransformerMixin):
-    """Given a collection of two-dimensional arrays, with row :math:`i` in
-    array :math:`A` encoding the "state" of a system at "time" :math:`i`,
-    this transformer returns a corresponding collection of so-called
-    *transition graphs*. The vertex set of graph :math:`G` corresponding to
-    :math:`A` is the set of all unique rows (states) in :math:`A`, and there
-    is an edge between two vertices if and only if one of the two rows
-    immediately follows the other anywhere in :math:`A`.
+    """Undirected transition graphs from arrays of time-evolving states.
+
+    Let A be a two-dimensional array viewed as a time series (along the row
+    axis) of one-dimensional arrays encoding the "state" of a system. The
+    corresponding *undirected transition graph* (or *network*) has as vertex
+    set the set of all unique states (rows) in A, and there is an edge
+    between two vertices if and only if one of the corresponding states
+    immediately follows the other in A.
+
+    Given a collection of two-dimensional arrays, this transformer performs
+    two tasks:
+
+        1. Optionally, it preprocesses the arrays by applying a function row
+           by row to them. This can be used e.g. as a "compression" step
+           to reduce the size of the state space.
+        2. It computes the undirected transition graph of each array as a
+           sparse matrix.
 
     Parameters
     ----------
+    func : None or callable, optional, default: ``numpy.argsort``
+        If a callable, it is the function to be applied to each row of each
+        array as a preprocessing steps. Allowed callables are functions
+        mapping 1-D arrays to 1-D arrays of constant length, and must be
+        compatible with :mod:`numpy.apply_along_axis`. If ``None``, this
+        function is the identity (no preprocessing). The default is
+        ``numpy.argsort``, which makes the final transition graphs
+        *ordinal partition networks* [1]_, [2]_, [3]_.
+
+    func_params : None or dict, optional, default: ``None``
+        Additional keyword arguments for `func`.
+
     n_jobs : int or None, optional, default: ``None``
         The number of jobs to use for the computation. ``None`` means 1 unless
         in a :obj:`joblib.parallel_backend` context. ``-1`` means using all
         processors.
 
+    Attributes
+    ----------
+    effective_func_params_ : dict
+        A copy of `func_params` if this was not set to ``None``, otherwise an
+        empty dictionary.
+
     Examples
     --------
     >>> import numpy as np
     >>> from giotto.graphs import TransitionGraph
-    >>> X = np.array([[['a'], ['b'], ['c']],
-    ...               [['c'], ['a'], ['b']]])
+    >>> X = np.array([[[1, 0], [2, 3], [5, 4]],
+    ...               [[5, 4], [5, 4], [5, 4]])
     >>> tg = TransitionGraph()
     >>> tg = tg.fit(X)
     >>> print(tg.transform(X)[0].toarray())
-    [[0 1 0]
-     [1 0 1]
-     [0 1 0]]
+    [[0 1]
+     [1 0]]
     >>> print(tg.transform(X)[1].toarray())
-    [[0 1 1]
-     [1 0 0]
-     [1 0 0]]
+    [[1 0]
+     [0 0]]
+
+    See also
+    --------
+    GraphGeodesicDistance, giotto.time_series.TakensEmbedding
+
+    Notes
+    -----
+    In general, the shapes of the sparse matrices output by :meth:`transform`
+    will be different across samples, and the same row or column index will
+    refer to different states in different samples.
+
+    References
+    ----------
+    .. [1] M. Small, "Complex networks from time series: Capturing dynamics",
+           *2013 IEEE International Symposium on Circuits and Systems
+           (IS-CAS2013)*, 2013; doi: `10.1109/iscas.2013.6572389
+           <http://dx.doi.org/10.1109/iscas.2013.6572389>`_.
+
+    .. [2] M. McCullough, M. Small, T. Stemler, and H. Ho-Ching Iu, "Time
+           lagged ordinal partition networks for capturing dynamics of
+           continuous dynamical systems"; *Chaos: An Interdisciplinary
+           Journal of Nonlinear Science* **25** (5), p. 053101, 2015;
+           doi: `10.1063/1.4919075 <http://dx.doi.org/10.1063/1.4919075>`_.
+
+    .. [3] A. Myers, E. Munch, and F. A. Khasawneh, "Persistent homology of
+           complex networks for dynamic state detection"; *Phys. Rev. E*
+           **100**, 022314, 2019; doi: `10.1103/PhysRevE.100.022314
+           <http://dx.doi.org/10.1109/CVPR.2015.7299106>`_.
 
     """
 
-    def __init__(self, n_jobs=None):
+    def __init__(self, func=np.argsort, func_params=None, n_jobs=None):
+        self.func = func
+        self.func_params = func_params
         self.n_jobs = n_jobs
 
     def _make_adjacency_matrix(self, X):
-        indices = np.unique(X, axis=0, return_inverse=True)[1]
-        n_indices = 2 * (len(indices) - 1)
-        first = indices[:-1]
-        second = indices[1:]
-        Xm = sp.csr_matrix((np.full(n_indices, 1),
+        Xm = np.apply_along_axis(self._func, 1, X)
+        Xm = np.unique(Xm, axis=0, return_inverse=True)[1]
+        n_indices = 2 * (len(Xm) - 1)
+        first = Xm[:-1]
+        second = Xm[1:]
+        Xm = sp.csr_matrix((np.full(n_indices, True),
                            (np.concatenate([first, second]),
                             np.concatenate([second, first]))))
-        # See issue #36
+        # Silence sparse warnings TODO BENCHMARK
         with warnings.catch_warnings():
             warnings.simplefilter('ignore', SparseEfficiencyWarning)
             sp.csr_matrix.setdiag(Xm, 0)
@@ -74,8 +137,8 @@ class TransitionGraph(BaseEstimator, TransformerMixin):
             Input data.
 
         y : None
-            There is no need of a target in a transformer, yet the pipeline API
-            requires this parameter.
+            There is no need for a target in a transformer, yet the pipeline
+            API requires this parameter.
 
         Returns
         -------
@@ -83,8 +146,15 @@ class TransitionGraph(BaseEstimator, TransformerMixin):
 
         """
         check_array(X, allow_nd=True)
+        if self.func is None:
+            self._func = _identity
+        self._func = self.func
 
-        self._is_fitted = True
+        if self.func_params is None:
+            self.effective_func_params_ = {}
+        else:
+            self.effective_func_params_ = self.func_params.copy()
+
         return self
 
     def transform(self, X, y=None):
@@ -99,8 +169,8 @@ class TransitionGraph(BaseEstimator, TransformerMixin):
             Input data.
 
         y : None
-            There is no need of a target in a transformer, yet the pipeline API
-            requires this parameter.
+            There is no need for a target in a transformer, yet the pipeline
+            API requires this parameter.
 
         Returns
         -------
@@ -110,12 +180,11 @@ class TransitionGraph(BaseEstimator, TransformerMixin):
 
         """
         # Check if fit had been called
-        check_is_fitted(self, ['_is_fitted'])
+        check_is_fitted(self, ['_func', 'effective_func_params_'])
         Xt = check_array(X, copy=True, allow_nd=True)
 
-        Xt = np.argsort(Xt, axis=2)
-
         Xt = Parallel(n_jobs=self.n_jobs)(
-            delayed(self._make_adjacency_matrix)(X[i]) for i in
-            range(X.shape[0]))
+            delayed(self._make_adjacency_matrix)(Xt[i]) for i in
+            range(Xt.shape[0]))
+        Xt = np.asarray(Xt)
         return Xt

giotto/graphs/tests/test_transition.py
@@ -7,12 +7,17 @@ from sklearn.exceptions import NotFittedError
 
 from giotto.graphs import TransitionGraph
 
-X_tg = np.array([[['a'], ['b'], ['c']]])
+X_tg = np.array([[[1, 0], [2, 3], [5, 4]],
+                 [[0, 1], [3, 2], [4, 5]]])
 
 X_tg_res = np.array([
-    csr_matrix((np.array([True] * 4),
-                (np.array([0, 1, 1, 2]),
-                 np.array([1, 0, 2, 1]))), shape=(3, 3))])
+    csr_matrix((np.array([True] * 2),
+                (np.array([0, 1]),
+                 np.array([1, 0]))), shape=(2, 2)),
+    csr_matrix((np.array([True] * 2),
+                (np.array([0, 1]),
+                 np.array([1, 0]))), shape=(2, 2)),
+])
 
 
 def test_transition_graph_not_fitted():
@@ -24,13 +29,18 @@ def test_transition_graph_not_fitted():
 
 def test_transition_graph_transform():
     tg = TransitionGraph()
+    Xt = tg.fit_transform(X_tg)
 
-    assert (tg.fit_transform(X_tg)[0] != X_tg_res[0]).nnz == 0
+    assert np.array_equal(Xt[0].todense(), X_tg_res[0].todense())
+    assert np.array_equal(Xt[1].todense(), X_tg_res[1].todense())
 
 
 def test_parallel_transition_graph_transform():
     tg = TransitionGraph(n_jobs=1)
     tg_parallel = TransitionGraph(n_jobs=2)
 
-    assert (tg.fit_transform(X_tg)[0] !=
-            tg_parallel.fit_transform(X_tg)[0]).nnz == 0
+    Xt = tg.fit_transform(X_tg)
+    Xt_parallel = tg_parallel.fit_transform(X_tg)
+
+    assert np.array_equal(Xt[0].todense(), Xt_parallel[0].todense())
+    assert np.array_equal(Xt[1].todense(), Xt_parallel[1].todense())

commit 662c8da1a35e23ac4d57a1a862874b702e840ee1

Fixes TransitionGraph  (#28) (#31)
* [#23] Fix and generalise TransitionGraph

* Fix bug introduced in e4c0686

giotto/graphs/transition.py
@@ -148,7 +148,8 @@ class TransitionGraph(BaseEstimator, TransformerMixin):
         check_array(X, allow_nd=True)
         if self.func is None:
             self._func = _identity
-        self._func = self.func
+        else:
+            self._func = self.func
 
         if self.func_params is None:
             self.effective_func_params_ = {}