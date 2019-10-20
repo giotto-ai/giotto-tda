5a590eeba1bfda3d4b3e1df8a2a1b5ecb95034d7
Umberto <u.lupo@l2f.ch>
2019-10-21 00:26:05 +0200

    Finalise docs, change n_neighbors default from 5 to 4, change metric_params default from None to {} (#32)

--- giotto/graphs/kneighbors.py
+++ giotto/graphs/kneighbors.py
@@ -12,52 +12,52 @@ from sklearn.utils.validation import check_array, check_is_fitted
 
 
 class KNeighborsGraph(BaseEstimator, TransformerMixin):
-    """Calculates adjacency matrices of :math:`k`-nearest neighbor graphs.
+    """Adjacency matrices of k-nearest neighbor graphs.
 
-    Let :math:`k` be a positive integer, and :math:`X` be a collection of point
-    clouds in Euclidean space, each encoded as a two-dimensional array. For
-    each point cloud :math:`\\mathcal{P}` in :math:`X`, the corresponding kNN
-    graph is an undirected and unweighted graph with an edge between any two
-    points :math:`p_i, p_j` in :math:`\\mathcal{P}` whenever either :math:`p_i`
-    is among the :math:`k` nearest neighbors of :math:`p_j`, or :math:`p_j`
-    is among the :math:`k` nearest neighbors of resp. :math:`p_i`. A point
-    is not regarded as a neighbor of itself, i.e. the resulting graph is
-    simple.
+    Given a two-dimensional array of row vectors seen as points in
+    high-dimensional space, the corresponding kNN graph is a simple,
+    undirected and unweighted graph with a vertex for every vector in the
+    array, and an edge between two vertices whenever either the first
+    corresponding vector is among the k nearest neighbors of the
+    second, or vice-versa.
+
+    :obj:`sklearn.neighbors.kneighbors_graph` is used to compute the
+    adjacency matrices of kNN graphs.
 
     Parameters
     ----------
-    n_neighbors : int, optional, default: ``5``
-        Number of neighbors to use by default for :meth:`kneighbors` queries.
+    n_neighbors : int, optional, default: ``4``
+        Number of neighbors to use.
 
     metric : string or callable, default ``'minkowski'``
         Metric to use for distance computation. Any metric from scikit-learn
-        or ``scipy.spatial.distance`` can be used.
+        or :obj:`scipy.spatial.distance` can be used.
         If metric is a callable function, it is called on each
         pair of instances (rows) and the resulting value recorded. The callable
         should take two arrays as input and return one value indicating the
         distance between them. This works for Scipy's metrics, but is less
         efficient than passing the metric name as a string.
         Distance matrices are not supported.
-        Valid values for metric are:
+        Valid values for `metric` are:
 
-        - from scikit-learn: ['cityblock', 'cosine', 'euclidean', 'l1', \
-          'l2', 'manhattan']
-        - from ``scipy.spatial.distance``: ['braycurtis', 'canberra', \
-          'chebyshev', 'correlation', 'dice', 'hamming', 'jaccard', \
-          'kulsinski', 'mahalanobis', 'minkowski', 'rogerstanimoto', \
-          'russellrao', 'seuclidean', 'sokalmichener', 'sokalsneath', \
-          'sqeuclidean', 'yule']
+        - from scikit-learn: [``'cityblock'``, ``'cosine'``, ``'euclidean'``,
+          ``'l1'``, ``'l2'``, ``'manhattan'``]
+        - from :obj:`scipy.spatial.distance`: [``'braycurtis'``,
+          ``'canberra'``, ``'chebyshev'``, ``'correlation'``, ``'dice'``,
+          ``'hamming'``, ``'jaccard'``, ``'kulsinski'``, ``'mahalanobis'``,
+          ``'minkowski'``, ``'rogerstanimoto'``, ``'russellrao'``,
+          ``'seuclidean'``, ``'sokalmichener'``, ``'sokalsneath'``,
+          ``'sqeuclidean'``, ``'yule'``]
 
-        See the documentation for ``scipy.spatial.distance`` for details on
+        See the documentation for :obj:`scipy.spatial.distance` for details on
         these metrics.
 
     p : int, optional, default: ``2``
-        Parameter for the Minkowski metric from
-        sklearn.metrics.pairwise.pairwise_distances. When p = 1, this is
-        equivalent to using manhattan_distance (l_1), and euclidean_distance
-        (l_2) for p = 2. For arbitrary p, minkowski_distance (l_p) is used.
+        Parameter for the Minkowski (i.e. :math:`\\ell^p`) metric from
+        :obj:`sklearn.metrics.pairwise.pairwise_distances`. `p` = 1 is the
+        Manhattan distance and `p` = 2 is the Euclidean distance.
 
-    metric_params : dict, optional, default: ``None``
+    metric_params : dict, optional, default: ``{}``
         Additional keyword arguments for the metric function.
 
     n_jobs : int or None, optional, default: ``None``
@@ -69,58 +69,51 @@ class KNeighborsGraph(BaseEstimator, TransformerMixin):
     --------
     >>> import numpy as np
     >>> from giotto.graphs import KNeighborsGraph
-    >>> X = np.array([
-    ...         np.array([
-    ...             [0, 1, 3, 0, 0],
-    ...             [1, 0, 5, 0, 0],
-    ...             [3, 5, 0, 4, 0],
-    ...             [0, 0, 4, 0, 0],
-    ...             [0, 0, 0, 0, 0]])])
+    >>> X = np.array([[[0, 1, 3, 0, 0],
+    ...                [1, 0, 5, 0, 0],
+    ...                [3, 5, 0, 4, 0],
+    ...                [0, 0, 4, 0, 0]]])
     >>> kng = KNeighborsGraph(n_neighbors=2)
-    >>> kng = kng.fit(X)
-    >>> print(kng.transform(X)[0].toarray())
-    [[0. 1. 1. 1. 1.]
-     [1. 0. 0. 1. 0.]
-     [1. 0. 0. 0. 1.]
-     [1. 1. 0. 0. 1.]
-     [1. 0. 1. 1. 0.]]
+    >>> Xg = kng.fit_transform(X)
+    >>> print(Xg[0].toarray())
+    [[0. 1. 1. 1.]
+     [1. 0. 0. 1.]
+     [1. 0. 0. 1.]
+     [1. 1. 1. 0.]]
 
     """
 
-    def __init__(self, n_neighbors=5, metric='euclidean',
-                 p=2, n_jobs=None, metric_params=None):
-        if metric_params is None:
-            metric_params = {}
+    def __init__(self, n_neighbors=4, metric='euclidean',
+                 p=2, metric_params={}, n_jobs=None):
         self.n_neighbors = n_neighbors
         self.metric = metric
         self.p = p
-        self.n_jobs = n_jobs
         self.metric_params = metric_params
+        self.n_jobs = n_jobs
 
     def _make_adjacency_matrix(self, X):
         A = self._nearest_neighbors(X)
         rows, cols = A.nonzero()
-        # See issue #36
         with warnings.catch_warnings():
             warnings.simplefilter('ignore', SparseEfficiencyWarning)
             A[cols, rows] = 1
         return A
 
     def fit(self, X, y=None):
-        """Do nothing and return the estimator unchanged. TODO CORRECT
+        """Do nothing and return the estimator unchanged.
 
         This method is there to implement the usual scikit-learn API and hence
         work in pipelines.
 
         Parameters
         ----------
-        X : ndarray, shape (n_samples, n_points, n_features)
-            Input data. Each entry of X along axis 0 is interpreted as an
-            ndarray of n_points in Euclidean space of dimension n_features.
+        X : ndarray, shape (n_samples, n_points, n_dimensions)
+            Input data. Each entry in `X` along axis 0 is an array of
+            ``n_points`` row vectors in ``n_dimensions``-dimensional space.
 
         y : None
-            There is no need for a target in a transformer, yet the pipeline API
-            requires this parameter.
+            There is no need for a target in a transformer, yet the pipeline
+            API requires this parameter.
 
         Returns
         -------
@@ -137,25 +130,23 @@ class KNeighborsGraph(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X, y=None):
-        """Compute the adjacency matrix of the kNN graph of each entry in the
-        input array along axis 0. ``sklearn.neighbors.kneighbors_graph`` is
-        used.
+        """Compute kNN graphs and return their adjacency matrices as
+        sparse matrices.
 
         Parameters
         ----------
-        X : ndarray, shape (n_samples, n_points, n_features)
-            Input data. Each entry of X along axis 0 is interpreted as an
-            ndarray of n_points in Euclidean space of dimension n_features.
+        X : ndarray, shape (n_samples, n_points, n_dimensions)
+            Input data. Each entry in `X` along axis 0 is an array of
+            ``n_points`` row vectors in ``n_dimensions``-dimensional space.
 
         y : None
-            There is no need for a target in a transformer, yet the pipeline API
-            requires this parameter.
+            There is no need for a target in a transformer, yet the pipeline
+            API requires this parameter.
 
         Returns
         -------
-        Xt : ndarray of sparse matrices in CSR format, shape \
-             (n_samples, )
-            The transformed array.
+        Xt : ndarray of sparse matrices in CSR format, shape (n_samples,)
+            Adjacency matrices of kNN graphs.
 
         """
         # Check if fit had been called
