8be3a7787226eb6345253720647702d5868845ce
julian <julian.burellaperez@heig-vd.ch>
2019-11-06 13:51:36 +0100

    Update cubical complex include, to add the library form the correct path
    Update CMakeLists.txt file, to indicate the path to the Gudhi's interface
    
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>

--- giotto/externals/bindings/cubical_complex_bindings.cpp
+++ giotto/externals/bindings/cubical_complex_bindings.cpp
@@ -7,6 +7,7 @@
 #include <gudhi/Bitmap_cubical_complex.h>
 #include <gudhi/Bitmap_cubical_complex_base.h>
 #include <gudhi/Bitmap_cubical_complex_periodic_boundary_conditions_base.h>
+#include <Cubical_complex_interface.h>
 
 #include <iostream>
 #include <string>
@@ -15,41 +16,23 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-template <typename CubicalComplexOptions =
-              Gudhi::Cubical_complex::Bitmap_cubical_complex_base<double>>
-class Cubical_complex_interface
-    : public Gudhi::Cubical_complex::Bitmap_cubical_complex<
-          CubicalComplexOptions> {
- public:
-  Cubical_complex_interface(const std::vector<unsigned>& dimensions,
-                            const std::vector<double>& top_dimensional_cells)
-      : Gudhi::cubical_complex::Bitmap_cubical_complex<CubicalComplexOptions>(
-            dimensions, top_dimensional_cells) {}
-
-  Cubical_complex_interface(const std::vector<unsigned>& dimensions,
-                            const std::vector<double>& top_dimensional_cells,
-                            const std::vector<bool>& periodic_dimensions)
-      : Gudhi::cubical_complex::Bitmap_cubical_complex<CubicalComplexOptions>(
-            dimensions, top_dimensional_cells, periodic_dimensions) {}
-
-  Cubical_complex_interface(const std::string& perseus_file)
-      : Gudhi::cubical_complex::Bitmap_cubical_complex<CubicalComplexOptions>(
-            perseus_file.c_str()) {}
-};
-
 namespace py = pybind11;
 
 PYBIND11_MODULE(giotto_cubical_complex, m) {
-  using Cubical_complex_interface_inst = Cubical_complex_interface<>;
+  using namespace pybind11::literals;
+  using Cubical_complex_interface_inst =
+      Gudhi::cubical_complex::Cubical_complex_interface<>;
   using Bitmap_cubical_complex_inst =
       Gudhi::Cubical_complex::Bitmap_cubical_complex<
           Gudhi::Cubical_complex::Bitmap_cubical_complex_base<double>>;
   py::class_<Bitmap_cubical_complex_inst, Cubical_complex_interface_inst>(
       m, "Cubical_complex_interface", py::buffer_protocol(), py::dynamic_attr())
-      .def(py::init<const std::vector<unsigned>&, const std::vector<double>&>())
+      .def(py::init<const std::vector<unsigned>&, const std::vector<double>&>(),
+           "dimensions"_a, "top_dimensional_cells"_a)
       .def(py::init<const std::vector<unsigned>&, const std::vector<double>&,
-                    const std::vector<bool>&>())
-      .def(py::init<const std::string&>())
+                    const std::vector<bool>&>(),
+           "dimensions"_a, "top_dimensional_cells"_a, "periodic_dimensions"_a)
+      .def(py::init<const std::string&>(), "perseus_file"_a)
       .def("num_simplices", &Cubical_complex_interface_inst::num_simplices)
       .def("dimension",
            py::overload_cast<>(&Cubical_complex_interface_inst::dimension,
