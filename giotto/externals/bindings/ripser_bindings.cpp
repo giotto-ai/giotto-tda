97c30e3f508860265c734b8cadc40402f48e3008
REDS institute <reds-heig@users.noreply.github.com>
2019-11-06 15:59:15 +0100

    Bindings update (#81)
    * Add clang format files following Google Guidelines for C++
    
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>
    
    * Update code format of all C++ bindings
    
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>
    
    * Update cubical complex include, to add the library form the correct path
    
    Update CMakeLists.txt file, to indicate the path to the Gudhi's interface
    
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>
    
    * Update persistent cohomology, to add the library from the correct location
    
    Update CMakeLists.txt file, to indicate the path to the Gudhi's interface
    Delete old local interface file
    
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>
    
    * Update simplex tree include, to add the library from the correct location
    
    Update CMakeLists.txt file, to indicate the path to the Gudhi's interface
    Delete old local interface file
    Fix unused include in rips_complex which made build fail
    
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>
    
    * Update headers of bindings:
    
    Remove author name
    Update license
    
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>
    
    * Update more headers of bindings:
    
    Remove author name
    Update license
    
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>
    
    * update submodule commit
    
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>

--- giotto/externals/bindings/ripser_bindings.cpp
+++ giotto/externals/bindings/ripser_bindings.cpp
@@ -1,8 +1,8 @@
 /******************************************************************************
-* Author:           Julián Burella Pérez
-* Description:      ripser's rips persistence interfacing with pybind11
-* License:          TBD
-*****************************************************************************/
+ * Author:           Julián Burella Pérez
+ * Description:      ripser's rips persistence interfacing with pybind11
+ * License:          TBD
+ *****************************************************************************/
 
 #include "../ripser/ripser/ripser.cpp"
 
@@ -10,23 +10,32 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-
 namespace py = pybind11;
 
 PYBIND11_MODULE(giotto_ripser, m) {
-    m.doc() = "Ripser python interface";
-    py::class_<ripserResults>(m, "ripserResults")
-        .def_readwrite("births_and_deaths_by_dim", &ripserResults::births_and_deaths_by_dim)
-        .def_readwrite("cocycles_by_dim", &ripserResults::cocycles_by_dim)
-        .def_readwrite("num_edges", &ripserResults::num_edges);
+  m.doc() = "Ripser python interface";
+  py::class_<ripserResults>(m, "ripserResults")
+      .def_readwrite("births_and_deaths_by_dim",
+                     &ripserResults::births_and_deaths_by_dim)
+      .def_readwrite("cocycles_by_dim", &ripserResults::cocycles_by_dim)
+      .def_readwrite("num_edges", &ripserResults::num_edges);
 
-    m.def("rips_dm", [](std::vector<float> D, int N, int modulus, int dim_max, float threshold, int do_cocycles){
-            ripserResults ret = rips_dm(&D[0], N, modulus, dim_max, threshold, do_cocycles);
-            return ret;
-            }, "ripser distance matrix");
-    m.def("rips_dm_sparse", [](std::vector<int> I, std::vector<int> J, std::vector<float> V, int NEdges, int N, int modulus, int dim_max, float threshold, int do_cocycles){
-            ripserResults ret = rips_dm_sparse(&I[0], &J[0], &V[0], NEdges, N, modulus, dim_max, threshold, do_cocycles);
-            return ret;
-
-            }, "ripser sparse distance matrix");
+  m.def("rips_dm",
+        [](std::vector<float> D, int N, int modulus, int dim_max,
+           float threshold, int do_cocycles) {
+          ripserResults ret =
+              rips_dm(&D[0], N, modulus, dim_max, threshold, do_cocycles);
+          return ret;
+        },
+        "ripser distance matrix");
+  m.def("rips_dm_sparse",
+        [](std::vector<int> I, std::vector<int> J, std::vector<float> V,
+           int NEdges, int N, int modulus, int dim_max, float threshold,
+           int do_cocycles) {
+          ripserResults ret =
+              rips_dm_sparse(&I[0], &J[0], &V[0], NEdges, N, modulus, dim_max,
+                             threshold, do_cocycles);
+          return ret;
+        },
+        "ripser sparse distance matrix");
 }
