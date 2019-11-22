bf8b441a9de76bf0dafb3b2af561a4b65aef7330
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2019-11-22 14:23:11 +0100

    Rename unit test for clarity

--- giotto/mapper/tests/test_filter.py
+++ giotto/mapper/tests/test_filter.py
@@ -19,7 +19,7 @@ def test_eccentricity_shape_equals_number_of_samples(X):
 
 
 @given(X=arrays(dtype=np.float, shape=array_shapes(min_dims=2, max_dims=2)))
-def test_eccentricity_value_with_infinity_norm_equals_max_row_value(X):
+def test_eccentricity_values_with_infinity_norm_equals_max_row_values(X):
     eccentricity = Eccentricity()
     Xt = eccentricity.fit_transform(X)
     distance_matrix = squareform(pdist(X))
