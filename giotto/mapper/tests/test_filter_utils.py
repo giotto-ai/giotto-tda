6aef03bda11d3c43a4ecdfa7c0db891a70d78f25
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2019-11-22 12:26:52 +0100

    Add unit test for infinity norm of eccentricity

--- giotto/mapper/tests/test_filter_utils.py
+++ giotto/mapper/tests/test_filter_utils.py
@@ -8,6 +8,7 @@ from sklearn.exceptions import NotFittedError
 from giotto.mapper import Eccentricity
 from hypothesis import given
 from hypothesis.extra.numpy import arrays, array_shapes
+from scipy.spatial.distance import pdist, squareform
 
 
 @given(X=arrays(dtype=np.float, shape=array_shapes(min_dims=2, max_dims=2)))
@@ -15,3 +16,11 @@ def test_eccentricity_shape_equals_number_of_samples(X):
     eccentricity = Eccentricity()
     Xt = eccentricity.fit_transform(X)
     assert Xt.shape == (len(X),)
+
+
+@given(X=arrays(dtype=np.float, shape=array_shapes(min_dims=2, max_dims=2)))
+def test_eccentricity_value_with_infinity_norm_equals_max_row_value(X):
+    eccentricity = Eccentricity()
+    Xt = eccentricity.fit_transform(X)
+    distance_matrix = squareform(pdist(X))
+    assert_almost_equal(Xt, np.max(distance_matrix, axis=1))
