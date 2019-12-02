adf13883b7abb03ffa4ab6a08a87dbf44e1c402c
RunnerMan <francesco.pase.work@gmail.com>
2019-12-02 16:08:40 +0100

    diffusion modules

--- giotto/graphs/__init__.py
+++ giotto/graphs/__init__.py
@@ -5,9 +5,17 @@ extract metric spaces from graphs.
 from giotto.graphs.geodesic_distance import GraphGeodesicDistance
 from giotto.graphs.kneighbors import KNeighborsGraph
 from giotto.graphs.transition import TransitionGraph
+from giotto.graphs.create_clique_complex import CreateCliqueComplex, CreateBoundaryMatrices, CreateLaplacianMatrices
+from giotto.graphs.heat_diffusion import HeatDiffusion
+from giotto.graphs.graph_entropy import GraphEntropy
 
 __all__ = [
     'TransitionGraph',
     'KNeighborsGraph',
     'GraphGeodesicDistance'
+    'CreateCliqueComplex'
+    'CreateBoundaryMatrices'
+    'CreateLaplacianMatrices'
+    'HeatDiffusion'
+    'GraphEntropy'
 ]
---
+++ giotto/graphs/tests/test_graph_entropy.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pytest
+
+from sklearn.exceptions import NotFittedError
+from create_clique_complex import CreateCliqueComplex, CreateBoundaryMatrices, CreateLaplacianMatrices
+from graph_entropy import GraphEntropy
+from heat_diffusion import HeatDiffusion
+
+taus = np.linspace(0, 5, 20)
+
+X = np.random.random((10, 2))
+alpha = 0.4
+cc = CreateCliqueComplex(data=X, alpha=alpha, data_type='cloud')
+cd = cc.create_complex_from_graph()
+lap_node = CreateLaplacianMatrices().fit(cd, (0)).transform(cd)[0]
+heat_vectors = HeatDiffusion().fit(lap_node, taus=taus).transform(lap_node)
+
+
+def test_graph_entropy_fitted():
+    entropy = GraphEntropy()
+
+    with pytest.raises(NotFittedError):
+        embs = entropy.transform(heat_vectors)
+
+
+test_graph_entropy_fitted()
+
