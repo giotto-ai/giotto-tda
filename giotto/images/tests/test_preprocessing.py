05a03fa4e7bf00c28e10b2a30fa04520ca684a6b
Guillaume Tauzin <g.tauzin@l2f.ch>
2020-01-15 16:08:56 +0100

    Fix Binarizer tests

--- giotto/images/tests/test_preprocessing.py
+++ giotto/images/tests/test_preprocessing.py
@@ -8,8 +8,8 @@ from sklearn.exceptions import NotFittedError
 
 from giotto.images import Binarizer
 
-images_2D = np.stack([np.random((7, 8)),
-                      np.concatenate([np.random((7, 4)), np.zeros((7, 4))],
+images_2D = np.stack([np.ones((7, 8)),
+                      np.concatenate([np.ones((7, 4)), np.zeros((7, 4))],
                                      axis=1),
                       np.zeros((7, 8))], axis=0)
 
@@ -22,20 +22,20 @@ images_3D = np.stack([np.ones((7, 8, 4)),
 def test_binarizer_not_fitted():
     binarizer = Binarizer()
     with pytest.raises(NotFittedError):
-        binarizer.transform(images_2d)
+        binarizer.transform(images_2D)
 
 
 def test_binarizer_errors():
     threshold = 'a'
     binarizer = Binarizer(threshold=threshold)
-    with pytest.raises(ValueError):
+    with pytest.raises(TypeError):
         binarizer.fit(images_2D)
 
 
 @pytest.mark.parametrize("threshold, expected",
                          [(0.65, images_2D),
                           (0.53, images_3D)])
-def test_binarizer_transform(operation, expected):
+def test_binarizer_transform(threshold, expected):
     binarizer = Binarizer(threshold=threshold)
 
     assert_almost_equal(binarizer.fit_transform(expected),
