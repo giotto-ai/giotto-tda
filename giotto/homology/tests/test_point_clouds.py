572199823a31d94c8793819b7f2df8bcf2b7b3d3
Guillaume Tauzin <g.tauzin@l2f.ch>
2020-01-15 11:05:03 +0100

    [WIP] Improve tests for WitnessPersistence

--- giotto/homology/tests/test_point_clouds.py
+++ giotto/homology/tests/test_point_clouds.py
@@ -6,7 +6,7 @@ import pytest
 from numpy.testing import assert_almost_equal
 from sklearn.exceptions import NotFittedError
 
-from giotto.homology import VietorisRipsPersistence
+from giotto.homology import VietorisRipsPersistence, WitnessPersistence
 
 X = np.array([[[2., 2.47942554],
                [2.47942554, 2.84147098],
@@ -39,3 +39,30 @@ def test_vrp_transform():
                            [0.69093919, 0.80131882, 1]]])
 
     assert_almost_equal(vrp.fit_transform(X), X_vrp_res)
+
+
+# def test_wit_params():
+#     metric = 'not_defined'
+#     wit = WitnessPersistence()
+
+#     with pytest.raises(ValueError):
+#         wit.fit_transform(X)
+
+
+def test_wit_not_fitted():
+    wit = WitnessPersistence()
+
+    with pytest.raises(NotFittedError):
+        wit.transform(X)
+
+
+def test_wit_transform():
+    wit = WitnessPersistence()
+    X_vrp_wit = np.array([[[0., 0.43094373, 0],
+                           [0., 0.5117411, 0],
+                           [0., 0.60077095, 0],
+                           [0., 0.62186205, 0],
+                           [0.69093919, 0.80131882, 1]]])
+    print(wit.fit_transform(X))
+    assert false
+    #assert_almost_equal(wit.fit_transform(X), X_vrp_wit)
