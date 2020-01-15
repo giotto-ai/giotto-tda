a5dbef0a1d2f5f4a1f784d0357800b84349bf94e
Umberto <u.lupo@l2f.ch>
2020-01-15 22:20:20 +0100

    Remove transform method from Nerve, add fit_transform, unhide attributes
    Attributes are only stored if fit is called.

--- giotto/mapper/nerve.py
+++ giotto/mapper/nerve.py
@@ -10,10 +10,7 @@ import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils.validation import check_is_fitted
 
-from ..utils._docs import adapt_fit_transform_docs
 
-
-@adapt_fit_transform_docs
 class Nerve(BaseEstimator, TransformerMixin):
     """One-dimensional skeleton of the nerve of a Mapper cover, i.e. the
     Mapper graph.
@@ -29,34 +26,35 @@ class Nerve(BaseEstimator, TransformerMixin):
         The minimum size of the intersection between Mapper cover sets
         required to create an edge in the Mapper graph.
 
+    Attributes
+    ----------
+    X_ : TODO: Write
+
+    edges_ : TODO: Write
+
     """
 
     def __init__(self, min_intersection=1):
         self.min_intersection = min_intersection
 
     def fit(self, X, y=None):
-        """Construct a Mapper graph from an abstract Mapper cover `X`.
-
-        The input to :meth:`fit` is a list of lists. Each sublist corresponds
-        to a (non-empty) pullback cover set -- equivalently, to a cover set in
-        the filter range which has non-empty preimage -- and contains
-        triples of the form ``(pullback_cover_id, partial_cluster_label, \
-        indices)`` where ``partial_cluster_label`` is a cluster label within
-        the pullback cover set identified by ``pullback_cover_id``,
-        and ``indices`` is the array of indices of points belonging to
-        cluster ``(pullback_cover_id, partial_cluster_label)``.
-
-        The output of :meth:`transform` is an :class:`igraph.Graph` object
-        storing the Mapper graph such that edges exist between two Mapper
-        cover sets if and only if the size of the intersection between the two
-        sets is no less than `min_intersection`.
+        """Compute and store the nodes and edges of the Mapper graph,
+        and return the estimator.
 
         Parameters
         ----------
         X : list of list of tuple
-            Input data structure describing an abstract Mapper cover.
-            In the context of a :class:`giotto.mapper.MapperPipeline`, this is
-            the output of the clustering step.
+            Input data structure describing an abstract Mapper cover. Each
+            sublist corresponds to a (non-empty) pullback cover set --
+            equivalently, to a cover set in the filter range which has
+            non-empty preimage -- and contains triples of the form ``( \
+            pullback_cover_id, partial_cluster_label, indices)`` where
+            ``partial_cluster_label`` is a cluster label within the pullback
+            cover set identified by ``pullback_cover_id``, and ``indices``
+            is the array of indices of points belonging to cluster ``( \
+            pullback_cover_id, partial_cluster_label)``. In the context of a
+            :class:`giotto.mapper.MapperPipeline`, this is the output of the
+            clustering step.
 
         y : None
             There is no need for a target in a transformer, yet the pipeline
@@ -68,22 +66,30 @@ class Nerve(BaseEstimator, TransformerMixin):
 
         """
         # TODO: Include a validation step for X
-        self._X = reduce(iconcat, X, [])
+        self.X_ = reduce(iconcat, X, [])
         # Preprocess X by 1) flattening and 2) extending each tuple
-        self._X = [(node_info[0], *node_info[1])
-                   for node_info in zip(range(len(self._X)), self._X)]
-        self._edges = list(self._generate_edges(self._X))
+        self.X_ = [(node_info[0], *node_info[1])
+                   for node_info in zip(range(len(self.X_)), self.X_)]
+        self.edges_ = list(self._generate_edges(self.X_))
         return self
 
-    def transform(self, X, y=None):
-        """Return Mapper graph as a graph object.
+    def fit_transform(self, X, y=None, **fit_params):
+        """Construct a Mapper graph from an abstract Mapper cover `X`.
 
         Parameters
         ----------
         X : list of list of tuple
-            Input data structure describing an abstract Mapper cover.
-            In the context of a :class:`giotto.mapper.MapperPipeline`, this is
-            the output of the clustering step.
+            Input data structure describing an abstract Mapper cover. Each
+            sublist corresponds to a (non-empty) pullback cover set --
+            equivalently, to a cover set in the filter range which has
+            non-empty preimage -- and contains triples of the form ``( \
+            pullback_cover_id, partial_cluster_label, indices)`` where
+            ``partial_cluster_label`` is a cluster label within the pullback
+            cover set identified by ``pullback_cover_id``, and ``indices``
+            is the array of indices of points belonging to cluster ``( \
+            pullback_cover_id, partial_cluster_label)``. In the context of a
+            :class:`giotto.mapper.MapperPipeline`, this is the output of the
+            clustering step.
 
         y : None
             There is no need for a target in a transformer, yet the pipeline
@@ -92,19 +98,28 @@ class Nerve(BaseEstimator, TransformerMixin):
         Returns
         -------
         graph : :class:`igraph.Graph` object
-            Mapper graph.
+            Mapper graph. Edges exist between two Mapper cover sets if and
+            only if the size of the intersection between the two sets is no
+            less than `min_intersection`.
 
         """
-        check_is_fitted(self, '_edges')
+        # TODO: Include a validation step for X
+        _X = reduce(iconcat, X, [])
+        # Preprocess X by 1) flattening and 2) extending each tuple
+        _X = [(node_info[0], *node_info[1])
+              for node_info in zip(range(len(_X)), _X)]
+        _edges = self._generate_edges(_X)
+
+        # Graph construction
         graph = ig.Graph()
-        graph.add_vertices([vertex[0] for vertex in self._X])
+        graph.add_vertices([vertex[0] for vertex in _X])
         graph.add_edges([
             (edge['node_indices'][0][0], edge['node_indices'][1][0])
-            for edge in self._edges
+            for edge in _edges
         ])
         graph['node_metadata'] = dict(
             zip(['node_id', 'interval_id', 'cluster_id', 'node_elements'],
-                zip(*self._X)))
+                zip(*_X)))
         return graph
 
     @staticmethod
