8480b95114b29ff62602e5056d890bc5d875d01a
Wojciech Reise <reisewojtus@gmail.com>
2020-01-20 16:29:22 +0100

    Tests for HeatKernels ( #176)

--- giotto/diagrams/tests/test_features.py
+++ giotto/diagrams/tests/test_features.py
@@ -11,6 +11,7 @@ from numpy.testing import assert_almost_equal
 from sklearn.exceptions import NotFittedError
 
 from giotto.diagrams import PersistenceEntropy, HeatKernel
+from giotto.utils.validation import _validate_distinct
 
 X_pe = np.array([[[0, 1, 0], [2, 3, 0], [4, 6, 1], [2, 6, 1]]])
 
@@ -33,52 +34,72 @@ pts_gen = arrays(
     dtype=np.float,
     elements=floats(allow_nan=False,
                     allow_infinity=False,
-                    min_value=0.,
+                    min_value=1.,
                     max_value=10),
     shape=(1, 20, 2)
 )
 dims_gen = arrays(
     dtype=np.int,
-    elements=integers(min_value=0.,
+    elements=integers(min_value=0,
                       max_value=3),
     shape=(1, 20, 1)
 )
 
 
+def get_input(pts, dims):
+    for p in pts:
+        try:
+            _validate_distinct([pts])
+        except ValueError:
+            p[0, 0:2] += 0.3 # add a distinct value
+    X = np.concatenate([np.sort(pts, axis=2), dims], axis=2)
+    return X
+
+
+def test_all_pts_the_same():
+    X = np.zeros((1, 4, 3))
+    hk = HeatKernel(sigma=1)
+    with pytest.raises(ValueError):
+        X_ = hk.fit(X).transform(X)
+
+
 @given(pts_gen, dims_gen)
 def test_hk_shape(pts, dims):
     n_values = 10
+    x = get_input(pts, dims)
+
     hk = HeatKernel(sigma=1, n_values=n_values)
+    num_dimensions = len(np.unique(dims))
+    x_t = hk.fit(x).transform(x)
 
-    X = np.concatenate([pts, dims], axis=2)
-    num_dimensions = np.unique(dims)
-    X_T = hk.fit(X).transform(X)
-
-    assert X_T.shape == (X.shape[0], num_dimensions, n_values, n_values)
+    assert x_t.shape == (x.shape[0], num_dimensions, n_values, n_values)
 
 
 @given(pts_gen, dims_gen)
 def test_hk_positive(pts, dims):
+    """ We expect the points above the PD-diagonal to be non-negative. (up to a numerical error)"""
     n_values = 10
     hk = HeatKernel(sigma=1, n_values=n_values)
 
-    X = np.concatenate([pts, dims], axis=2)
-    num_dimensions = np.unique(dims)
-    X_T = hk.fit(X).transform(X)
+    x = get_input(pts, dims)
+    x_t = hk.fit(x).transform(x)
 
-    assert np.all(X_T >= 0)
+    assert np.all((np.tril(x_t[:, :, ::-1, :]) + 1e-13) >= 0.)
 
 
-@given(pts_gen, dims_gen)
-def test_hk_with_diag_points(pts, dims):
+@given(pts_gen)
+def test_hk_with_diag_points(pts):
+    """Add points on the diagonal, and verify that we have the same results (on the same fitted values)."""
     n_values = 10
     hk = HeatKernel(sigma=1, n_values=n_values)
 
-    X = np.concatenate([pts, np.zeros((pts.shape[0], pts.shape[1], 1))], axis=2)
-    num_dimensions = np.unique(dims)
+    x = get_input(pts, np.zeros((pts.shape[0], pts.shape[1], 1)))
     diag_points = np.array([[[2, 2, 0], [3, 3, 0], [7, 7, 0]]])
-    X_with_diag_points = np.concatenate([X, diag_points], axis=1)
+    x_with_diag_points = np.concatenate([x, diag_points], axis=1)
 
-    X_T, X_with_diag_points_T = [hk.fit(x).transform(x) for x in [X, X_with_diag_points]]
+    # X_total = np.concatenate([X,X_with_diag_points], axis =0)
+    hk = hk.fit(x_with_diag_points)
 
-    assert_almost_equal(X_with_diag_points_T, X_T)
+    x_t, x_with_diag_points_t = [hk.transform(x_) for x_ in [x, x_with_diag_points]]
+
+    assert_almost_equal(x_with_diag_points_t, x_t, decimal=13)
