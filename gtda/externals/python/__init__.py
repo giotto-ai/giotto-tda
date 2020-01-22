46714cca1ba3dfe075c62e3b18ee12a56aab9223
julian <julian.burellaperez@heig-vd.ch>
2020-01-22 11:55:43 +0100

    Add python Interface for Subsampling
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>

--- gtda/externals/python/__init__.py
+++ gtda/externals/python/__init__.py
@@ -6,3 +6,5 @@ from .witness_complex_interface import WitnessComplex
 from .strong_witness_complex_interface import StrongWitnessComplex
 from .rips_complex_interface import RipsComplex, SparseRipsComplex
 from .cech_complex_interface import CechComplex
+from .subsampling_interface import choose_n_farthest_points,\
+    pick_n_random_points, sparsify_point_set
