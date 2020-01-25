ba3e0c9b5566e79bb011e6e619e609552c65393e
Umberto <u.lupo@l2f.ch>
2020-01-25 18:29:04 +0100

    More precise configuration details

--- examples/.nbconvert_conf.py
+++ examples/.nbconvert_conf.py
@@ -1,12 +1,14 @@
 import os
 
-c = get_config()
 ipynb_files = [
     f for f in os.listdir(".") if os.path.isfile(f) and f.endswith(".ipynb")]
 
+c = get_config()
 c.NbConvertApp.notebooks = ipynb_files
-c.NbConvertApp.execute = True
-c.NbConvertApp.export_format = 'notebook'
 c.NbConvertApp.ExecutePreprocessor.execute = True
+c.NbConvertApp.ExecutePreprocessor.export_format = 'notebook'
+c.NbConvertApp.FilesWriter.build_directory = ''
+c.NbConvertApp.ClearOutputPreprocessor.enabled = True
+c.NbConvertApp.ExecutePreprocessor.use_output_suffix = False
 c.NbConvertApp.ExecutePreprocessor.startup_timeout = 300
 c.NbConvertApp.ExecutePreprocessor.timeout = 600
