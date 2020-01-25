658ea709f06de493d0a26a6f4b58e5b5235ebf57
Umberto <u.lupo@l2f.ch>
2020-01-25 18:42:48 +0100

    Place equal sign after --config

--- examples/.nbconvert_conf.py
+++ examples/.nbconvert_conf.py
@@ -7,10 +7,7 @@ c = get_config()
 
 c.NbConvertApp.notebooks = ipynb_files
 
-c.NbConvertApp.FilesWriter.build_directory = ''
-c.NbConvertApp.ClearOutputPreprocessor.enabled = True
 c.NbConvertApp.ExecutePreprocessor.enabled = True
 c.NbConvertApp.ExecutePreprocessor.export_format = 'notebook'
-c.NbConvertApp.ExecutePreprocessor.use_output_suffix = False
 c.NbConvertApp.ExecutePreprocessor.startup_timeout = 300
 c.NbConvertApp.ExecutePreprocessor.timeout = 600
