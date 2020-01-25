61d559ca6070f6d7e60f9a392d99feaa92fe265c
Umberto <u.lupo@l2f.ch>
2020-01-25 17:49:05 +0100

    Try to fix use of config file for nbconvert

--- examples/.nbconvert_conf.py
+++ examples/.nbconvert_conf.py
@@ -3,4 +3,10 @@ import os
 c = get_config()
 ipynb_files = [
     f for f in os.listdir(".") if os.path.isfile(f) and f.endswith(".ipynb")]
+
 c.NbConvertApp.notebooks = ipynb_files
+c.NbConvertApp.execute = True
+c.NbConvertApp.export_format = 'notebook'
+c.NbConvertApp.ExecutePreprocessor.execute = True
+c.NbConvertApp.ExecutePreprocessor.startup_timeout = 300
+c.NbConvertApp.ExecutePreprocessor.timeout = 600
