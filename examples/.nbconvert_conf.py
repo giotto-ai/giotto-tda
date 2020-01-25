19d3f2ee632305369b4c5f9e457449cb0e754512
Umberto <u.lupo@l2f.ch>
2020-01-25 18:32:00 +0100

    Fix incorrect attribute

--- examples/.nbconvert_conf.py
+++ examples/.nbconvert_conf.py
@@ -4,11 +4,13 @@ ipynb_files = [
     f for f in os.listdir(".") if os.path.isfile(f) and f.endswith(".ipynb")]
 
 c = get_config()
+
 c.NbConvertApp.notebooks = ipynb_files
-c.NbConvertApp.ExecutePreprocessor.execute = True
-c.NbConvertApp.ExecutePreprocessor.export_format = 'notebook'
+
 c.NbConvertApp.FilesWriter.build_directory = ''
 c.NbConvertApp.ClearOutputPreprocessor.enabled = True
+c.NbConvertApp.ExecutePreprocessor.enabled = True
+c.NbConvertApp.ExecutePreprocessor.export_format = 'notebook'
 c.NbConvertApp.ExecutePreprocessor.use_output_suffix = False
 c.NbConvertApp.ExecutePreprocessor.startup_timeout = 300
 c.NbConvertApp.ExecutePreprocessor.timeout = 600
