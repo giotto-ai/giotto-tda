b52343e1d12685cfec17b33bc536258e42cd92de
Wojciech Reise <reisewojtus@gmail.com>
2020-01-27 11:21:56 +0100

    Raise test coverage for OneDCover and CubicalCover

--- gtda/mapper/tests/test_cover.py
+++ gtda/mapper/tests/test_cover.py
@@ -4,17 +4,50 @@ from hypothesis.extra.numpy import arrays, array_shapes
 from hypothesis.strategies import floats, integers, booleans, composite
 from numpy.testing import assert_almost_equal
 from functools import reduce
+import pytest
 
-from gtda.mapper.cover import OneDimensionalCover
+from sklearn.exceptions import NotFittedError
+from gtda.mapper.cover import OneDimensionalCover, CubicalCover
 
+
+@composite
+def get_filter(draw, shape=None):
+    if shape is None:
+        shape = array_shapes(min_dims=1, max_dims=1,
+                             min_side=4)
+    points = draw(arrays(dtype=np.float,
+                         elements=floats(allow_nan=False,
+                                         allow_infinity=False,
+                                         min_value=-1e10,
+                                         max_value=1e10),
+                         shape=shape, unique=True))
+    return points
+
+
+@composite
+def get_nb_intervals(draw):
+    nb_intervals = draw(integers(min_value=3, max_value=20))
+    return nb_intervals
+
+
+@composite
+def get_overlap_fraction(draw):
+    overlap =draw(floats(allow_nan=False,
+                        allow_infinity=False,
+                        min_value=0., exclude_min=True,
+                        max_value=1., exclude_max=True),
+                  )
+    return overlap
+
+
+@composite
+def get_kind(draw):
+    is_uniform = draw(booleans())
+    return 'uniform' if is_uniform else 'balanced'
 
 @given(
-    filter_values=arrays(
-        dtype=np.float,
-        elements=floats(min_value=-1e100, allow_nan=False,
-                        allow_infinity=False),
-        shape=integers(min_value=2, max_value=1e3)),
-    n_intervals=integers(min_value=1, max_value=100)
+    filter_values=get_filter(),
+    n_intervals=get_nb_intervals()
 )
 def test_one_dimensional_cover_shape(filter_values, n_intervals):
     # TODO: Extend to inputs with shape (n_samples, 1)
@@ -31,12 +64,7 @@ def test_one_dimensional_cover_shape(filter_values, n_intervals):
 
 
 @given(
-    filter_values=arrays(dtype=np.float,
-                         elements=floats(allow_nan=False,
-                                         allow_infinity=False,
-                                         max_value=1e3),
-                         shape=integers(min_value=1, max_value=100)
-                         )
+    filter_values=get_filter()
 )
 def test_filter_values_covered_by_single_interval(filter_values):
     # TODO: Extend to inputs with shape (n_samples, 1)
@@ -48,19 +76,9 @@ def test_filter_values_covered_by_single_interval(filter_values):
 
 
 @given(
-    filter_values=arrays(dtype=np.float,
-                         elements=floats(allow_nan=False,
-                                         allow_infinity=False,
-                                         min_value=-1e10,
-                                         max_value=1e10),
-                         shape=array_shapes(min_dims=1, max_dims=1,
-                                            min_side=2),
-                         unique=True),
-    n_intervals=integers(min_value=3, max_value=50),
-    overlap_frac=floats(allow_nan=False,
-                        allow_infinity=False,
-                        min_value=0,
-                        max_value=1)
+    filter_values=get_filter(),
+    n_intervals=get_nb_intervals(),
+    overlap_frac=get_overlap_fraction()
 )
 def test_equal_interval_length(filter_values, n_intervals, overlap_frac):
     cover = OneDimensionalCover(kind='uniform',
@@ -78,20 +96,14 @@ def test_equal_interval_length(filter_values, n_intervals, overlap_frac):
 
 
 @composite
-def get_intervals(draw):
-    nb_intervals = draw(integers(min_value=10, max_value=20))
+def get_input_tests_balanced(draw):
+    nb_intervals = draw(get_nb_intervals())
     nb_in_each_interval = draw(integers(min_value=2, max_value=5))
-    points = draw(arrays(dtype=np.float,
-                         elements=floats(allow_nan=False,
-                                         allow_infinity=False,
-                                         min_value=-1e10,
-                                         max_value=1e10),
-                         shape=(nb_in_each_interval * nb_intervals,),
-                         unique=True))
+    points = draw(get_filter(shape=(nb_in_each_interval * nb_intervals,)))
     return [points, nb_in_each_interval, nb_intervals]
 
 
-@given(input=get_intervals())
+@given(input=get_input_tests_balanced())
 def test_balanced_is_balanced(input):
     points, nb_in_each_interval, nb_intervals = input
     oneD_cover = OneDimensionalCover(kind='balanced',
@@ -105,14 +117,8 @@ def test_balanced_is_balanced(input):
 
 
 @given(
-    filter_values=arrays(dtype=np.float,
-                         elements=floats(allow_nan=False,
-                                         allow_infinity=False,
-                                         max_value=1e3),
-                         shape=array_shapes(min_dims=1, max_dims=1,
-                                            min_side=2),
-                         unique=True),
-    n_intervals=integers(min_value=1, max_value=10)
+    filter_values=get_filter(),
+    n_intervals=get_nb_intervals()
 )
 def test_filter_values_covered_by_interval_union(filter_values,
                                                  n_intervals):
@@ -128,25 +134,14 @@ def test_filter_values_covered_by_interval_union(filter_values,
 
 
 @given(
-    pts=arrays(dtype=np.float,
-               elements=floats(allow_nan=False,
-                               allow_infinity=False,
-                               min_value=-1e3,
-                               max_value=1e3),
-               shape=array_shapes(min_dims=1, max_dims=1,
-                                  min_side=5),
-               unique=True),
-    n_intervals=integers(min_value=3, max_value=10),
-    overlap_frac=floats(allow_nan=False,
-                        allow_infinity=False,
-                        min_value=0., exclude_min=True,
-                        max_value=1., exclude_max=True),
-    is_uniform=booleans()
+    pts=get_filter(),
+    n_intervals=get_nb_intervals(),
+    overlap_frac=get_overlap_fraction(),
+    kind=get_kind()
 )
 def test_fit_transform_against_fit_and_transform(pts, n_intervals,
-                                                 is_uniform, overlap_frac):
+                                                 kind, overlap_frac):
     """Fitting and transforming should give the same result as fit_transform"""
-    kind = 'uniform' if is_uniform else 'balanced'
     cover = OneDimensionalCover(n_intervals=n_intervals, kind=kind,
                                 overlap_frac=overlap_frac)
     x_fit_transf = cover.fit_transform(pts)
@@ -156,6 +151,50 @@ def test_fit_transform_against_fit_and_transform(pts, n_intervals,
     cover2 = cover2.fit(pts)
     x_fit_and_transf = cover2.transform(pts)
     assert_almost_equal(x_fit_transf, x_fit_and_transf)
+
+
+def test_fit_transform_limits_not_computed():
+    """We do not compute intervals when `kind`= `'balanced'`, unless fit is explicitly called."""
+    cover = OneDimensionalCover(n_intervals=10, kind='balanced',
+                                overlap_frac=0.3)
+    x = np.arange(0, 30)
+    _ = cover.fit_transform(x)
+    with pytest.raises(NotFittedError):
+        _ = cover.get_fitted_intervals()
+
+
+@given(filter=get_filter(),
+       kind=get_kind(),
+       n_intervals=get_nb_intervals(),
+       overlap_fraction=get_overlap_fraction(),
+)
+def test_cubical_fit_transform_consistent_with_OneD(filter, kind,
+                                                    n_intervals,
+                                                    overlap_fraction):
+    """Check that CubicalCover gives the same results as OneDimensionalCover,
+    on one-d data """
+    one_d = OneDimensionalCover(kind, n_intervals, overlap_fraction)
+    cubical = CubicalCover(kind, n_intervals, overlap_fraction)
+    x_one_d = one_d.fit_transform(filter)
+    x_cubical = cubical.fit_transform(filter)
+    assert_almost_equal(x_one_d, x_cubical)
+
+
+@given(filter=get_filter(),
+       kind=get_kind(),
+       n_intervals=get_nb_intervals(),
+       overlap_fraction=get_overlap_fraction(),
+       )
+def test_cubical_fit_A_transform_consistent_with_OneD(filter, kind,
+                                                      n_intervals,
+                                                      overlap_fraction):
+    """Check that CubicalCover gives the same results as OneDimensionalCover,
+    on one-d data """
+    one_d = OneDimensionalCover(kind, n_intervals, overlap_fraction)
+    cubical = CubicalCover(kind, n_intervals, overlap_fraction)
+    x_one_d = one_d.fit(filter).transform(filter)
+    x_cubical = cubical.fit(filter).transform(filter)
+    assert_almost_equal(x_one_d, x_cubical)
 
 # @given(
 #     filter_values=arrays(dtype=np.float,
