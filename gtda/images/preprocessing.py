70ab9e4926ef595771a04395952447d17c2f8510
Guillaume Tauzin <guillaume.tauzin@epfl.ch>
2020-02-05 14:25:36 +0100

    Fix docstring
    Signed-off-by: Guillaume Tauzin <guillaume.tauzin@epfl.ch>

--- gtda/images/preprocessing.py
+++ gtda/images/preprocessing.py
@@ -189,8 +189,8 @@ class Inverter(BaseEstimator, TransformerMixin):
         Xt : ndarray, shape (n_samples, n_pixels_x, n_pixels_y [, n_pixels_z])
             Transformed collection of images. Each entry along axis 0 is a
             2D or 3D binary image.
-        """
 
+        """
         check_is_fitted(self, ['_is_fitted'])
         Xt = check_array(X, ensure_2d=False, allow_nd=True, copy=True)
 
