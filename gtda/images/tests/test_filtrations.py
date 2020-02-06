433c49d628c87323be0a3c3f58806f71014a4fc7
Guillaume Tauzin <guillaume.tauzin@epfl.ch>
2020-02-06 16:58:31 +0100

    Add tests
    Signed-off-by: Guillaume Tauzin <guillaume.tauzin@epfl.ch>

--- gtda/images/tests/test_filtrations.py
+++ gtda/images/tests/test_filtrations.py
@@ -6,7 +6,7 @@ import pytest
 from numpy.testing import assert_almost_equal
 from sklearn.exceptions import NotFittedError
 
-from gtda.images import HeightFiltration
+from gtda.images import HeightFiltration, RadialFiltration
 
 images_2D = np.stack([np.ones((3, 4)),
                       np.concatenate([np.ones((3, 2)), np.zeros((3, 2))],
@@ -71,4 +71,59 @@ def test_height_transform(direction, images, expected):
     height = HeightFiltration(direction=direction)
 
     assert_almost_equal(height.fit_transform(images),
+                        expected)
+
+
+def test_radial_not_fitted():
+    radial = RadialFiltration()
+    with pytest.raises(NotFittedError):
+        radial.transform(images_2D)
+
+
+def test_radial_errors():
+    center = 'a'
+    radial = RadialFiltration(center=center)
+    with pytest.raises(TypeError):
+        radial.fit(images_2D)
+
+images_2D_radial = np.array(
+    [[[0., 0.70710678, 1.41421356, 2.12132034],
+      [0.70710678, 1.41421356, 2.12132034, 2.82842712],
+      [1.41421356, 2.12132034, 2.82842712, 3.53553391]],
+     [[0., 0.70710678, 4.53553391, 4.53553391],
+      [0.70710678, 1.41421356, 4.53553391, 4.53553391],
+    [1.41421356, 2.12132034, 4.53553391, 4.53553391]],
+     [[4.53553391, 4.53553391, 4.53553391, 4.53553391],
+      [4.53553391, 4.53553391, 4.53553391, 4.53553391],
+      [4.53553391, 4.53553391, 4.53553391, 4.53553391]]])
+
+
+images_3D_radial = np.array(
+    [[[[0. , 0.70710678], [0.70710678, 1.41421356],
+       [1.41421356, 2.12132034], [2.12132034, 2.82842712]],
+      [[0., 0.70710678], [0.70710678, 1.41421356],
+       [1.41421356, 2.12132034], [2.12132034, 2.82842712]],
+      [[0., 0.70710678], [0.70710678, 1.41421356],
+       [1.41421356, 2.12132034], [2.12132034, 2.82842712]]],
+     [[[0., 0.70710678], [0.70710678, 1.41421356],
+       [3.82842712, 3.82842712], [3.82842712, 3.82842712]],
+    [[0., 0.70710678], [0.70710678, 1.41421356],
+     [3.82842712, 3.82842712], [3.82842712, 3.82842712]],
+      [[0., 0.70710678], [0.70710678, 1.41421356],
+       [3.82842712, 3.82842712], [3.82842712, 3.82842712]]],
+     [[[3.82842712, 3.82842712], [3.82842712, 3.82842712],
+       [3.82842712, 3.82842712], [3.82842712, 3.82842712]],
+      [[3.82842712, 3.82842712], [3.82842712, 3.82842712],
+       [3.82842712, 3.82842712], [3.82842712, 3.82842712]],
+      [[3.82842712, 3.82842712], [3.82842712, 3.82842712],
+       [3.82842712, 3.82842712], [3.82842712, 3.82842712]]]])
+
+@pytest.mark.parametrize("center, images, expected",
+                         [(None, images_2D, images_2D_radial),
+                          ([0, 0], images_2D, images_2D_radial),
+                          ([1, 0, 1], images_3D, images_3D_radial)])
+def test_radial_transform(center, images, expected):
+    radial = RadialFiltration(center=center)
+
+    assert_almost_equal(radial.fit_transform(images),
                         expected)
