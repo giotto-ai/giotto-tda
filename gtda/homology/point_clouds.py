df6bb0c236217b6f052d998b68dbac58a51e6f9b
Guillaume Tauzin <guillaume.tauzin@epfl.ch>
2020-02-09 20:09:20 +0100

    Add EuclideanCechPersistence
    Signed-off-by: Guillaume Tauzin <guillaume.tauzin@epfl.ch>

--- gtda/homology/point_clouds.py
+++ gtda/homology/point_clouds.py
@@ -457,3 +457,197 @@ class SparseRipsPersistence(BaseEstimator, TransformerMixin):
         Xt = np.stack(Xt)
         Xt = np.nan_to_num(Xt, posinf=self.infinity_values_)
         return Xt
+
+
+@adapt_fit_transform_docs
+class EuclideanCechPersistence(BaseEstimator, TransformerMixin):
+    """`Persistence diagrams <https://giotto.ai/theory>`_ resulting from
+    `Cech filtrations <https://giotto.ai/theory>`_.
+    Given a `point cloud <https://giotto.ai/theory>`_ in Euclidean space,
+    information about the appearance and disappearance of topological
+    features (technically, `homology classes <https://giotto.ai/theory>`_) of
+    various dimensions and at different scales is summarised in the
+    corresponding persistence diagram.
+
+    Parameters
+    ----------
+    homology_dimensions : iterable, optional, default: ``(0, 1)``
+        Dimensions (non-negative integers) of the topological features to be
+        detected.
+
+    coeff : int prime, optional, default: ``2``
+        Compute homology with coefficients in the prime field
+        :math:`\\mathbb{F}_p = \\{ 0, \\ldots, p - 1 \\}` where
+        :math:`p` equals `coeff`.
+
+    max_edge_length : float, optional, default: ``numpy.inf``
+        Upper bound on the maximum value of the Vietoris-Rips filtration
+        parameter. Points whose distance is greater than this value will
+        never be connected by an edge, and topological features at scales
+        larger than this value will not be detected.
+
+    infinity_values : float or None, default : ``None``
+        Which death value to assign to features which are still alive at
+        filtration value `max_edge_length`. ``None`` has the same behaviour
+        as `max_edge_length`.
+
+    n_jobs : int or None, optional, default: ``None``
+        The number of jobs to use for the computation. ``None`` means 1 unless
+        in a :obj:`joblib.parallel_backend` context. ``-1`` means using all
+        processors.
+
+    Attributes
+    ----------
+    infinity_values_ : float
+        Effective death value to assign to features which are still alive at
+        filtration value `max_edge_length`.
+
+    See also
+    --------
+    VietorisRipsPersistence, ConsistentRescaling
+
+    Notes
+    -----
+    `GUDHI <https://github.com/GUDHI/gudhi-devel>`_ is used as a C++ backend
+    for computing Cubical persistent homology. Python bindings were modified
+    for performance.
+
+    Persistence diagrams produced by this class must be interpreted with
+    care due to the presence of padding triples which carry no information.
+    See :meth:`transform` for additional information.
+
+    References
+    ----------
+    [1] C. Maria, "Persistent Cohomology", 2020; `GUDHI User and Reference Manual \
+    <http://gudhi.gforge.inria.fr/doc/3.1.0/group__persistent_cohomology.html>`_.
+
+    """
+
+    _hyperparameters = {'max_edge_length': [numbers.Number],
+                        'infinity_values_': [numbers.Number],
+                        '_homology_dimensions': [list, [int, (0, np.inf)]],
+                        'coeff': [int, (2, np.inf)]}
+
+    def __init__(self, metric='euclidean', max_edge_length=np.inf,
+                 homology_dimensions=(0, 1), coeff=2,
+                 infinity_values=None, n_jobs=None):
+        self.metric = metric
+        self.homology_dimensions = homology_dimensions
+        self.coeff = coeff
+        self.max_edge_length = max_edge_length
+        self.infinity_values = infinity_values
+        self.n_jobs = n_jobs
+
+    def _gudhi_diagram(self, X):
+        cech_complex = CechComplex(
+            distance_points=Xdgms, max_radius=self.max_edge_length)
+        simplex_tree = sparse_rips_complex.create_simplex_tree(
+            max_dimension=max(self._homology_dimensions) + 1)
+        Xdgms = simplex_tree.persistence(
+            homology_coeff_field=self.coeff, min_persistence=0)
+
+        # Separate diagrams by homology dimensions
+        Xdgms = {dim: np.array([Xdgms[i][1] for i in range(len(Xdgms))
+                                if Xdgms[i][0] == dim]).reshape((-1, 2))
+                 for dim in self.homology_dimensions}
+
+        if 0 in self._homology_dimensions:
+            Xdgms[0] = Xdgms[0][1:, :]  # Remove final death at np.inf
+
+        # Add dimension as the third elements of each (b, d) tuple
+        Xdgms = {dim: np.hstack([Xdgms[dim],
+                                 dim * np.ones((Xdgms[dim].shape[0], 1),
+                                               dtype=Xdgms[dim].dtype)])
+                 for dim in self._homology_dimensions}
+        return Xdgms
+
+    def fit(self, X, y=None):
+        """Calculate `infinity_values_`. Then, return the estimator.
+
+        This method is here to implement the usual scikit-learn API and hence
+        work in pipelines.
+
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_points, n_dimensions)
+            Input data. Each entry along axis 0 is a point cloud of
+            ``n_points`` row vectors in ``n_dimensions``-dimensional space.
+
+        y : None
+            There is no need for a target in a transformer, yet the pipeline
+            API requires this parameter.
+
+        Returns
+        -------
+        self : object
+
+        """
+        if self.infinity_values is None:
+            self.infinity_values_ = self.max_edge_length
+        else:
+            self.infinity_values_ = self.infinity_values
+
+        self._homology_dimensions = sorted(self.homology_dimensions)
+
+        validate_params({**self.get_params(),
+                         'infinity_values_': self.infinity_values_,
+                         '_homology_dimensions': self._homology_dimensions},
+                        self._hyperparameters)
+        check_array(X, allow_nd=True)
+
+        self._max_homology_dimension = self._homology_dimensions[-1]
+        return self
+
+    def transform(self, X, y=None):
+        """For each point cloud in `X`, compute the relevant persistence
+        diagram as an array of triples [b, d, q]. Each triple represents a
+        persistent topological feature in dimension q (belonging to
+        `homology_dimensions`) which is born at b and dies at d. Only triples
+        in which b < d are meaningful. Triples in which b and d are equal
+        ("diagonal elements") may be artificially introduced during the
+        computation for padding purposes, since the number of non-trivial
+        persistent topological features is typically not constant across
+        samples. They carry no information and hence should be effectively
+        ignored by any further computation.
+
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_points, n_dimensions)
+            Input data. Each entry along axis 0 is a point cloud of
+            ``n_points`` row vectors in ``n_dimensions``-dimensional space.
+
+        y : None
+            There is no need for a target in a transformer, yet the pipeline
+            API requires this parameter.
+
+        Returns
+        -------
+        Xt : ndarray of shape (n_samples, n_features, 3)
+            Array of persistence diagrams computed from the feature arrays in
+            `X`. ``n_features`` equals :math:`\\sum_q n_q`, where :math:`n_q`
+            is the maximum number of topological features in dimension
+            :math:`q` across all samples in `X`.
+        """
+        check_is_fitted(self)
+        X = check_array(X, allow_nd=True)
+
+        Xt = Parallel(n_jobs=self.n_jobs)(
+            delayed(self._gudhi_diagram)(X[i, :, :]) for i in range(
+                X.shape[0]))
+
+        max_n_points = {
+            dim: max(1, np.max([Xt[i][dim].shape[0] for i in range(len(
+                Xt))])) for dim in self.homology_dimensions}
+        min_values = {
+            dim: min([np.min(Xt[i][dim][:, 0]) if Xt[i][dim].size else
+                      np.inf for i in range(len(Xt))]) for dim in
+            self.homology_dimensions}
+        min_values = {
+            dim: min_values[dim] if min_values[dim] != np.inf else 0 for dim
+            in self.homology_dimensions}
+        Xt = Parallel(n_jobs=self.n_jobs)(delayed(_pad_diagram)(
+            Xt[i], self._homology_dimensions, max_n_points, min_values)
+            for i in range(len(Xt)))
+        Xt = np.stack(Xt)
+        Xt = np.nan_to_num(Xt, posinf=self.infinity_values_)
+        return Xt
