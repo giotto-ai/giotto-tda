4f9c434a83d10b8c10bd61f344a8e9d5adccc078
Roman Yurchak <rth.yurchak@gmail.com>
2020-02-11 10:15:57 +0100

    Don't use interctive mode docker

--- .azure-ci/build_manylinux2010.sh
+++ .azure-ci/build_manylinux2010.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -it --rm -e python_ver=$PYTHON_VER \
+docker run -t --rm -e python_ver=$PYTHON_VER \
 	-v `pwd`:/io \
 	-v "${CCACHE_DIR}":/root/.ccache/  \
 	quay.io/pypa/manylinux2010_x86_64 \
