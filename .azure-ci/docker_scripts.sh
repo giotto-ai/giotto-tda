40e27524802e0d49638928bce09d916835a05684
Roman Yurchak <rth.yurchak@gmail.com>
2020-02-20 14:30:11 +0100

    More strict wheel / auditwheel versions

--- .azure-ci/docker_scripts.sh
+++ .azure-ci/docker_scripts.sh
@@ -46,7 +46,7 @@ pip uninstall -y giotto-tda
 pip uninstall -y giotto-tda-nightly
 
 # Build wheels
-pip install "wheel~=0.34.1" "auditwheel~=3.1"
+pip install wheel==0.34.1 auditwheel==3.1.0
 python setup.py sdist bdist_wheel
 
 # Repair wheels with auditwheel
