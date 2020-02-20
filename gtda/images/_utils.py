1020cb3019d5c16080530e9d5946b166106b2c18
Guillaume Tauzin <guillaume.tauzin@epfl.ch>
2020-02-20 12:48:12 +0100

    Use broadcasting in _dilate
    Signed-off-by: Guillaume Tauzin <guillaume.tauzin@epfl.ch>

--- gtda/images/_utils.py
+++ gtda/images/_utils.py
@@ -6,7 +6,7 @@ from scipy import ndimage as ndi
 
 
 def _dilate(Xd, min_iteration, max_iteration, min_value, max_value):
-    Xd = Xd.astype(float, copy=False)
+    Xd = Xd * 1.
     for iteration in range(min_iteration, min(max_iteration, max_value) + 1):
         Xtemp = np.asarray([ndi.binary_dilation(Xd[i])
                             for i in range(Xd.shape[0])])
