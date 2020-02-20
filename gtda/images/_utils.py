ba5bfdee76f27915ce716a374492368ee82387e4
Guillaume Tauzin <guillaume.tauzin@epfl.ch>
2020-02-20 12:37:17 +0100

    Use astype(float,..)
    Signed-off-by: Guillaume Tauzin <guillaume.tauzin@epfl.ch>

--- gtda/images/_utils.py
+++ gtda/images/_utils.py
@@ -6,7 +6,7 @@ from scipy import ndimage as ndi
 
 
 def _dilate(Xd, min_iteration, max_iteration, min_value, max_value):
-    Xd = Xd * 1.
+    Xd = Xd.astype(float, copy=False)
     for iteration in range(min_iteration, min(max_iteration, max_value) + 1):
         Xtemp = np.asarray([ndi.binary_dilation(Xd[i])
                             for i in range(Xd.shape[0])])
