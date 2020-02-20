d4e138c1a7014e961ed0e0011405a0a2f4a419e7
Roman Yurchak <rth.yurchak@gmail.com>
2020-02-20 18:15:33 +0100

    Run auditwheel repair for manylinux2010 wheels (#312)
    * Add auditwheel repair steps for manylinux2010 wheels
    
    * Fix wheel and auditwheel versions
    
    * Fixes in twine upload (remove commands now unnecessary)

--- .azure-ci/docker_scripts.sh
+++ .azure-ci/docker_scripts.sh
@@ -46,5 +46,10 @@ pip uninstall -y giotto-tda
 pip uninstall -y giotto-tda-nightly
 
 # Build wheels
-pip install wheel
+pip install wheel==0.34.1 auditwheel==3.1.0
 python setup.py bdist_wheel
+
+# Repair wheels with auditwheel
+auditwheel repair dist/*whl -w dist/
+# remove wheels that are not manylinux2010
+rm -rf dist/*-linux*.whl
