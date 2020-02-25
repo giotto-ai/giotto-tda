b9d37e72a26e85ad4f3b8e3af413a327a7756c27
Wojciech Reise <32167173+wreise@users.noreply.github.com>
2020-02-25 15:01:40 +0100

    Silhouette (#247)
    * Add Silhouette transformer and tests
    
    * Introduce silhouette-based distances and amplitudes
    
    * Improve docstrings for other transformers in features
    
    * Modify docstrings in distances
    
    * Use singleton lists instead of one-element slices
    
    * Fix == which should have been =, simplify code
    
    * Rename persistent_image to persistence_image in validation
    
    * Add the entry corresponding to Silhouettes to theory, and improve entry for persistence landscapes
    
    Co-authored-by: Umberto Lupo <46537483+ulupo@users.noreply.github.com>

--- gtda/diagrams/_utils.py
+++ gtda/diagrams/_utils.py
@@ -80,7 +80,7 @@ def _bin(X, metric, n_bins=100, **kw_args):
     max_vals = {dim: np.max(sub_diags[dim], axis=(0, 1))
                 for dim in homology_dimensions}
 
-    if metric in ['landscape', 'betti', 'heat']:
+    if metric in ['landscape', 'betti', 'heat', 'silhouette']:
         #  Taking the min(resp. max) of a tuple `m` amounts to extracting
         #  the birth (resp. death) value
         min_vals = {d: np.array(2*[np.min(m)]) for d, m in min_vals.items()}
@@ -89,7 +89,7 @@ def _bin(X, metric, n_bins=100, **kw_args):
     # Scales between axes should be kept the same, but not between dimension
     all_max_values = np.stack(list(max_vals.values()))
     if len(homology_dimensions) == 1:
-        all_max_values == all_max_values.reshape(1, -1)
+        all_max_values = all_max_values.reshape(1, -1)
     global_max_val = np.max(all_max_values, axis=0)
     max_vals = {dim: np.array([max_vals[dim][k] if
                                (max_vals[dim][k] != min_vals[dim][k])
@@ -103,9 +103,9 @@ def _bin(X, metric, n_bins=100, **kw_args):
                                                       max_vals[dim],
                                                       retstep=True,
                                                       num=n_bins)
-    if metric in ['landscape', 'betti', 'heat']:
+    if metric in ['landscape', 'betti', 'heat', 'silhouette']:
         for dim in homology_dimensions:
-            samplings[dim] = samplings[dim][:, 0, None, None]
+            samplings[dim] = samplings[dim][:, [0], None]
             step_sizes[dim] = step_sizes[dim][0]
     return samplings, step_sizes
 
