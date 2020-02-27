edac249bc369f64e02b0ffc9a45b5548cb7a2011
Umberto Lupo <46537483+ulupo@users.noreply.github.com>
2020-02-27 13:41:41 +0100

    Fix some grammar, use f-strings

--- gtda/utils/validation.py
+++ gtda/utils/validation.py
@@ -2,8 +2,8 @@
 # License: GNU AGPLv3
 
 import numbers
-import types
 import numpy as np
+import types
 
 available_metrics = {
     'bottleneck': [('delta', numbers.Number, (0., 1.))],
@@ -30,41 +30,40 @@ available_metric_params = {metric: [p[0] for p in param_lst]
 
 
 def check_diagram(X, copy=False):
-    """Input validation on a diagram
+    """Input validation on a persistence diagram.
+
     """
     if len(X.shape) != 3:
-        raise ValueError("X should be a 3d np.array: X.shape"
-                         " = {}".format(X.shape))
+        raise ValueError(f"X should be a 3d np.array: X.shape = {X.shape}.")
     if X.shape[2] != 3:
-        raise ValueError("X should be a 3d np.array with a 3rd dimension of"
-                         " 3 components: X.shape[2] = {}".format(X.shape[2]))
+        raise ValueError(f"X should be a 3d np.array with a 3rd dimension of "
+                         f"3 components: X.shape[2] = {X.shape[2]}.")
 
     homology_dimensions = sorted(list(set(X[0, :, 2])))
     for dim in homology_dimensions:
         if dim == np.inf:
             if len(homology_dimensions) != 1:
-                raise ValueError("np.inf is a valid homology dimension for a "
-                                 "stacked diagram but it should be the only "
-                                 "one: homology_dimensions "
-                                 "= {}".format(homology_dimensions))
+                raise ValueError(f"np.inf is a valid homology dimension for a "
+                                 f"stacked diagram but it should be the only "
+                                 f"one: homology_dimensions "
+                                 f"= {homology_dimensions}.")
         else:
             if dim != int(dim):
-                raise ValueError("All homology dimensions should be"
-                                 " integer valued: {} can't be casted"
-                                 " to an int of the same value.".format(dim))
+                raise ValueError(f"All homology dimensions should be "
+                                 f"integer valued: {dim} can't be cast "
+                                 f"to an int of the same value.")
             if dim != np.abs(dim):
-                raise ValueError("All homology dimensions should be"
-                                 " integer valued: {} can't be casted"
-                                 " to an int of the same value.".format(dim))
+                raise ValueError(f"All homology dimensions should be "
+                                 f"integer valued: {dim} can't be cast "
+                                 f"to an int of the same value.")
 
     n_points_above_diag = np.sum(X[:, :, 1] >= X[:, :, 0])
     n_points_global = X.shape[0] * X.shape[1]
     if n_points_above_diag != n_points_global:
-        raise ValueError("All points of all n_samples persistent diagrams "
-                         "should be above the diagonal, X[:,:,1] >= X[:,:,0]."
-                         " {} points in "
-                         "all n_samples diagrams are under the diagonal."
-                         "".format(n_points_global - n_points_above_diag))
+        raise ValueError(f"All points of all persistence diagrams should "
+                         f"be above the diagonal, X[:,:,1] >= X[:,:,0]. "
+                         f"{n_points_global - n_points_above_} points are "
+                         f"under the diagonal.")
     if copy:
         return np.copy(X)
     else:
@@ -97,10 +96,9 @@ def validate_params(parameters, references):
     for key in references.keys():
         # Check type
         if not isinstance(parameters[key], references[key][0]):
-            raise TypeError("Parameter {} is of type {}"
-                            " while it should be of type {}"
-                            "".format(key, type(parameters[key]),
-                                      references[key][0]))
+            raise TypeError(
+                f"Parameter {key} is of type {type(parameters[key])} while it "
+                f"should be of type {references[key][0]}.")
 
         # If the key is a list, tuple, or numpy array, check element by element
         if references[key][0] == list or references[key][0] == tuple or \
@@ -110,25 +108,22 @@ def validate_params(parameters, references):
                 # that has no decimals, else we check the type of the element
                 if references[key][1][0] == int:
                     if not isinstance(parameter, numbers.Number):
-                        raise TypeError("Parameter {} is a {} of {}"
-                                        " but contains an element of type {}"
-                                        "".format(key, type(parameters[key]),
-                                                  references[key][1][0],
-                                                  type(parameter)))
+                        raise TypeError(
+                            f"Parameter {key} is a {type(parameters[key])} of "
+                            f"{references[key][1][0]} but contains an "
+                            f"element of type {type(parameter)}.")
                     if not float(parameter).is_integer():
-                        raise TypeError("Parameter {} is a {} of int"
-                                        " but contains an element of type {}"
-                                        " that is not an integer."
-                                        "".format(key, type(parameters[key]),
-                                                  type(parameter)))
+                        raise TypeError(
+                            f"Parameter {key} is a {type(parameters[key])} of "
+                            f"int but contains an element of type "
+                            f"{type(parameter)} that is not an integer.")
                 else:
                     # Otherwise just check for the type
                     if not isinstance(parameter, references[key][1][0]):
-                        raise TypeError("Parameter {} is a {} of {}"
-                                        " but contains an element of type {}"
-                                        "".format(key, type(parameters[key]),
-                                                  references[key][1][0],
-                                                  type(parameter)))
+                        raise TypeError(
+                            "Parameter {key} is a {type(parameters[key])} of "
+                            "{references[key][1][0]}  but contains an "
+                            "element of type {type(parameter)}.")
 
                 # If there is no parameter range to check, continue
                 if references[key][1][1] is None:
@@ -137,21 +132,19 @@ def validate_params(parameters, references):
                 if isinstance(references[key][1][1], tuple):
                     if (parameter < references[key][1][1][0] or
                             parameter > references[key][1][1][1]):
-                        raise ValueError("Parameter {} is a {} containing {}"
-                                         "which should be in the range [{},{}]"
-                                         "".format(key, type(parameters[key]),
-                                                   parameter,
-                                                   references[key][1][1][0],
-                                                   references[key][1][1][1]))
+                        raise ValueError(
+                            f"Parameter {key} is a {type(parameters[key])} "
+                            f"containing {parameter} which should be in the "
+                            f"range [{references[key][1][1][0]}, "
+                            f"{references[key][1][1][1]}].")
                 # Check if element is in a list
                 elif isinstance(references[key][1][1], list):
                     if parameter not in references[key][1][1]:
-                        raise ValueError("Parameter {} is a {} containing {},"
-                                         " while it should only contain one of"
-                                         " the following {}"
-                                         "".format(key, type(parameters[key]),
-                                                   parameter,
-                                                   references[key][1][1]))
+                        raise ValueError(
+                            f"Parameter {key} is a {type(parameters[key])} "
+                            f"containing {parameter}, while it should only "
+                            f"contain one of the following: "
+                            f"{references[key][1][1]}.")
         else:
             # If only the type should be checked, continue
             if references[key][1] is None:
@@ -161,46 +154,43 @@ def validate_params(parameters, references):
             if isinstance(references[key][1], tuple):
                 if (parameters[key] < references[key][1][0] or
                         parameters[key] > references[key][1][1]):
-                    raise ValueError("Parameter {} is {}, while it"
-                                     " should be in the range [{}, {}]"
-                                     "".format(key, parameters[key],
-                                               references[key][1][0],
-                                               references[key][1][1]))
+                    raise ValueError(
+                        f"Parameter {key} is {parameters[key]}, while it  "
+                        f"should be in the range [{references[key][1][0]}, "
+                        f"{references[key][1][1]}].")
             # Check if parameter is in a list
             elif isinstance(references[key][1], list):
                 if parameters[key] not in references[key][1]:
-                    raise ValueError("Parameter {} is {}, while it"
-                                     " should be one of the following {}"
-                                     "".format(key, parameters[key],
-                                               references[key][1]))
+                    raise ValueError(
+                        f"Parameter {key} is {parameters[key]}, while it "
+                        f"should be one of the following: "
+                        f"{references[key][1]}.")
 
 
 def validate_metric_params(metric, metric_params):
     if metric not in available_metrics.keys():
-        raise ValueError("No metric called {}."
-                         " Available metrics are {}."
-                         "".format(metric,
-                                   list(available_metrics.keys())))
+        raise ValueError(
+            f"No metric called {metric}. Available metrics are "
+            f"{list(available_metrics.keys())}.")
 
     for (param, param_type, param_values) in available_metrics[metric]:
         if param in metric_params.keys():
             input_param = metric_params[param]
             if not isinstance(input_param, param_type):
-                raise TypeError("{} in params_metric is of type {}"
-                                " but must be an {}."
-                                "".format(param, type(input_param),
-                                          param_type))
+                raise TypeError(
+                    f"{param} in params_metric is of type {type(input_param)} "
+                    f" but must be an {param_type}.")
             if param_values is not None:
                 if input_param < param_values[0] or \
                    input_param > param_values[1]:
-                    raise ValueError("{} in param_metric should be between {} "
-                                     "and {} but has been set to {}."
-                                     "".format(param, param_values[0],
-                                               param_values[1], input_param))
+                    raise ValueError(
+                        f"{param} in param_metric should be between "
+                        f"{param_values[0]} and {param_values[1]} but has "
+                        f"been set to {input_param}.")
 
     for param in metric_params.keys():
         if param not in available_metric_params[metric]:
-            raise ValueError("{} in metric_param is not an available"
-                             " parameter. Available metric_params"
-                             " are {}".format(param,
-                                              available_metric_params[metric]))
+            raise ValueError(
+                f"{param} in metric_param is not an available parameter. "
+                f"Available metric_params are: "
+                f"{available_metric_params[metric]}.")
