1f5b397df1c644c0f5ec6be56e2745093f2bb251
Umberto <u.lupo@l2f.ch>
2020-03-06 15:33:47 +0100

    Fix ``ref_of`` which should have been ``ref_in``

--- gtda/utils/validation.py
+++ gtda/utils/validation.py
@@ -123,7 +123,7 @@ def validate_params(parameters, references, exclude=None):
         - ``'type'``, mapping to a class or tuple of classes. ``parameter``
           is checked to be an instance of this class or tuple of classes.
 
-        - ``'in'``, mapping to a dictionary ``ref_of``, when the value of
+        - ``'in'``, mapping to a dictionary ``ref_in``, when the value of
           ``'type'`` is not one of ``list``, ``tuple``, ``numpy.ndarray`` or
           ``dict``. The following check is performed: ``parameter in ref_in``.
 
