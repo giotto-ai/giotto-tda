1b17ab6bdc88b4450769e9ac3779234afe6cb8ea
julian <julian.burellaperez@heig-vd.ch>
2020-03-18 11:51:05 +0100

    Fix issue when compiling on Windows due to ssize_t is POSIX only
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>

--- gtda/externals/bindings/bottleneck_bindings.cpp
+++ gtda/externals/bindings/bottleneck_bindings.cpp
@@ -4,6 +4,15 @@
  * License:          Apache 2.0
  *****************************************************************************/
 
+/* ssize_t is not standard C, it is a typedef from Posix.
+ * Following solution is copy/pasted from solution found in
+ * https://stackoverflow.com/a/35368387
+ */
+#if defined(_MSC_VER)
+#include <BaseTsd.h>
+typedef SSIZE_T ssize_t;
+#endif
+
 #include <bottleneck/include/bottleneck.h>
 
 // PYBIND11
