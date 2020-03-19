38c32d882c0780f29dea147c5e42eeea851408e4
Wojciech Reise <reisewojtus@gmail.com>
2020-03-19 15:17:13 +0100

    One bibtex entry in images

--- gtda/images/preprocessing.py
+++ gtda/images/preprocessing.py
@@ -179,10 +179,10 @@ class Inverter(BaseEstimator, TransformerMixin, PlotterMixin):
 
     References
     ----------
-    .. [1] A. Garin and G. Tauzin, "A topological reading lesson: \
-           Classification  of MNIST  using  TDA"; 19th International \
-           IEEE Conference on Machine Learning and Applications (ICMLA 2020), \
-           2019; arXiv: `1910.08345 <https://arxiv.org/abs/1910.08345>`_.
+    [1] A. Garin and G. Tauzin, "A topological reading lesson: \
+        Classification  of MNIST  using  TDA"; 19th International \
+        IEEE Conference on Machine Learning and Applications (ICMLA 2020), \
+        2019; arXiv: `1910.08345 <https://arxiv.org/abs/1910.08345>`_.
 
     """
 
