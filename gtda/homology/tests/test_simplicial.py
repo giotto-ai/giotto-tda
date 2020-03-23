70cc5c184b23165a9d9ca68c5b1a3f482631b0be
Umberto Lupo <46537483+ulupo@users.noreply.github.com>
2020-03-23 13:29:10 +0100

    Fix tests syntax

--- gtda/homology/tests/test_simplicial.py
+++ gtda/homology/tests/test_simplicial.py
@@ -105,7 +105,10 @@ def test_vrp_list_of_arrays():
     pc_2 = np.array([[0, 1], [1, 2]])
     pc_list = [pc[0].copy(), pc_2]
     vrp = VietorisRipsPersistence()
-    vrp.fit(pc_list)def test_fp_params():
+    vrp.fit(pc_list)
+
+    
+def test_fp_params():
     coeff = 'not_defined'
     fp = FlagserPersistence(coeff=coeff)
 
