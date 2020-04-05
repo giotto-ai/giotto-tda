9bab872352cbbfd046425163c589c94e9ad37628
Umberto <u.lupo@l2f.ch>
2020-04-05 17:20:38 +0200

    Refactoring of check_point_clouds
    - Rename distance_matrix kwarg to distance_matrices
    - Add checks for square input when distance_matrices is set to True
    - More comprehensive/friendly error messages and warnings

--- gtda/utils/validation.py
+++ gtda/utils/validation.py
@@ -6,7 +6,9 @@ from operator import and_
 from warnings import warn
 
 import numpy as np
+
 from sklearn.utils.validation import check_array
+from sklearn.exceptions import DataDimensionalityWarning
 
 
 def check_diagrams(X, copy=False):
@@ -186,9 +188,23 @@ def validate_params(parameters, references, exclude=None):
     return _validate_params(parameters_, references)
 
 
-def check_point_clouds(X, distance_matrix=False, **kwargs):
-    """Input validation on an array or list representing a collection of point
-    clouds or distance matrices.
+def _check_array_mod(X, **kwargs):
+    """Modified version of :func:`~sklearn.utils.validation.check_array. When
+    keyword parameter `force_all_finite` is set to False, NaNs are not
+    accepted but infinity is."""
+    if not kwargs['force_all_finite']:
+        Xnew = check_array(X, **kwargs)
+        if np.isnan(Xnew).any():
+            raise ValueError(
+                "Input contains NaN. Only finite values and infinity are "
+                "allowed when parameter `force_all_finite` is False.")
+        return Xnew
+    return check_array(X, **kwargs)
+
+
+def check_point_clouds(X, distance_matrices=False, **kwargs):
+    """Input validation on arrays or lists representing collections of point
+    clouds or of distance/adjacency matrices.
 
     The input is checked to be either a single 3D array using a single call
     to :func:`~sklearn.utils.validation.check_array`, or a list of 2D arrays by
@@ -204,14 +220,22 @@ def check_point_clouds(X, distance_matrix=False, **kwargs):
     X : object
         Input object to check / convert.
 
-    distance_matrix : bool, optional, default: ``False``
+    distance_matrices : bool, optional, default: ``False``
         Whether the input represents a collection of distance matrices or of
         concrete point clouds in Euclidean space. In the first case, entries
         are allowed to be infinite unless otherwise specified in `kwargs`.
 
     kwargs
         Keyword arguments accepted by
-        :func:`~gtda.utils.validation.check_list_of_arrays`.
+        :func:`~sklearn.utils.validation.check_array`, with the following
+        caveats: 1) `ensure_2d` and `allow_nd` are ignored; 2) if not passed
+        explicitly, `force_all_finite` is set to be the boolean negation of
+        `distance_matrices`; 3) when `force_all_finite` is set to ``False``,
+        NaN inputs are not allowed; 4) `accept_sparse` and
+        `accept_large_sparse` are only meaningful in the case of lists of 2D
+        arrays, in which case they are passed to individual instances of
+        :func:`~sklearn.utils.validation.check_array` validating each entry
+        in the list.
 
     Returns
     -------
@@ -219,30 +243,67 @@ def check_point_clouds(X, distance_matrix=False, **kwargs):
         The converted and validated object.
 
     """
-    kwargs_ = {'force_all_finite': not distance_matrix}
+    kwargs_ = {'force_all_finite': not distance_matrices}
     kwargs_.update(kwargs)
-    if hasattr(X, 'shape'):
+    kwargs_.pop('allow_nd', None)
+    kwargs_.pop('ensure_2d', None)
+    if hasattr(X, 'shape') and hasattr(X, 'ndim'):
         if X.ndim != 3:
-            raise ValueError("ndarray input must be 3D.")
-        return check_array(X, allow_nd=True, **kwargs_)
+            if X.ndim == 2:
+                extra_2D = \
+                    "\nReshape your input X using X.reshape(1, *X.shape) or " \
+                    "X[None, :, :] if X is a single point cloud/distance " \
+                    "matrix/adjacency matrix of a weighted graph."
+            else:
+                extra_2D = ""
+            raise ValueError(
+                f"Input must be a single 3D array or a list of 2D arrays. "
+                f"Array of dimension {X.ndim} passed." + extra_2D)
+        if (X.shape[1] != X.shape[2]) and distance_matrices:
+            raise ValueError(
+                f"Input array X must have X.shape[1] == X.shape[2]: "
+                f"{X.shape[1]} != {X.shape[2]} passed.")
+        elif (X.shape[1] == X.shape[2]) and not distance_matrices:
+            warn(
+                "Input array X has X.shape[1] == X.shape[2]. This is "
+                "consistent with a collection of distance/adjacency "
+                "matrices, but the input is being treated as a collection "
+                "of vectors in Euclidean space.",
+                DataDimensionalityWarning, stacklevel=2)
+        Xnew = _check_array_mod(X, **kwargs_, allow_nd=True)
     else:
-        if not distance_matrix:
-            reference = X[0].shape[1]  # Embedding dimension of first sample
-            if not reduce(
-                    and_, (x.shape[1] == reference for x in X[1:]), True):
-                warn("Not all point clouds have the same embedding dimension.")
+        has_check_failed = False
+        messages = []
+        Xnew = []
+        for i, x in enumerate(X):
+            try:
+                xnew = _check_array_mod(x, **kwargs_, ensure_2d=True)
+                if distance_matrices:
+                    if not x.shape[0] == x.shape[1]:
+                        raise ValueError(
+                            f"All arrays must be square: {x.shape[0]} rows "
+                            f"and {x.shape[1]} columns found in this array.")
+                Xnew.append(xnew)
+            except ValueError as e:
+                has_check_failed = True
+                messages.append(f"Entry {i}:\n{e}")
+        if has_check_failed:
+            raise ValueError(
+                "The following errors were raised by the inputs:\n\n" +
+                "\n\n".join(messages))
 
-    has_check_failed = False
-    messages = []
-    Xnew = []
-    for i, x in enumerate(X):
-        try:
-            Xnew.append(check_array(x, **kwargs_))
-            messages = ['']
-        except ValueError as e:
-            has_check_failed = True
-            messages.append(str(e))
-    if has_check_failed:
-        raise ValueError("The following errors were raised by the inputs: \n"
-                         "\n".join(messages))
+        if not distance_matrices:
+            if reduce(and_, (x.shape[0] == x.shape[1] for x in X), True):
+                warn(
+                    "All arrays are square. This is consistent with a "
+                    "collection of distance/adjacency matrices, but the input "
+                    "is being treated as a collection of vectors in Euclidean "
+                    "space.", DataDimensionalityWarning, stacklevel=2)
+
+            ref_dim = X[0].shape[1]  # Embedding dimension of first sample
+            if not reduce(and_, (x.shape[1] == ref_dim for x in X[1:]), True):
+                warn(
+                    "Not all point clouds have the same embedding dimension.",
+                    DataDimensionalityWarning, stacklevel=2)
+
     return Xnew
