488fdef3de397ff875b0ee7d52890ce66f37c1b9
julian <julian.burellaperez@heig-vd.ch>
2020-04-07 11:52:21 +0200

    Fix BOOST_ROOT unset in azure-pipelines for windows
    see https://github.com/actions/virtual-environments/commit/e91f2138c0b7304831e9aaa43ea47279d4160ef4\#diff-55dcabaaca18e802a555dfb957187fbeL474
    
    Signed-off-by: julian <julian.burellaperez@heig-vd.ch>

--- cmake/HelperBoost.cmake
+++ cmake/HelperBoost.cmake
@@ -4,6 +4,9 @@
 # The custom path will be at `C:\\local\`
 if(WIN32)
     list(APPEND BOOST_ROOT "C:/local")
+    # azure-pipelines maintainers removed BOOST_ROOT
+    # see https://github.com/actions/virtual-environments/commit/e91f2138c0b7304831e9aaa43ea47279d4160ef4#diff-55dcabaaca18e802a555dfb957187fbeL474
+    list(APPEND BOOST_ROOT ${BOOST_ROOT_1_72_0})
     list(APPEND BOOST_ROOT "") # Add custom path to your boost installation
 endif()
 
