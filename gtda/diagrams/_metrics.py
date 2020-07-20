b0e0c0783cfc85393ca519c787ce2720a281be2e
Umberto Lupo <umberto.lupo@gmail.com>
2020-07-20 23:50:11 +0200

    Better place comment

--- gtda/diagrams/_metrics.py
+++ gtda/diagrams/_metrics.py
@@ -75,7 +75,7 @@ def landscapes(diagrams, sampling, n_layers):
 
 
 def _heat(image, sampled_diag, sigma):
-    _sample_image(image, sampled_diag)  # modifies `heat` inplace
+    _sample_image(image, sampled_diag)
     image[:] = gaussian_filter(image, sigma, mode="reflect")
 
 
--- gtda/diagrams/_utils.py
+++ gtda/diagrams/_utils.py
@@ -29,6 +29,7 @@ def _sort(Xs):
 
 
 def _sample_image(image, sampled_diag):
+    # NOTE: Modifies `image` in-place
     unique, counts = np.unique(sampled_diag, axis=0, return_counts=True)
     unique = tuple(tuple(row) for row in unique.astype(np.int).T)
     image[unique] = counts
