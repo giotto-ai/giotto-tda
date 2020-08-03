8657f783e98b22b4e6290c2b0f2b756fcd9860d3
Umberto Lupo <umberto.lupo@gmail.com>
2020-08-03 18:54:45 +0200

    Improve coverage of mapper visualization modules

--- gtda/mapper/tests/test_visualization.py
+++ gtda/mapper/tests/test_visualization.py
@@ -1,13 +1,14 @@
-import plotly.io as pio
-import numpy as np
 import warnings
-
 from unittest import TestCase
 
+import numpy as np
+import plotly.io as pio
+import pytest
+
+from gtda.mapper import FirstSimpleGap
 from gtda.mapper import make_mapper_pipeline
 from gtda.mapper import (plot_interactive_mapper_graph,
                          plot_static_mapper_graph)
-from gtda.mapper import FirstSimpleGap
 
 
 class TestCaseNoTemplate(TestCase):
@@ -42,6 +43,35 @@ colors = np.array([8., 8., 3., 8., 0., 8., 8., 8., 5.,
                    8., 8., 8., 8., 4., 2., 8., 1., 8., 2., 8.])
 
 
+@pytest.mark.parametrize("layout_dim", [2, 3])
+def test_valid_layout_dim(layout_dim):
+    pipe = make_mapper_pipeline()
+    fig = plot_static_mapper_graph(pipe, X, layout_dim=layout_dim)
+    edge_trace = fig.get_state()['_data'][0]
+    assert 'x' in edge_trace.keys()
+    assert 'y' in edge_trace.keys()
+    is_z_present = 'z' in edge_trace.keys()
+    assert is_z_present if layout_dim == 3 else not is_z_present
+
+
+@pytest.mark.parametrize("layout_dim", [1, 4])
+def test_invalid_layout_dim(layout_dim):
+    with pytest.raises(ValueError):
+        pipe = make_mapper_pipeline()
+        _ = plot_static_mapper_graph(pipe, X, layout_dim=layout_dim)
+
+
+def test_invalid_layout_algorithm():
+    with pytest.raises(KeyError):
+        pipe = make_mapper_pipeline()
+        _ = plot_static_mapper_graph(pipe, X, layout="inexistent_layout")
+
+
+def _get_size_from_hovertext(s):
+    size_str = s.split("<br>")[3].split(": ")[1]
+    return int(size_str)
+
+
 class TestStaticPlot(TestCaseNoTemplate):
 
     def test_is_data_present(self):
@@ -64,21 +94,18 @@ class TestStaticPlot(TestCaseNoTemplate):
         assert len(fig_colors) == num_nodes
 
 
+def _get_widget_by_trait(fig, key, val=None):
+    for k, v in fig.widgets.items():
+        try:
+            b = getattr(v, key) == val if val is not None \
+                else getattr(v, key)
+            if b:
+                return fig.widgets[k]
+        except (AttributeError, TypeError):
+            pass
+
+
 class TestInteractivePlot(TestCaseNoTemplate):
-
-    def _get_widget_by_trait(self, fig, key, val=None):
-        for k, v in fig.widgets.items():
-            try:
-                b = getattr(v, key) == val if val is not None \
-                    else getattr(v, key)
-                if b:
-                    return fig.widgets[k]
-            except (AttributeError, TypeError):
-                pass
-
-    def _get_size_from_hovertext(self, s):
-        size_str = s.split("<br>")[3].split(": ")[1]
-        return int(size_str)
 
     def test_cluster_sizes(self):
         """Verify that the total number of calculated clusters is equal to
@@ -86,11 +113,12 @@ class TestInteractivePlot(TestCaseNoTemplate):
         pipe = make_mapper_pipeline(clusterer=FirstSimpleGap())
         warnings.simplefilter("ignore")
         fig = plot_interactive_mapper_graph(pipe, X)
-        w_scatter = self._get_widget_by_trait(fig, 'data')
+        w_scatter = _get_widget_by_trait(fig, 'data')
 
-        node_sizes_vis = [self._get_size_from_hovertext(s_)
-                          for s_ in w_scatter.get_state()
-                          ['_data'][1]['hovertext']]
+        node_sizes_vis = [
+            _get_size_from_hovertext(s_)
+            for s_ in w_scatter.get_state()['_data'][1]['hovertext']
+        ]
 
         g = pipe.fit_transform(X)
         node_size_real = [len(node) for node in g.vs['node_elements']]
