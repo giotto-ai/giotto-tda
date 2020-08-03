63eb0c4b07ed55f6ca420f7a66d9938eb387b2d5
Umberto Lupo <umberto.lupo@gmail.com>
2020-08-04 00:19:15 +0200

    Improve test coverage of mapper visualization modules

--- gtda/mapper/tests/test_visualization.py
+++ gtda/mapper/tests/test_visualization.py
@@ -19,40 +19,21 @@ class TestCaseNoTemplate(TestCase):
         pio.templates.default = "plotly"
 
 
-X = np.array([[-19.33965799, -284.58638371],
-              [-290.25710696,  184.31095197],
-              [250.38108853,  134.5112574],
-              [-259.46357187, -172.12937543],
-              [115.72180479,  -69.67624071],
-              [120.12187185,  248.39783826],
-              [234.08476944,  115.54743986],
-              [246.68634685,  119.170029],
-              [-154.27214561, -272.07656956],
-              [225.37435664,  186.3253872],
-              [54.17543392,   76.4066916],
-              [175.28163213, -193.46279193],
-              [228.63910018, -121.16687597],
-              [-101.58902866,   48.86471748],
-              [-185.23421146,  244.14414753],
-              [-275.05799067, -204.99265911],
-              [-170.12180583,  176.10258455],
-              [-155.54055842, -214.420498],
-              [184.6940872,    2.08810678],
-              [-184.42012962,   28.8978038]])
+N = 50
+d = 3
+X = np.random.randn(N, d)
+colors = np.random.randint(0, 10, N)
 
-colors = np.array([8., 8., 3., 8., 0., 8., 8., 8., 5.,
-                   8., 8., 8., 8., 4., 2., 8., 1., 8., 2., 8.])
-
-viridis_colorscale = [[0.0, '#440154'],
-                      [0.1111111111111111, '#482878'],
-                      [0.2222222222222222, '#3e4989'],
-                      [0.3333333333333333, '#31688e'],
-                      [0.4444444444444444, '#26828e'],
-                      [0.5555555555555556, '#1f9e89'],
-                      [0.6666666666666666, '#35b779'],
-                      [0.7777777777777778, '#6ece58'],
-                      [0.8888888888888888, '#b5de2b'],
-                      [1.0, '#fde725']]
+viridis_colorscale = ((0.0, '#440154'),
+                      (0.1111111111111111, '#482878'),
+                      (0.2222222222222222, '#3e4989'),
+                      (0.3333333333333333, '#31688e'),
+                      (0.4444444444444444, '#26828e'),
+                      (0.5555555555555556, '#1f9e89'),
+                      (0.6666666666666666, '#35b779'),
+                      (0.7777777777777778, '#6ece58'),
+                      (0.8888888888888888, '#b5de2b'),
+                      (1.0, '#fde725'))
 
 hsl_colorscale = ['hsl(19.0, 96.0%, 67.0%)',
                   'hsl(60.0, 100.0%, 87.0%)',
@@ -63,10 +44,9 @@ hsl_colorscale = ['hsl(19.0, 96.0%, 67.0%)',
 def test_valid_layout_dim(layout_dim):
     pipe = make_mapper_pipeline()
     fig = plot_static_mapper_graph(pipe, X, layout_dim=layout_dim)
-    edge_trace = fig.get_state()['_data'][0]
-    assert 'x' in edge_trace.keys()
-    assert 'y' in edge_trace.keys()
-    is_z_present = 'z' in edge_trace.keys()
+    edge_trace = fig.data[0]
+    assert hasattr(edge_trace, "x") and hasattr(edge_trace, "y")
+    is_z_present = hasattr(edge_trace, "z")
     assert is_z_present if layout_dim == 3 else not is_z_present
 
 
@@ -90,7 +70,7 @@ def test_valid_hoverlabel_bgcolor(layout_dim):
         pipe, X, layout_dim=layout_dim,
         plotly_params={"node_trace": {"hoverlabel_bgcolor": "white"}}
     )
-    assert fig.get_state()["_data"][1]["hoverlabel"]["bgcolor"] == "white"
+    assert fig.data[1]["hoverlabel"]["bgcolor"] == "white"
 
 
 def test_unsuitable_colorscale_for_hoverlabel_3d():
@@ -105,7 +85,7 @@ def test_unsuitable_colorscale_for_hoverlabel_3d():
 def test_valid_colorscale():
     pipe = make_mapper_pipeline()
 
-    fig = plot_static_mapper_graph(
+    fig_2d = plot_static_mapper_graph(
         pipe, X, layout_dim=2,
         plotly_params={"node_trace": {"marker_colorscale": "blues"}}
     )
@@ -115,18 +95,51 @@ def test_valid_colorscale():
     )
 
     # Test that the custom colorscale is correctly applied both in 2d and in 3d
-    marker_colorscale = fig.get_state()["_data"][1]["marker"]["colorscale"]
-    marker_colorscale_3d = \
-        fig_3d.get_state()["_data"][1]["marker"]["colorscale"]
+    marker_colorscale = fig_2d.data[1]["marker"]["colorscale"]
+    marker_colorscale_3d = fig_3d.data[1]["marker"]["colorscale"]
     assert marker_colorscale == marker_colorscale_3d
 
     # Test that the default colorscale is "viridis" and that the custom one is
     # different
     fig_default = plot_static_mapper_graph(pipe, X)
     marker_colorscale_default = \
-        fig_default.get_state()["_data"][1]["marker"]["colorscale"]
+        fig_default.data[1]["marker"]["colorscale"]
     assert marker_colorscale_default == viridis_colorscale
     assert marker_colorscale != marker_colorscale_default
+
+
+@pytest.mark.parametrize("color_variable", [None, colors])
+@pytest.mark.parametrize("node_color_statistic", [None, np.max])
+def test_colors_same_2d_3d(color_variable, node_color_statistic):
+    pipe = make_mapper_pipeline()
+    fig_2d = plot_static_mapper_graph(
+        pipe, X, layout_dim=2, color_variable=color_variable,
+        node_color_statistic=node_color_statistic
+    )
+    fig_3d = plot_static_mapper_graph(
+        pipe, X, layout_dim=3, color_variable=color_variable,
+        node_color_statistic=node_color_statistic
+    )
+    assert fig_2d.data[1].marker.color == fig_3d.data[1].marker.color
+
+
+@pytest.mark.parametrize("layout_dim", [2, 3])
+def test_color_by_column_dropdown_2d(layout_dim):
+    pipe = make_mapper_pipeline()
+    fig = plot_static_mapper_graph(
+        pipe, X, layout_dim=layout_dim, color_by_columns_dropdown=True
+    )
+    fig_buttons = fig.layout.updatemenus[0].buttons
+
+    assert list(fig.data[1].marker.color) == \
+           list(fig_buttons[0].args[0]["marker.color"][1])
+
+    for i in range(X.shape[1]):
+        fig_col_i = plot_static_mapper_graph(
+            pipe, X, layout_dim=layout_dim, color_variable=i
+        )
+        assert list(fig_col_i.data[1].marker.color) == \
+            list(fig_buttons[i + 1].args[0]["marker.color"][1])
 
 
 def _get_size_from_hovertext(s):
@@ -144,15 +157,15 @@ class TestStaticPlot(TestCaseNoTemplate):
         fig = plot_static_mapper_graph(pipe, X,
                                        color_variable=colors,
                                        clone_pipeline=False)
-        node_trace_x = fig.get_state()['_data'][1]["x"]
-        node_trace_y = fig.get_state()['_data'][1]["y"]
+        node_trace_x = fig.data[1].x
+        node_trace_y = fig.data[1].y
 
-        assert node_trace_x["shape"][0] == node_trace_y["shape"][0]
+        assert node_trace_x.shape[0] == node_trace_y.shape[0]
 
-        num_nodes = node_trace_x["shape"][0]
+        num_nodes = node_trace_x.shape[0]
         assert len(X) >= num_nodes
 
-        fig_colors = fig.get_state()['_data'][1]['marker']['color']
+        fig_colors = fig.data[1].marker.color
         assert len(fig_colors) == num_nodes
 
 
@@ -179,7 +192,7 @@ class TestInteractivePlot(TestCaseNoTemplate):
 
         node_sizes_vis = [
             _get_size_from_hovertext(s_)
-            for s_ in w_scatter.get_state()['_data'][1]['hovertext']
+            for s_ in w_scatter.data[1].hovertext
         ]
 
         g = pipe.fit_transform(X)
