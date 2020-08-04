giotto-ai/giotto-tda

commit 4bc90b2e88926dbf2b4da2a455e7480485303d64

Refactor igraph.Graph output of Nerve, modify `fit` behaviour of Nerve, add `store_edge_elements` kwarg to Nerve and make_mapper_pipeline, add Nerve and ParallelClustering to docs (#447)
* Refactor igraph.Graph output, add Nerve and ParallelClustering to docs, add store_edge_elements kwarg to Nerve and make_mapper_pipeline

- Store node metadata not as a graph-level dictionary, but as vertex attributes accessible by graph.vs[attr_name][node_id] or graph.vs[node_id][attr_name] for attr_name in ["pullback_set_label", "partial_cluster_label", "node_elements"]
- Remove "node_id" from node attributes as it always coincided with the igraph.Graph node number anyway.
- Automatically store sizes of intersections as edge weights, accessible by graph.es["weight"].
- Add "store_intersections" kwarg to Nerve and make_mapper_pipeline to allow storing indices of node intersections as edge attributes, accessible via graph.es["edge_elements"].
- Simplify logic of Nerve.fit_transform code
- Change the attributes stored by Nerve.fit. Now the entire graph is stored as graph_ instead.
- Improve documentation of make_mapper_pipeline
- Expose ParallelClustering and Nerve in __init__ and docs.
- Adapt tests, mapper quickstart notebook, and mapper plotting functions.

* Add two tests for the behaviour of store_edge_elements and min_intersection

* Remove check for shape of `layout` in _calculate_graph_data

`layout` can only be a string or a callable, not an ndarray

* Improve test coverage of mapper visualization modules

* Create tests for plot_betti_surfaces and plot_betti_curves

* Add plotly_params to remaining plot methods in diagrams/representations, missed out in #441 

* Fix some linting and docstrings

* Minor improvements

* Avoid shadowing range function in plot_diagram

gtda/plotting/persistence_diagrams.py
@@ -37,72 +37,72 @@ def plot_diagram(diagram, homology_dimensions=None, plotly_params=None):
         homology_dimensions = np.unique(diagram[:, 2])
 
     diagram_no_dims = diagram[:, :2]
-    max_birth, max_death = np.where(
-        np.isposinf(diagram_no_dims), -np.inf, diagram_no_dims
-    ).max(axis=0)
-    min_birth, min_death = np.where(
-        np.isneginf(diagram_no_dims), np.inf, diagram_no_dims
-    ).min(axis=0)
+    max_birth, max_death = np.max(
+        np.where(np.isposinf(diagram_no_dims), -np.inf, diagram_no_dims),
+        axis=0
+        )
+    min_birth, min_death = np.max(
+        np.where(np.isneginf(diagram_no_dims), np.inf, diagram_no_dims),
+        axis=0
+        )
 
     fig = gobj.Figure()
     fig.add_trace(gobj.Scatter(x=[100 * min(-np.abs(max_death), min_birth),
                                   100 * max_death],
                                y=[100 * min(-np.abs(max_death), min_birth),
                                   100 * max_death],
-                               mode='lines',
-                               line=dict(dash='dash', width=1, color='black'),
-                               showlegend=False, hoverinfo='none'))
+                               mode="lines",
+                               line=dict(dash="dash", width=1, color="black"),
+                               showlegend=False, hoverinfo="none"))
 
     for dim in homology_dimensions:
-        name = f'H{int(dim)}' if dim != np.inf else 'Any homology dimension'
+        name = f"H{int(dim)}" if dim != np.inf else "Any homology dimension"
         subdiagram = diagram[diagram[:, 2] == dim]
         diff = (subdiagram[:, 1] != subdiagram[:, 0])
         subdiagram = subdiagram[diff]
         fig.add_trace(gobj.Scatter(x=subdiagram[:, 0], y=subdiagram[:, 1],
-                                   mode='markers', name=name))
+                                   mode="markers", name=name))
 
-    range = max_death - min_birth
-    extra_space = 0.02 * range
+    parameter_range = max_death - min_birth
+    extra_space = 0.02 * parameter_range
 
     fig.update_layout(
         width=500,
         height=500,
-        xaxis1=dict(
-            title='Birth',
-            side='bottom',
-            type='linear',
-            range=[min_birth - extra_space, max_death + extra_space],
-            autorange=False,
-            ticks='outside',
-            showline=True,
-            zeroline=True,
-            linewidth=1,
-            linecolor='black',
-            mirror=False,
-            showexponent='all',
-            exponentformat='e'
-        ),
-        yaxis1=dict(
-            title='Death',
-            side='left',
-            type='linear',
-            range=[min_birth - extra_space, max_death + extra_space],
-            autorange=False,
-            scaleanchor="x",
-            scaleratio=1,
-            ticks='outside',
-            showline=True,
-            zeroline=True,
-            linewidth=1,
-            linecolor='black',
-            mirror=False,
-            showexponent='all',
-            exponentformat='e'
-        ),
-        plot_bgcolor='white'
-    )
+        xaxis1={
+            "title": "Birth",
+            "side": "bottom",
+            "type": "linear",
+            "range": [min_birth - extra_space, max_death + extra_space],
+            "autorange": False,
+            "ticks": "outside",
+            "showline": True,
+            "zeroline": True,
+            "linewidth": 1,
+            "linecolor": "black",
+            "mirror": False,
+            "showexponent": "all",
+            "exponentformat": "e"
+            },
+        yaxis1={
+            "title": "Death",
+            "side": "left",
+            "type": "linear",
+            "range": [min_birth - extra_space, max_death + extra_space],
+            "autorange": False, "scaleanchor": "x", "scaleratio": 1,
+            "ticks": "outside",
+            "showline": True,
+            "zeroline": True,
+            "linewidth": 1,
+            "linecolor": "black",
+            "mirror": False,
+            "showexponent": "all",
+            "exponentformat": "e"
+            },
+        plot_bgcolor="white"
+        )
 
-    # Update trace and layout according to user input
+    # Update traces and layout according to user input
     if plotly_params:
         fig.update_traces(plotly_params.get("traces", None))
         fig.update_layout(plotly_params.get("layout", None))

examples/mapper_quickstart.ipynb
@@ -342,7 +342,7 @@
    "cell_type": "markdown",
    "metadata": {},
    "source": [
-    "The resulting graph is a [`python-igraph`](https://igraph.org/python/) object that contains metadata that is stored in the form of dictionaries. We can access this data as follows:"
+    "The resulting graph is a [`python-igraph`](https://igraph.org/python/) object which stores node metadata in the form of attributes. We can access this data as follows:"
    ]
   },
   {
@@ -351,14 +351,14 @@
    "metadata": {},
    "outputs": [],
    "source": [
-    "graph[\"node_metadata\"].keys()"
+    "graph.vs.attributes()"
    ]
   },
   {
    "cell_type": "markdown",
    "metadata": {},
    "source": [
-    "Here `node_id` is a globally unique node identifier used to construct the graph, while `pullback_set_label` and `partial_cluster_label` refer to the interval and cluster sets described above. The `node_elements` refers to the indices of our original data that belong to each node. For example, to find which points belong to the first node of the graph we can access the desired data as follows:"
+    "Here `'pullback_set_label'` and `'partial_cluster_label'` refer to the interval and cluster sets described above. `'node_elements'` refers to the indices of our original data that belong to each node. For example, to find which points belong to the first node of the graph we can access the desired data as follows:"
    ]
   },
   {
@@ -367,23 +367,14 @@
    "metadata": {},
    "outputs": [],
    "source": [
-    "node_id, node_elements = (\n",
-    "    graph[\"node_metadata\"][\"node_id\"],\n",
-    "    graph[\"node_metadata\"][\"node_elements\"],\n",
-    ")"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "print(\n",
-    "    \"Node ID: {}, \\nNode elements: {}, \\nData points: {}\".format(\n",
-    "        node_id[0], node_elements[0], data[node_elements[0]]\n",
-    "    )\n",
-    ")"
+    "node_id = 0\n",
+    "node_elements = graph.vs[\"node_elements\"]\n",
+    "\n",
+    "print(f\"\"\"\n",
+    "Node ID: {node_id}\n",
+    "Node elements: {node_elements[node_id]}\n",
+    "Data points: {data[node_elements[node_id]]}\n",
+    "\"\"\")"
    ]
   },
   {

gtda/images/tests/test_preprocessing.py
@@ -1,4 +1,4 @@
-"""Testing for images preprocessors."""
+"""Testing for image preprocessors."""
 # License: GNU AGPLv3
 
 import numpy as np

gtda/mapper/__init__.py
@@ -1,9 +1,10 @@
 """The module :mod:`gtda.mapper` implements the Mapper algorithm for
 topological clustering and visualisation."""
 
-from .cluster import FirstHistogramGap, FirstSimpleGap
+from .cluster import FirstHistogramGap, FirstSimpleGap, ParallelClustering
 from .cover import CubicalCover, OneDimensionalCover
 from .filter import Eccentricity, Entropy, Projection
+from .nerve import Nerve
 from .pipeline import make_mapper_pipeline
 from .utils.decorators import method_to_transform
 from .utils.pipeline import transformer_from_callable_on_rows
@@ -18,6 +19,8 @@ __all__ = [
     'CubicalCover',
     'FirstSimpleGap',
     'FirstHistogramGap',
+    'ParallelClustering',
+    'Nerve',
     'make_mapper_pipeline',
     'plot_static_mapper_graph',
     'plot_interactive_mapper_graph',

gtda/mapper/cluster.py
@@ -32,6 +32,8 @@ class ParallelClustering(BaseEstimator):
     location of a portion of ``X_tot`` to cluster separately. Parallelism is
     achieved over the columns of ``masks``.
 
+    This estimator is not intended for direct use.
+
     Parameters
     ----------
     clusterer : object, optional, default: ``None``
@@ -94,7 +96,7 @@ class ParallelClustering(BaseEstimator):
             self._precomputed = precomputed[0]
         else:
             raise NotImplementedError("Behaviour when metric and affinity "
-                                      "are both set to 'precomputed' not yet"
+                                      "are both set to 'precomputed' not yet "
                                       "implemented by ParallelClustering.")
 
     def fit(self, X, y=None, sample_weight=None):

gtda/mapper/nerve.py
@@ -1,4 +1,4 @@
-"""Construct the nerve of a Mapper cover."""
+"""Construct the nerve of a refined Mapper cover."""
 # License: GNU AGPLv3
 
 from functools import reduce
@@ -11,59 +11,49 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 
 class Nerve(BaseEstimator, TransformerMixin):
-    """One-dimensional skeleton of the nerve of a Mapper cover, i.e. the
-    Mapper graph.
+    """1-skeleton of the nerve of a refined Mapper cover, i.e. the Mapper
+    graph.
 
     This transformer is the final step in the
     :class:`gtda.mapper.pipeline.MapperPipeline` objects created
-    by :func:`gtda.mapper.make_mapper_pipeline`. It is not intended for
-    direct use.
+    by :func:`gtda.mapper.make_mapper_pipeline`. It corresponds the last two
+    arrows in `this diagram <../../../../_images/mapper_pipeline.svg>`_.
+
+    This transformer is not intended for direct use.
 
     Parameters
     ----------
     min_intersection : int, optional, default: ``1``
-        The minimum size of the intersection between Mapper cover sets
-        required to create an edge in the Mapper graph.
+        Minimum size of the intersection, between data subsets associated to
+        any two Mapper nodes, required to create an edge between the nodes in
+        the Mapper graph.
+
+    store_edge_elements : bool, options, default: ``False``
+        Whether the indices of data elements associated to Mapper edges (i.e.
+        in the intersections allowed by `min_intersection`) should be stored in
+        the :class:`igraph.Graph` object output by :meth:`fit_transform`. When
+        ``True``, might lead to a large :class:`igraph.Graph` object.
 
     Attributes
     ----------
-    X_ : list of tuple
-        Nodes of the Mapper graph obtained from the input data for
-        :meth:`fit`. It is a flattened version of the input Mapper cover,
-        with the addition of a globally unique node ID as the first entry in
-        each tuple. Created only when :meth:`fit` is called.
-
-    edges_ : list of dict
-        Edges of the Mapper graph obtained from the input data for
-        :meth:`fit`. Each edge is a dictionary with two keys:
-        ``'node_indices'`` is mapped to a pair of triples characterising the
-        two adjacent nodes; ``'intersection'`` is mapped to the array of
-        indices of points in the intersection between the two nodes. Created
-        only when :meth:`fit` is called.
+    graph_ : :class:`igraph.Graph` object
+        Mapper graph obtained from the input data. Created when :meth:`fit` is
+        called.
 
     """
 
-    def __init__(self, min_intersection=1):
+    def __init__(self, min_intersection=1, store_edge_elements=False):
         self.min_intersection = min_intersection
+        self.store_edge_elements = store_edge_elements
 
     def fit(self, X, y=None):
-        """Compute and store the nodes and edges of the Mapper graph,
-        and return the estimator.
+        """Compute the Mapper graph as in :meth:`fit_transform`, but store the
+        graph as :attr:`graph_` and return the estimator.
 
         Parameters
         ----------
         X : list of list of tuple
-            Input data structure describing an abstract Mapper cover. Each
-            sublist corresponds to a (non-empty) pullback cover set --
-            equivalently, to a cover set in the filter range which has
-            non-empty preimage -- and contains triples of the form ``( \
-            pullback_set_label, partial_cluster_label, indices)`` where
-            ``partial_cluster_label`` is a cluster label within the pullback
-            cover set identified by ``pullback_set_label``, and ``indices``
-            is the array of indices of points belonging to cluster ``( \
-            pullback_set_label, partial_cluster_label)``. In the context of a
-            :class:`gtda.mapper.MapperPipeline`, this is the output of the
-            clustering step.
+            See :meth:`fit_transform`.
 
         y : None
             There is no need for a target in a transformer, yet the pipeline
@@ -74,27 +64,28 @@ class Nerve(BaseEstimator, TransformerMixin):
         self : object
 
         """
-        # TODO: Include a validation step for X
-        self.X_, self.edges_ = self._graph_data_creation(X)
+        self.graph_ = self.fit_transform(X, y=y)
         return self
 
-    def fit_transform(self, X, y=None, **fit_params):
-        """Construct a Mapper graph from an abstract Mapper cover `X`.
+    def fit_transform(self, X, y=None):
+        """Construct a Mapper graph from a refined Mapper cover.
 
         Parameters
         ----------
         X : list of list of tuple
-            Input data structure describing an abstract Mapper cover. Each
-            sublist corresponds to a (non-empty) pullback cover set --
+            Data structure describing a cover of a dataset (e.g. as depicted in
+            `this diagram <../../../../_images/mapper_pipeline.svg>`_) produced
+            by the clustering step of a :class:`gtda.mapper.MapperPipeline`.
+            Each sublist corresponds to a (non-empty) pullback cover set --
             equivalently, to a cover set in the filter range which has
-            non-empty preimage -- and contains triples of the form ``( \
-            pullback_set_label, partial_cluster_label, indices)`` where
+            non-empty preimage. It contains triples of the form ``(\
+            pullback_set_label, partial_cluster_label, node_elements)`` where
             ``partial_cluster_label`` is a cluster label within the pullback
-            cover set identified by ``pullback_set_label``, and ``indices``
-            is the array of indices of points belonging to cluster ``( \
-            pullback_set_label, partial_cluster_label)``. In the context of a
-            :class:`gtda.mapper.MapperPipeline`, this is the output of the
-            clustering step.
+            cover set identified by ``pullback_set_label``, and
+            ``node_elements`` is an array of integer indices. To each pair
+            ``(pullback_set_label, partial_cluster_label)`` there corresponds
+            a unique node in the output Mapper graph. This node represents
+            the data subset defined by the indices in ``node_elements``.
 
         y : None
             There is no need for a target in a transformer, yet the pipeline
@@ -103,47 +94,66 @@ class Nerve(BaseEstimator, TransformerMixin):
         Returns
         -------
         graph : :class:`igraph.Graph` object
-            Mapper graph. Edges exist between two Mapper cover sets if and
-            only if the size of the intersection between the two sets is no
-            less than `min_intersection`.
+            Undirected Mapper graph according to `X` and `min_intersection`.
+            Each node is an :class:`igraph.Vertex` object with attributes
+            ``"pullback_set_label"``, ``"partial_cluster_label"`` and
+            ``"node_elements"'``. Each edge is an :class:`igraph.Edge` object
+            with a ``"weight"`` attribute which is equal to the size of the
+            intersection between the data subsets associated to its two nodes.
+            If `store_edge_elements` is ``True`` each edge also has an
+            additional attribute ``"edge_elements"``.
 
         """
         # TODO: Include a validation step for X
-        _X, _edges = self._graph_data_creation(X)
+        # Graph construction -- vertices with their metadata
+        nodes = reduce(iconcat, X, [])
+        graph = ig.Graph(len(nodes))
 
-        # Graph construction
-        graph = ig.Graph()
-        graph.add_vertices([vertex[0] for vertex in _X])
-        graph.add_edges([
-            (edge['node_indices'][0][0], edge['node_indices'][1][0])
-            for edge in _edges
-        ])
-        graph['node_metadata'] = dict(
-            zip(['node_id', 'pullback_set_label', 'partial_cluster_label',
-                 'node_elements'],
-                zip(*_X)))
+        # Since `nodes` is a list, say of length N, of triples of the form
+        # (pullback_set_label, partial_cluster_label, node_elements),
+        # zip(*nodes) generates three tuples of length N, each corresponding to
+        # a type of node attribute.
+        node_attributes = zip(*nodes)
+        graph.vs["pullback_set_label"] = next(node_attributes)
+        graph.vs["partial_cluster_label"] = next(node_attributes)
+        node_elements = next(node_attributes)
+        graph.vs["node_elements"] = node_elements
+
+        # Graph construction -- edges with weights given by intersection sizes
+        node_index_pairs, weights, intersections = \
+            self._generate_edge_data(node_elements)
+        graph.es["weight"] = 1
+        graph.add_edges(node_index_pairs)
+        graph.es["weight"] = weights
+        if self.store_edge_elements:
+            graph.es["edge_elements"] = intersections
+
         return graph
 
-    def _graph_data_creation(self, X):
-        X_ = reduce(iconcat, X, [])
-        # Preprocess X by 1) flattening and 2) extending each tuple
-        X_ = [(node_info[0], *node_info[1])
-              for node_info in zip(range(len(X_)), X_)]
-        edges_ = self._generate_edges(X_)
-        return X_, edges_
+    def _generate_edge_data(self, node_elements):
+        node_tuples = combinations(enumerate(node_elements), 2)
 
-    @staticmethod
-    def _pairwise_intersections(min_intersection, node_pair):
-        data = dict()
-        node_1, node_2 = node_pair
-        data['node_indices'] = tuple((node_1[0:3], node_2[0:3]))
-        data['intersection'] = np.intersect1d(node_1[3], node_2[3])
-        if data['intersection'].size >= min_intersection:
-            yield data
+        node_index_pairs = []
+        weights = []
+        intersections = []
 
-    def _generate_edges(self, nodes):
-        node_tuples = combinations(nodes, 2)
-        for pair in node_tuples:
-            for intersection in \
-             self._pairwise_intersections(self.min_intersection, pair):
-                yield intersection
+        # Boilerplate is just to avoid boolean checking at each iteration
+        if not self.store_edge_elements:
+            for (node_1_idx, node_1_elements), (node_2_idx, node_2_elements) \
+                    in node_tuples:
+                intersection = np.intersect1d(node_1_elements, node_2_elements)
+                intersection_size = len(intersection)
+                if intersection_size >= self.min_intersection:
+                    node_index_pairs.append((node_1_idx, node_2_idx))
+                    weights.append(intersection_size)
+        else:
+            for (node_1_idx, node_1_elements), (node_2_idx, node_2_elements) \
+                    in node_tuples:
+                intersection = np.intersect1d(node_1_elements, node_2_elements)
+                intersection_size = len(intersection)
+                if intersection_size >= self.min_intersection:
+                    node_index_pairs.append((node_1_idx, node_2_idx))
+                    weights.append(intersection_size)
+                    intersections.append(intersection)
+
+        return node_index_pairs, weights, intersections

commit 8cea7e67f4a059ff5a3e0d3dace33c9c4a3db7e4

Fix bug introduced in 4bc90b2e88926dbf2b4da2a455e7480485303d64
np.max should have been np.min in plot_diagram for minumum birth and death calculation

gtda/plotting/persistence_diagrams.py
@@ -41,19 +41,20 @@ def plot_diagram(diagram, homology_dimensions=None, plotly_params=None):
         np.where(np.isposinf(diagram_no_dims), -np.inf, diagram_no_dims),
         axis=0
         )
-    min_birth, min_death = np.max(
+    min_birth, min_death = np.min(
         np.where(np.isneginf(diagram_no_dims), np.inf, diagram_no_dims),
         axis=0
         )
 
     fig = gobj.Figure()
-    fig.add_trace(gobj.Scatter(x=[100 * min(-np.abs(max_death), min_birth),
-                                  100 * max_death],
-                               y=[100 * min(-np.abs(max_death), min_birth),
-                                  100 * max_death],
-                               mode="lines",
-                               line=dict(dash="dash", width=1, color="black"),
-                               showlegend=False, hoverinfo="none"))
+    fig.add_trace(gobj.Scatter(
+        x=[100 * min(-np.abs(max_death), min_birth), 100 * max_death],
+        y=[100 * min(-np.abs(max_death), min_birth), 100 * max_death],
+        mode="lines",
+        line={"dash": "dash", "width": 1, "color": "black"},
+        showlegend=False,
+        hoverinfo="none"
+        ))
 
     for dim in homology_dimensions:
         name = f"H{int(dim)}" if dim != np.inf else "Any homology dimension"

gtda/plotting/point_clouds.py
@@ -58,7 +58,7 @@ def plot_point_cloud(point_cloud, dimension=None, plotly_params=None):
                 "zeroline": True,
                 "showexponent": "all",
                 "exponentformat": "e"
-            },
+                },
             "yaxis1": {
                 "title": "1st",
                 "side": "left",
@@ -69,24 +69,28 @@ def plot_point_cloud(point_cloud, dimension=None, plotly_params=None):
                 "zeroline": True,
                 "showexponent": "all",
                 "exponentformat": "e"
-            },
+                },
             "plot_bgcolor": "white"
-        }
+            }
 
         fig = gobj.Figure(layout=layout)
-        fig.update_xaxes(zeroline=True, linewidth=1, linecolor='black',
+        fig.update_xaxes(zeroline=True, linewidth=1, linecolor="black",
                          mirror=False)
-        fig.update_yaxes(zeroline=True, linewidth=1, linecolor='black',
+        fig.update_yaxes(zeroline=True, linewidth=1, linecolor="black",
                          mirror=False)
 
-        fig.add_trace(gobj.Scatter(x=point_cloud[:, 0],
-                                   y=point_cloud[:, 1],
-                                   mode='markers',
-                                   marker=dict(size=4,
-                                               color=list(range(
-                                                   point_cloud.shape[0])),
-                                               colorscale='Viridis',
-                                               opacity=0.8)))
+        fig.add_trace(gobj.Scatter(
+            x=point_cloud[:, 0],
+            y=point_cloud[:, 1],
+            mode="markers",
+            marker={
+                "size": 4,
+                "color": list(range(point_cloud.shape[0])),
+                "colorscale": "Viridis",
+                "opacity": 0.8
+                }
+            ))
+
     elif dimension == 3:
         scene = {
             "xaxis": {
@@ -94,33 +98,36 @@ def plot_point_cloud(point_cloud, dimension=None, plotly_params=None):
                 "type": "linear",
                 "showexponent": "all",
                 "exponentformat": "e"
-            },
+                },
             "yaxis": {
                 "title": "1st",
                 "type": "linear",
                 "showexponent": "all",
                 "exponentformat": "e"
-            },
+                },
             "zaxis": {
                 "title": "2nd",
                 "type": "linear",
                 "showexponent": "all",
                 "exponentformat": "e"
+                }
             }
-        }
 
         fig = gobj.Figure()
         fig.update_layout(scene=scene)
 
-        fig.add_trace(gobj.Scatter3d(x=point_cloud[:, 0],
-                                     y=point_cloud[:, 1],
-                                     z=point_cloud[:, 2],
-                                     mode='markers',
-                                     marker=dict(size=4,
-                                                 color=list(range(
-                                                     point_cloud.shape[0])),
-                                                 colorscale='Viridis',
-                                                 opacity=0.8)))
+        fig.add_trace(gobj.Scatter3d(
+            x=point_cloud[:, 0],
+            y=point_cloud[:, 1],
+            z=point_cloud[:, 2],
+            mode="markers",
+            marker={
+                "size": 4,
+                "color": list(range(point_cloud.shape[0])),
+                "colorscale": "Viridis",
+                "opacity": 0.8
+                }
+            ))
 
     # Update trace and layout according to user input
     if plotly_params: