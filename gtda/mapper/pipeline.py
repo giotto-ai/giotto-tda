giotto-ai/giotto-tda

commit 4bc90b2e88926dbf2b4da2a455e7480485303d64

Refactor igraph.Graph output of Nerve, modify `fit` behaviour of Nerve, add `store_edge_elements` kwarg to Nerve and make_mapper_pipeline, add Nerve and ParallelClustering to docs (#447)
* Refactor igraph.Graph output, add Nerve and ParallelClustering to docs, add store_edge_elements kwarg to Nerve and make_mapper_pipeline

- Store node metadata not as a graph-level dictionary, but as vertex attributes accessible by graph.vs[attr_name][node_id] or graph.vs[node_id][attr_name] for attr_name in ["pullback_set_label", "partial_cluster_label", "node_elements"]
- Remove "node_id" from node attributes as it always coincided with the igraph.Graph node number anyway.
- Automatically store sizes of intersections as edge weights, accessible by graph.es["weight"].
- Add "store_intersections" kwarg to Nerve and make_mapper_pipeline to allow storing indices of node intersections as edge attributes, accessible via graph.es["edge_elements"].
- Simplify logic of Nerve.fit_transform code
- Change the attributes stored by Nerve.fit. Now the entire graph is stored as graph_ instead.
- Improve documentation of make_mapper_pipeline
- Expose ParallelClustering and Nerve in __init__ and docs.
- Adapt tests, mapper quickstart notebook, and mapper plotting functions.

* Add two tests for the behaviour of store_edge_elements and min_intersection

* Remove check for shape of `layout` in _calculate_graph_data

`layout` can only be a string or a callable, not an ndarray

* Improve test coverage of mapper visualization modules

* Create tests for plot_betti_surfaces and plot_betti_curves

* Add plotly_params to remaining plot methods in diagrams/representations, missed out in #441 

* Fix some linting and docstrings

* Minor improvements

* Avoid shadowing range function in plot_diagram

gtda/mapper/pipeline.py
@@ -149,6 +149,7 @@ def make_mapper_pipeline(scaler=None,
                          parallel_backend_prefer='threads',
                          graph_step=True,
                          min_intersection=1,
+                         store_edge_elements=False,
                          memory=None,
                          verbose=False):
     """Construct a MapperPipeline object according to the specified Mapper
@@ -207,12 +208,20 @@ def make_mapper_pipeline(scaler=None,
 
     graph_step : bool, optional, default: ``True``
         Whether the resulting pipeline should stop at the calculation of the
-        Mapper cover, or include the construction of the Mapper graph.
+        (refined) Mapper cover, or include the construction of the Mapper
+        graph.
 
     min_intersection : int, optional, default: ``1``
         Minimum size of the intersection between clusters required for creating
         an edge in the Mapper graph. Ignored if `graph_step` is set to
         ``False``.
+
+    store_edge_elements : bool, options, default: ``False``
+        Whether the indices of data elements associated to Mapper edges (i.e.
+        in the intersections allowed by `min_intersection`) should be stored in
+        the :class:`igraph.Graph` object output by the pipeline's
+        :meth:`fit_transform`. When ``True``, might lead to large
+        :class:`igraph.Graph` objects.
 
     memory : None, str or object with the joblib.Memory interface, \
         optional, default: ``None``
@@ -231,7 +240,12 @@ def make_mapper_pipeline(scaler=None,
     Returns
     -------
     mapper_pipeline : :class:`~gtda.mapper.pipeline.MapperPipeline` object
-        Output Mapper pipeline.
+        Output Mapper pipeline. The output of `mapper_pipeline`'s
+        :meth:`fit_transform` is: a) an :class:`igraph.Graph` object as per the
+        output of `~gtda.mapper.nerve.Nerve`, when `graph_step` is ``True``; b)
+        a list of lists of tuples as per the output of
+        `~gtda.mapper.clustering.ParallelClustering` (or input of
+        `~gtda.mapper.nerve.Nerve`), otherwise.
 
     Examples
     --------
@@ -253,15 +267,14 @@ def make_mapper_pipeline(scaler=None,
     >>> mapper_graph = mapper.fit_transform(X)  # Create the mapper graph
     >>> print(type(mapper_graph))
     igraph.Graph
-    >>> # Node metadata stored as dict in graph object
-    >>> print(mapper_graph['node_metadata'].keys())
-    dict_keys(['node_id', 'pullback_set_label', 'partial_cluster_label',
-               'node_elements'])
+    >>> # Node metadata stored as vertex attributes in graph object
+    >>> print(mapper_graph.vs.attributes())
+    ['pullback_set_label', 'partial_cluster_label', 'node_elements']
     >>> # Find which points belong to first node of graph
-    >>> node_id = mapper_graph['node_metadata']['node_id']
-    >>> node_elements = mapper_graph['node_metadata']['node_elements']
-    >>> print(f"Node ID: {node_id[0]}, Node elements: {node_elements[0]}, "
-    ...       f"Data points: {X[node_elements[0]]}")
+    >>> node_id = 0
+    >>> node_elements = mapper_graph.vs['node_elements']
+    >>> print(f"Node ID: {node_id}, Node elements: {node_elements[node_id]}, "
+    ...       f"Data points: {X[node_elements[node_id]")
     Node Id: 0,
     Node elements: [8768],
     Data points: [[0.01838998 0.76928754 0.98199244 0.0074299 ]]
@@ -398,7 +411,10 @@ def make_mapper_pipeline(scaler=None,
     ]
 
     if graph_step:
-        all_steps.append(('nerve', Nerve(min_intersection=min_intersection)))
+        all_steps.append(
+            ('nerve', Nerve(min_intersection=min_intersection,
+                            store_edge_elements=store_edge_elements))
+        )
 
     mapper_pipeline = MapperPipeline(
         steps=all_steps, memory=memory, verbose=verbose)

gtda/plotting/persistence_diagrams.py
@@ -37,72 +37,72 @@ def plot_diagram(diagram, homology_dimensions=None, plotly_params=None):
         homology_dimensions = np.unique(diagram[:, 2])
 
     diagram_no_dims = diagram[:, :2]
-    max_birth, max_death = np.where(
-        np.isposinf(diagram_no_dims), -np.inf, diagram_no_dims
-    ).max(axis=0)
-    min_birth, min_death = np.where(
-        np.isneginf(diagram_no_dims), np.inf, diagram_no_dims
-    ).min(axis=0)
+    max_birth, max_death = np.max(
+        np.where(np.isposinf(diagram_no_dims), -np.inf, diagram_no_dims),
+        axis=0
+        )
+    min_birth, min_death = np.max(
+        np.where(np.isneginf(diagram_no_dims), np.inf, diagram_no_dims),
+        axis=0
+        )
 
     fig = gobj.Figure()
     fig.add_trace(gobj.Scatter(x=[100 * min(-np.abs(max_death), min_birth),
                                   100 * max_death],
                                y=[100 * min(-np.abs(max_death), min_birth),
                                   100 * max_death],
-                               mode='lines',
-                               line=dict(dash='dash', width=1, color='black'),
-                               showlegend=False, hoverinfo='none'))
+                               mode="lines",
+                               line=dict(dash="dash", width=1, color="black"),
+                               showlegend=False, hoverinfo="none"))
 
     for dim in homology_dimensions:
-        name = f'H{int(dim)}' if dim != np.inf else 'Any homology dimension'
+        name = f"H{int(dim)}" if dim != np.inf else "Any homology dimension"
         subdiagram = diagram[diagram[:, 2] == dim]
         diff = (subdiagram[:, 1] != subdiagram[:, 0])
         subdiagram = subdiagram[diff]
         fig.add_trace(gobj.Scatter(x=subdiagram[:, 0], y=subdiagram[:, 1],
-                                   mode='markers', name=name))
+                                   mode="markers", name=name))
 
-    range = max_death - min_birth
-    extra_space = 0.02 * range
+    parameter_range = max_death - min_birth
+    extra_space = 0.02 * parameter_range
 
     fig.update_layout(
         width=500,
         height=500,
-        xaxis1=dict(
-            title='Birth',
-            side='bottom',
-            type='linear',
-            range=[min_birth - extra_space, max_death + extra_space],
-            autorange=False,
-            ticks='outside',
-            showline=True,
-            zeroline=True,
-            linewidth=1,
-            linecolor='black',
-            mirror=False,
-            showexponent='all',
-            exponentformat='e'
-        ),
-        yaxis1=dict(
-            title='Death',
-            side='left',
-            type='linear',
-            range=[min_birth - extra_space, max_death + extra_space],
-            autorange=False,
-            scaleanchor="x",
-            scaleratio=1,
-            ticks='outside',
-            showline=True,
-            zeroline=True,
-            linewidth=1,
-            linecolor='black',
-            mirror=False,
-            showexponent='all',
-            exponentformat='e'
-        ),
-        plot_bgcolor='white'
-    )
+        xaxis1={
+            "title": "Birth",
+            "side": "bottom",
+            "type": "linear",
+            "range": [min_birth - extra_space, max_death + extra_space],
+            "autorange": False,
+            "ticks": "outside",
+            "showline": True,
+            "zeroline": True,
+            "linewidth": 1,
+            "linecolor": "black",
+            "mirror": False,
+            "showexponent": "all",
+            "exponentformat": "e"
+            },
+        yaxis1={
+            "title": "Death",
+            "side": "left",
+            "type": "linear",
+            "range": [min_birth - extra_space, max_death + extra_space],
+            "autorange": False, "scaleanchor": "x", "scaleratio": 1,
+            "ticks": "outside",
+            "showline": True,
+            "zeroline": True,
+            "linewidth": 1,
+            "linecolor": "black",
+            "mirror": False,
+            "showexponent": "all",
+            "exponentformat": "e"
+            },
+        plot_bgcolor="white"
+        )
 
-    # Update trace and layout according to user input
+    # Update traces and layout according to user input
     if plotly_params:
         fig.update_traces(plotly_params.get("traces", None))
         fig.update_layout(plotly_params.get("layout", None))

examples/mapper_quickstart.ipynb
@@ -342,7 +342,7 @@
    "cell_type": "markdown",
    "metadata": {},
    "source": [
-    "The resulting graph is a [`python-igraph`](https://igraph.org/python/) object that contains metadata that is stored in the form of dictionaries. We can access this data as follows:"
+    "The resulting graph is a [`python-igraph`](https://igraph.org/python/) object which stores node metadata in the form of attributes. We can access this data as follows:"
    ]
   },
   {
@@ -351,14 +351,14 @@
    "metadata": {},
    "outputs": [],
    "source": [
-    "graph[\"node_metadata\"].keys()"
+    "graph.vs.attributes()"
    ]
   },
   {
    "cell_type": "markdown",
    "metadata": {},
    "source": [
-    "Here `node_id` is a globally unique node identifier used to construct the graph, while `pullback_set_label` and `partial_cluster_label` refer to the interval and cluster sets described above. The `node_elements` refers to the indices of our original data that belong to each node. For example, to find which points belong to the first node of the graph we can access the desired data as follows:"
+    "Here `'pullback_set_label'` and `'partial_cluster_label'` refer to the interval and cluster sets described above. `'node_elements'` refers to the indices of our original data that belong to each node. For example, to find which points belong to the first node of the graph we can access the desired data as follows:"
    ]
   },
   {
@@ -367,23 +367,14 @@
    "metadata": {},
    "outputs": [],
    "source": [
-    "node_id, node_elements = (\n",
-    "    graph[\"node_metadata\"][\"node_id\"],\n",
-    "    graph[\"node_metadata\"][\"node_elements\"],\n",
-    ")"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "print(\n",
-    "    \"Node ID: {}, \\nNode elements: {}, \\nData points: {}\".format(\n",
-    "        node_id[0], node_elements[0], data[node_elements[0]]\n",
-    "    )\n",
-    ")"
+    "node_id = 0\n",
+    "node_elements = graph.vs[\"node_elements\"]\n",
+    "\n",
+    "print(f\"\"\"\n",
+    "Node ID: {node_id}\n",
+    "Node elements: {node_elements[node_id]}\n",
+    "Data points: {data[node_elements[node_id]]}\n",
+    "\"\"\")"
    ]
   },
   {

gtda/images/tests/test_preprocessing.py
@@ -1,4 +1,4 @@
-"""Testing for images preprocessors."""
+"""Testing for image preprocessors."""
 # License: GNU AGPLv3
 
 import numpy as np

gtda/mapper/__init__.py
@@ -1,9 +1,10 @@
 """The module :mod:`gtda.mapper` implements the Mapper algorithm for
 topological clustering and visualisation."""
 
-from .cluster import FirstHistogramGap, FirstSimpleGap
+from .cluster import FirstHistogramGap, FirstSimpleGap, ParallelClustering
 from .cover import CubicalCover, OneDimensionalCover
 from .filter import Eccentricity, Entropy, Projection
+from .nerve import Nerve
 from .pipeline import make_mapper_pipeline
 from .utils.decorators import method_to_transform
 from .utils.pipeline import transformer_from_callable_on_rows
@@ -18,6 +19,8 @@ __all__ = [
     'CubicalCover',
     'FirstSimpleGap',
     'FirstHistogramGap',
+    'ParallelClustering',
+    'Nerve',
     'make_mapper_pipeline',
     'plot_static_mapper_graph',
     'plot_interactive_mapper_graph',

gtda/mapper/cluster.py
@@ -32,6 +32,8 @@ class ParallelClustering(BaseEstimator):
     location of a portion of ``X_tot`` to cluster separately. Parallelism is
     achieved over the columns of ``masks``.
 
+    This estimator is not intended for direct use.
+
     Parameters
     ----------
     clusterer : object, optional, default: ``None``
@@ -94,7 +96,7 @@ class ParallelClustering(BaseEstimator):
             self._precomputed = precomputed[0]
         else:
             raise NotImplementedError("Behaviour when metric and affinity "
-                                      "are both set to 'precomputed' not yet"
+                                      "are both set to 'precomputed' not yet "
                                       "implemented by ParallelClustering.")
 
     def fit(self, X, y=None, sample_weight=None):

commit 39dfea02fb102afdcadad937fca5eda6452c995e

Fix minimum birth bug in `plot_diagram` (#449)
* Fix bug introduced in 4bc90b2e88926dbf2b4da2a455e7480485303d64

np.max should have been np.min in plot_diagram for minimum birth and death calculation

* Clean/simplify plot_diagram further to be more ready for extended persistence and better behave under plotly HTML autoscaling

* Make `store_edge_elements` work with MapperPipeline.get_mapper_params and MapperPipeline.set_params, missed out in 4bc90b2e88926dbf2b4da2a455e7480485303d64

gtda/mapper/pipeline.py
@@ -8,16 +8,16 @@ from .nerve import Nerve
 from .utils._list_feature_union import ListFeatureUnion
 from .utils.pipeline import transformer_from_callable_on_rows, identity
 
-global_pipeline_params = ('memory', 'verbose')
-nodes_params = ('scaler', 'filter_func', 'cover')
-clust_prepr_params = ('clustering_preprocessing',)
-clust_params = ('clusterer', 'n_jobs',
-                'parallel_backend_prefer')
-nerve_params = ('min_intersection',)
-clust_prepr_params_prefix = 'pullback_cover__'
-nodes_params_prefix = 'pullback_cover__map_and_cover__'
-clust_params_prefix = 'clustering__'
-nerve_params_prefix = 'nerve__'
+global_pipeline_params = ("memory", "verbose")
+nodes_params = ("scaler", "filter_func", "cover")
+clust_prepr_params = ("clustering_preprocessing",)
+clust_params = ("clusterer", "n_jobs",
+                "parallel_backend_prefer")
+nerve_params = ("min_intersection", "store_edge_elements")
+clust_prepr_params_prefix = "pullback_cover__"
+nodes_params_prefix = "pullback_cover__map_and_cover__"
+clust_params_prefix = "clustering__"
+nerve_params_prefix = "nerve__"
 
 
 class MapperPipeline(Pipeline):
@@ -43,10 +43,10 @@ class MapperPipeline(Pipeline):
     >>> pipe = make_mapper_pipeline(filter_func=filter_func,
     ...                             cover=cover,
     ...                             clusterer=clusterer)
-    >>> print(pipe.get_mapper_params()['clusterer__eps'])
+    >>> print(pipe.get_mapper_params()["clusterer__eps"])
     0.5
     >>> pipe.set_params(clusterer___eps=0.1)
-    >>> print(pipe.get_mapper_params()['clusterer__eps'])
+    >>> print(pipe.get_mapper_params()["clusterer__eps"])
     0.1
 
     See also
@@ -130,14 +130,14 @@ class MapperPipeline(Pipeline):
             key[len(prefix):]: kwargs[key]
             for key in kwargs
             if (key.startswith(prefix)
-                and not key.startswith(prefix + 'steps')
-                and not key.startswith(prefix + 'memory')
-                and not key.startswith(prefix + 'verbose')
-                and not key.startswith(prefix + 'transformer_list')
-                and not key.startswith(prefix + 'n_jobs')
-                and not key.startswith(prefix + 'transformer_weights')
-                and not key.startswith(prefix + 'map_and_cover'))
-        }
+                and not key.startswith(prefix + "steps")
+                and not key.startswith(prefix + "memory")
+                and not key.startswith(prefix + "verbose")
+                and not key.startswith(prefix + "transformer_list")
+                and not key.startswith(prefix + "n_jobs")
+                and not key.startswith(prefix + "transformer_weights")
+                and not key.startswith(prefix + "map_and_cover"))
+            }
 
 
 def make_mapper_pipeline(scaler=None,
@@ -146,7 +146,7 @@ def make_mapper_pipeline(scaler=None,
                          clustering_preprocessing=None,
                          clusterer=None,
                          n_jobs=None,
-                         parallel_backend_prefer='threads',
+                         parallel_backend_prefer="threads",
                          graph_step=True,
                          min_intersection=1,
                          store_edge_elements=False,
@@ -199,12 +199,12 @@ def make_mapper_pipeline(scaler=None,
         in a :obj:`joblib.parallel_backend` context. ``-1`` means using all
         processors.
 
-    parallel_backend_prefer : ``'processes'`` | ``'threads'``, optional, \
-        default: ``'threads'``
+    parallel_backend_prefer : ``"processes"`` | ``"threads"``, optional, \
+        default: ``"threads"``
         Soft hint for the default joblib backend to use in a joblib-parallel
         application of the clustering step across pullback cover sets. To be
         used in conjunction with `n_jobs`. The default process-based backend is
-        'loky' and the default thread-based backend is 'threading'. See [2]_.
+        "loky" and the default thread-based backend is "threading". See [2]_.
 
     graph_step : bool, optional, default: ``True``
         Whether the resulting pipeline should stop at the calculation of the
@@ -257,11 +257,11 @@ def make_mapper_pipeline(scaler=None,
     >>> print(mapper.__class__)
     <class 'gtda.mapper.pipeline.MapperPipeline'>
     >>> mapper_params = mapper.get_mapper_params()
-    >>> print(mapper_params['filter_func'].__class__)
+    >>> print(mapper_params["filter_func"].__class__)
     <class 'sklearn.decomposition._pca.PCA'>
-    >>> print(mapper_params['cover'].__class__)
+    >>> print(mapper_params["cover"].__class__)
     <class 'gtda.mapper.cover.CubicalCover'>
-    >>> print(mapper_params['clusterer'].__class__)
+    >>> print(mapper_params["clusterer"].__class__)
     <class 'sklearn.cluster._dbscan.DBSCAN'>
     >>> X = np.random.random((10000, 4))  # 10000 points in 4-dimensional space
     >>> mapper_graph = mapper.fit_transform(X)  # Create the mapper graph
@@ -272,7 +272,7 @@ def make_mapper_pipeline(scaler=None,
     ['pullback_set_label', 'partial_cluster_label', 'node_elements']
     >>> # Find which points belong to first node of graph
     >>> node_id = 0
-    >>> node_elements = mapper_graph.vs['node_elements']
+    >>> node_elements = mapper_graph.vs["node_elements"]
     >>> print(f"Node ID: {node_id}, Node elements: {node_elements[node_id]}, "
     ...       f"Data points: {X[node_elements[node_id]")
     Node Id: 0,
@@ -372,7 +372,7 @@ def make_mapper_pipeline(scaler=None,
     if filter_func is None:
         from sklearn.decomposition import PCA
         _filter_func = PCA(n_components=2)
-    elif not hasattr(filter_func, 'fit_transform'):
+    elif not hasattr(filter_func, "fit_transform"):
         _filter_func = transformer_from_callable_on_rows(filter_func)
     else:
         _filter_func = filter_func
@@ -395,26 +395,26 @@ def make_mapper_pipeline(scaler=None,
         _clusterer = clusterer
 
     map_and_cover = Pipeline(
-        steps=[('scaler', _scaler),
-               ('filter_func', _filter_func),
-               ('cover', _cover)],
+        steps=[("scaler", _scaler),
+               ("filter_func", _filter_func),
+               ("cover", _cover)],
         verbose=verbose)
 
     all_steps = [
-        ('pullback_cover', ListFeatureUnion(
-            [('clustering_preprocessing', _clustering_preprocessing),
-             ('map_and_cover', map_and_cover)])),
-        ('clustering', ParallelClustering(
+        ("pullback_cover", ListFeatureUnion(
+            [("clustering_preprocessing", _clustering_preprocessing),
+             ("map_and_cover", map_and_cover)])),
+        ("clustering", ParallelClustering(
             clusterer=_clusterer,
             n_jobs=n_jobs,
             parallel_backend_prefer=parallel_backend_prefer))
-    ]
+        ]
 
     if graph_step:
         all_steps.append(
-            ('nerve', Nerve(min_intersection=min_intersection,
+            ("nerve", Nerve(min_intersection=min_intersection,
                             store_edge_elements=store_edge_elements))
-        )
+            )
 
     mapper_pipeline = MapperPipeline(
         steps=all_steps, memory=memory, verbose=verbose)

gtda/plotting/persistence_diagrams.py
@@ -37,23 +37,26 @@ def plot_diagram(diagram, homology_dimensions=None, plotly_params=None):
         homology_dimensions = np.unique(diagram[:, 2])
 
     diagram_no_dims = diagram[:, :2]
-    max_birth, max_death = np.max(
-        np.where(np.isposinf(diagram_no_dims), -np.inf, diagram_no_dims),
-        axis=0
+    max_val_display = np.max(
+        np.where(np.isposinf(diagram_no_dims), -np.inf, diagram_no_dims)
         )
-    min_birth, min_death = np.max(
-        np.where(np.isneginf(diagram_no_dims), np.inf, diagram_no_dims),
-        axis=0
+    min_val_display = np.min(
+        np.where(np.isneginf(diagram_no_dims), np.inf, diagram_no_dims)
         )
+    parameter_range = max_val_display - min_val_display
+    extra_space = 0.02 * parameter_range
+    min_val_display -= extra_space
+    max_val_display += extra_space
 
     fig = gobj.Figure()
-    fig.add_trace(gobj.Scatter(x=[100 * min(-np.abs(max_death), min_birth),
-                                  100 * max_death],
-                               y=[100 * min(-np.abs(max_death), min_birth),
-                                  100 * max_death],
-                               mode="lines",
-                               line=dict(dash="dash", width=1, color="black"),
-                               showlegend=False, hoverinfo="none"))
+    fig.add_trace(gobj.Scatter(
+        x=[min_val_display, max_val_display],
+        y=[min_val_display, max_val_display],
+        mode="lines",
+        line={"dash": "dash", "width": 1, "color": "black"},
+        showlegend=False,
+        hoverinfo="none"
+        ))
 
     for dim in homology_dimensions:
         name = f"H{int(dim)}" if dim != np.inf else "Any homology dimension"
@@ -63,9 +66,6 @@ def plot_diagram(diagram, homology_dimensions=None, plotly_params=None):
         fig.add_trace(gobj.Scatter(x=subdiagram[:, 0], y=subdiagram[:, 1],
                                    mode="markers", name=name))
 
-    parameter_range = max_death - min_birth
-    extra_space = 0.02 * parameter_range
-
     fig.update_layout(
         width=500,
         height=500,
@@ -73,7 +73,7 @@ def plot_diagram(diagram, homology_dimensions=None, plotly_params=None):
             "title": "Birth",
             "side": "bottom",
             "type": "linear",
-            "range": [min_birth - extra_space, max_death + extra_space],
+            "range": [min_val_display, max_val_display],
             "autorange": False,
             "ticks": "outside",
             "showline": True,
@@ -88,7 +88,7 @@ def plot_diagram(diagram, homology_dimensions=None, plotly_params=None):
             "title": "Death",
             "side": "left",
             "type": "linear",
-            "range": [min_birth - extra_space, max_death + extra_space],
+            "range": [min_val_display, max_val_display],
             "autorange": False, "scaleanchor": "x", "scaleratio": 1,
             "ticks": "outside",
             "showline": True,

gtda/plotting/point_clouds.py
@@ -58,7 +58,7 @@ def plot_point_cloud(point_cloud, dimension=None, plotly_params=None):
                 "zeroline": True,
                 "showexponent": "all",
                 "exponentformat": "e"
-            },
+                },
             "yaxis1": {
                 "title": "1st",
                 "side": "left",
@@ -69,24 +69,28 @@ def plot_point_cloud(point_cloud, dimension=None, plotly_params=None):
                 "zeroline": True,
                 "showexponent": "all",
                 "exponentformat": "e"
-            },
+                },
             "plot_bgcolor": "white"
-        }
+            }
 
         fig = gobj.Figure(layout=layout)
-        fig.update_xaxes(zeroline=True, linewidth=1, linecolor='black',
+        fig.update_xaxes(zeroline=True, linewidth=1, linecolor="black",
                          mirror=False)
-        fig.update_yaxes(zeroline=True, linewidth=1, linecolor='black',
+        fig.update_yaxes(zeroline=True, linewidth=1, linecolor="black",
                          mirror=False)
 
-        fig.add_trace(gobj.Scatter(x=point_cloud[:, 0],
-                                   y=point_cloud[:, 1],
-                                   mode='markers',
-                                   marker=dict(size=4,
-                                               color=list(range(
-                                                   point_cloud.shape[0])),
-                                               colorscale='Viridis',
-                                               opacity=0.8)))
+        fig.add_trace(gobj.Scatter(
+            x=point_cloud[:, 0],
+            y=point_cloud[:, 1],
+            mode="markers",
+            marker={
+                "size": 4,
+                "color": list(range(point_cloud.shape[0])),
+                "colorscale": "Viridis",
+                "opacity": 0.8
+                }
+            ))
+
     elif dimension == 3:
         scene = {
             "xaxis": {
@@ -94,33 +98,36 @@ def plot_point_cloud(point_cloud, dimension=None, plotly_params=None):
                 "type": "linear",
                 "showexponent": "all",
                 "exponentformat": "e"
-            },
+                },
             "yaxis": {
                 "title": "1st",
                 "type": "linear",
                 "showexponent": "all",
                 "exponentformat": "e"
-            },
+                },
             "zaxis": {
                 "title": "2nd",
                 "type": "linear",
                 "showexponent": "all",
                 "exponentformat": "e"
+                }
             }
-        }
 
         fig = gobj.Figure()
         fig.update_layout(scene=scene)
 
-        fig.add_trace(gobj.Scatter3d(x=point_cloud[:, 0],
-                                     y=point_cloud[:, 1],
-                                     z=point_cloud[:, 2],
-                                     mode='markers',
-                                     marker=dict(size=4,
-                                                 color=list(range(
-                                                     point_cloud.shape[0])),
-                                                 colorscale='Viridis',
-                                                 opacity=0.8)))
+        fig.add_trace(gobj.Scatter3d(
+            x=point_cloud[:, 0],
+            y=point_cloud[:, 1],
+            z=point_cloud[:, 2],
+            mode="markers",
+            marker={
+                "size": 4,
+                "color": list(range(point_cloud.shape[0])),
+                "colorscale": "Viridis",
+                "opacity": 0.8
+                }
+            ))
 
     # Update trace and layout according to user input
     if plotly_params: