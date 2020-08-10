57583a16f7301021e34d9e84d2844b02e1795286
Umberto Lupo <umberto.lupo@gmail.com>
2020-08-10 11:22:25 +0200

    Remove y flip

--- gtda/diagrams/representations.py
+++ gtda/diagrams/representations.py
@@ -698,7 +698,7 @@ class HeatKernel(BaseEstimator, TransformerMixin, PlotterMixin):
         check_is_fitted(self)
         x = self.samplings_[self.homology_dimensions_[homology_dimension_idx]]
         return plot_heatmap(
-            Xt[sample][homology_dimension_idx], x=x, y=x[::-1],
+            Xt[sample][homology_dimension_idx], x=x, y=x,
             colorscale=colorscale, plotly_params=plotly_params
             )
 
