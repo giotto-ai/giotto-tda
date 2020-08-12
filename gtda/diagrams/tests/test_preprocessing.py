giotto-ai/giotto-tda

commit 17b906e3d5677b9c28a5c782783b16d5a5d448db

Make homology_dimensions_ attributes tuples instead of lists, with integers when possible

gtda/diagrams/preprocessing.py
@@ -169,7 +169,7 @@ class Scaler(BaseEstimator, TransformerMixin, PlotterMixin):
         Dictionary containing all information present in `metric_params` as
         well as relevant quantities computed in :meth:`fit`.
 
-    homology_dimensions_ : list
+    homology_dimensions_ : tuple
         Homology dimensions seen in :meth:`fit`, sorted in ascending order.
 
     scale_ : float
@@ -239,7 +239,10 @@ class Scaler(BaseEstimator, TransformerMixin, PlotterMixin):
         validate_params(self.effective_metric_params_,
                         _AVAILABLE_AMPLITUDE_METRICS[self.metric])
 
-        self.homology_dimensions_ = sorted(set(X[0, :, 2]))
+        self.homology_dimensions_ = tuple(
+            sorted([int(dim) if dim != np.inf else dim
+                    for dim in set(X[0, :, 2])])
+            )
 
         self.effective_metric_params_['samplings'], \
             self.effective_metric_params_['step_sizes'] = \
@@ -356,10 +359,10 @@ class Filtering(BaseEstimator, TransformerMixin, PlotterMixin):
     """Filtering of persistence diagrams.
 
     Filtering a diagram means discarding all points [b, d, q] representing
-    non-trivial topological features whose lifetime d - b is less than or
-    equal to a cutoff value. Points on the diagonal (i.e. for which b and d
-    are equal) may still appear in the output for padding purposes, but carry
-    no information.
+    non-trivial topological features whose lifetime d - b is less than or equal
+    to a cutoff value. Points on the diagonal (i.e. for which b and d are
+    equal) may still appear in the output for padding purposes, but carry no
+    information.
 
     **Important note**:
 
@@ -379,11 +382,10 @@ class Filtering(BaseEstimator, TransformerMixin, PlotterMixin):
 
     Attributes
     ----------
-    homology_dimensions_ : list
-        If `homology_dimensions` is set to ``None``, then this is the list
-        of homology dimensions seen in :meth:`fit`, sorted in ascending
-        order. Otherwise, it is a similarly sorted version of
-        `homology_dimensions`.
+    homology_dimensions_ : tuple
+        If `homology_dimensions` is set to ``None``, contains the homology
+        dimensions seen in :meth:`fit`, sorted in ascending order. Otherwise,
+        it is a similarly sorted version of `homology_dimensions`.
 
     See also
     --------
@@ -434,10 +436,15 @@ class Filtering(BaseEstimator, TransformerMixin, PlotterMixin):
             self.get_params(), self._hyperparameters)
 
         if self.homology_dimensions is None:
-            self.homology_dimensions_ = [int(dim) for dim in set(X[0, :, 2])]
+            self.homology_dimensions_ = [
+                int(dim) if dim != np.inf else dim for dim in set(X[0, :, 2])
+                ]
         else:
             self.homology_dimensions_ = self.homology_dimensions
-        self.homology_dimensions_ = sorted(self.homology_dimensions_)
+        self.homology_dimensions_ = tuple(
+            sorted([int(dim) if dim != np.inf else dim
+                    for dim in set(X[0, :, 2])])
+            )
 
         return self
 

gtda/diagrams/_utils.py
@@ -47,7 +47,7 @@ def _multirange(counts):
 
 def _filter(X, filtered_homology_dimensions, cutoff):
     n = len(X)
-    homology_dimensions = sorted(list(set(X[0, :, 2])))
+    homology_dimensions = sorted(set(X[0, :, 2]))
     unfiltered_homology_dimensions = [dim for dim in homology_dimensions if
                                       dim not in filtered_homology_dimensions]
 
@@ -91,7 +91,7 @@ def _filter(X, filtered_homology_dimensions, cutoff):
 
 
 def _bin(X, metric, n_bins=100, **kw_args):
-    homology_dimensions = sorted(list(set(X[0, :, 2])))
+    homology_dimensions = sorted(set(X[0, :, 2]))
     # For some vectorizations, we force the values to be the same + widest
     sub_diags = {dim: _subdiagrams(X, [dim], remove_dim=True)
                  for dim in homology_dimensions}

gtda/diagrams/distance.py
@@ -104,7 +104,7 @@ class PairwiseDistance(BaseEstimator, TransformerMixin):
         Dictionary containing all information present in `metric_params` as
         well as relevant quantities computed in :meth:`fit`.
 
-    homology_dimensions_ : list
+    homology_dimensions_ : tuple
         Homology dimensions seen in :meth:`fit`, sorted in ascending order.
 
     See also
@@ -178,7 +178,10 @@ class PairwiseDistance(BaseEstimator, TransformerMixin):
         validate_params(
             self.effective_metric_params_, _AVAILABLE_METRICS[self.metric])
 
-        self.homology_dimensions_ = sorted(set(X[0, :, 2]))
+        self.homology_dimensions_ = tuple(
+            sorted([int(dim) if dim != np.inf else dim
+                    for dim in set(X[0, :, 2])])
+            )
 
         self.effective_metric_params_['samplings'], \
             self.effective_metric_params_['step_sizes'] = \

gtda/diagrams/features.py
@@ -58,7 +58,7 @@ class PersistenceEntropy(BaseEstimator, TransformerMixin):
 
     Attributes
     ----------
-    homology_dimensions_ : list
+    homology_dimensions_ : tuple
         Homology dimensions seen in :meth:`fit`, sorted in ascending order.
 
     See also
@@ -127,7 +127,10 @@ class PersistenceEntropy(BaseEstimator, TransformerMixin):
         validate_params(
             self.get_params(), self._hyperparameters, exclude=['n_jobs'])
 
-        self.homology_dimensions_ = sorted(set(X[0, :, 2]))
+        self.homology_dimensions_ = tuple(
+            sorted([int(dim) if dim != np.inf else dim
+                    for dim in set(X[0, :, 2])])
+            )
         self._n_dimensions = len(self.homology_dimensions_)
 
         return self
@@ -257,7 +260,7 @@ class Amplitude(BaseEstimator, TransformerMixin):
         Dictionary containing all information present in `metric_params` as
         well as relevant quantities computed in :meth:`fit`.
 
-    homology_dimensions_ : list
+    homology_dimensions_ : tuple
         Homology dimensions seen in :meth:`fit`, sorted in ascending order.
 
     See also
@@ -327,7 +330,10 @@ class Amplitude(BaseEstimator, TransformerMixin):
         validate_params(self.effective_metric_params_,
                         _AVAILABLE_AMPLITUDE_METRICS[self.metric])
 
-        self.homology_dimensions_ = sorted(set(X[0, :, 2]))
+        self.homology_dimensions_ = tuple(
+            sorted([int(dim) if dim != np.inf else dim
+                    for dim in set(X[0, :, 2])])
+            )
 
         self.effective_metric_params_['samplings'], \
             self.effective_metric_params_['step_sizes'] = \

gtda/diagrams/representations.py
@@ -49,7 +49,7 @@ class BettiCurve(BaseEstimator, TransformerMixin, PlotterMixin):
 
     Attributes
     ----------
-    homology_dimensions_ : list
+    homology_dimensions_ : tuple
         Homology dimensions seen in :meth:`fit`, sorted in ascending order.
 
     samplings_ : dict
@@ -112,7 +112,10 @@ class BettiCurve(BaseEstimator, TransformerMixin, PlotterMixin):
         validate_params(
             self.get_params(), self._hyperparameters, exclude=["n_jobs"])
 
-        self.homology_dimensions_ = sorted(list(set(X[0, :, 2])))
+        self.homology_dimensions_ = tuple(
+            sorted([int(dim) if dim != np.inf else dim
+                    for dim in set(X[0, :, 2])])
+            )
         self._n_dimensions = len(self.homology_dimensions_)
         self._samplings, _ = _bin(X, "betti", n_bins=self.n_bins)
         self.samplings_ = {dim: s.flatten()
@@ -284,7 +287,7 @@ class PersistenceLandscape(BaseEstimator, TransformerMixin, PlotterMixin):
 
     Attributes
     ----------
-    homology_dimensions_ : list
+    homology_dimensions_ : tuple
         Homology dimensions seen in :meth:`fit`.
 
     samplings_ : dict
@@ -350,7 +353,10 @@ class PersistenceLandscape(BaseEstimator, TransformerMixin, PlotterMixin):
         validate_params(
             self.get_params(), self._hyperparameters, exclude=["n_jobs"])
 
-        self.homology_dimensions_ = sorted(list(set(X[0, :, 2])))
+        self.homology_dimensions_ = tuple(
+            sorted([int(dim) if dim != np.inf else dim
+                    for dim in set(X[0, :, 2])])
+            )
         self._n_dimensions = len(self.homology_dimensions_)
         self._samplings, _ = _bin(X, "landscape", n_bins=self.n_bins)
         self.samplings_ = {dim: s.flatten()
@@ -546,7 +552,7 @@ class HeatKernel(BaseEstimator, TransformerMixin, PlotterMixin):
 
     Attributes
     ----------
-    homology_dimensions_ : list
+    homology_dimensions_ : tuple
         Homology dimensions seen in :meth:`fit`.
 
     samplings_ : dict
@@ -620,7 +626,10 @@ class HeatKernel(BaseEstimator, TransformerMixin, PlotterMixin):
         validate_params(
             self.get_params(), self._hyperparameters, exclude=["n_jobs"])
 
-        self.homology_dimensions_ = sorted(list(set(X[0, :, 2])))
+        self.homology_dimensions_ = tuple(
+            sorted([int(dim) if dim != np.inf else dim
+                    for dim in set(X[0, :, 2])])
+            )
         self._n_dimensions = len(self.homology_dimensions_)
         self._samplings, self._step_size = _bin(X, "heat", n_bins=self.n_bins)
         self.samplings_ = {dim: s.flatten()
@@ -767,7 +776,7 @@ class PersistenceImage(BaseEstimator, TransformerMixin, PlotterMixin):
         Effective function corresponding to `weight_function`. Set in
         :meth:`fit`.
 
-    homology_dimensions_ : list
+    homology_dimensions_ : tuple
         Homology dimensions seen in :meth:`fit`.
 
     samplings_ : dict
@@ -855,7 +864,10 @@ class PersistenceImage(BaseEstimator, TransformerMixin, PlotterMixin):
         else:
             self.effective_weight_function_ = self.weight_function
 
-        self.homology_dimensions_ = sorted(list(set(X[0, :, 2])))
+        self.homology_dimensions_ = tuple(
+            sorted([int(dim) if dim != np.inf else dim
+                    for dim in set(X[0, :, 2])])
+            )
         self._n_dimensions = len(self.homology_dimensions_)
         self._samplings, self._step_size = _bin(X, "persistence_image",
                                                 n_bins=self.n_bins)
@@ -1003,7 +1015,7 @@ class Silhouette(BaseEstimator, TransformerMixin, PlotterMixin):
 
     Attributes
     ----------
-    homology_dimensions_ : list
+    homology_dimensions_ : tuple
         Homology dimensions seen in :meth:`fit`, sorted in ascending order.
 
     samplings_ : dict
@@ -1076,7 +1088,10 @@ class Silhouette(BaseEstimator, TransformerMixin, PlotterMixin):
         validate_params(
             self.get_params(), self._hyperparameters, exclude=["n_jobs"])
 
-        self.homology_dimensions_ = sorted(list(set(X[0, :, 2])))
+        self.homology_dimensions_ = tuple(
+            sorted([int(dim) if dim != np.inf else dim
+                    for dim in set(X[0, :, 2])])
+            )
         self._n_dimensions = len(self.homology_dimensions_)
         self._samplings, _ = _bin(X, "silhouette", n_bins=self.n_bins)
         self.samplings_ = {dim: s.flatten()

gtda/utils/validation.py
@@ -47,7 +47,7 @@ def check_diagrams(X, copy=False):
             f"components, but there are {X_array.shape[2]} components.")
 
     X_array = X_array.astype(float, copy=False)
-    homology_dimensions = sorted(list(set(X_array[0, :, 2])))
+    homology_dimensions = sorted(set(X_array[0, :, 2]))
     for dim in homology_dimensions:
         if dim == np.inf:
             if len(homology_dimensions) != 1:

commit 9ec759cb5d859315e9614d1c29d85cd49a459ed9

Fix small bug introduced in Filtering in 17b906e3d5677b9c28a5c782783b16d5a5d448db, add new tests for Filtering

gtda/diagrams/preprocessing.py
@@ -441,10 +441,7 @@ class Filtering(BaseEstimator, TransformerMixin, PlotterMixin):
                 ]
         else:
             self.homology_dimensions_ = self.homology_dimensions
-        self.homology_dimensions_ = tuple(
-            sorted([int(dim) if dim != np.inf else dim
-                    for dim in set(X[0, :, 2])])
-            )
+        self.homology_dimensions_ = tuple(sorted(self.homology_dimensions_))
 
         return self
 

gtda/diagrams/tests/test_preprocessing.py
@@ -278,6 +278,28 @@ def test_filt_transform_zero(X):
     assert_almost_equal(X_res, X[:, [0], :])
 
 
+def total_lifetimes_in_dims(X, dims):
+    return sum([
+        np.sum(np.diff(X[X[:, :, 2] == dim], axis=1)[:, 0]) for dim in dims
+        ])
+
+
+@pytest.mark.parametrize('homology_dimensions', [None, (0, 1, 2), (0,), (1,),
+                                                 (2,), (0, 1), (0, 2), (1, 2)])
+def test_filt_transform_unfiltered_hom_dims(homology_dimensions):
+    filt = Filtering(epsilon=2., homology_dimensions=homology_dimensions)
+    X_1_res = filt.fit_transform(X_1)
+    if homology_dimensions is None:
+        unfiltered_hom_dims = []
+    else:
+        unfiltered_hom_dims = [
+            dim for dim in filt.homology_dimensions_
+            if dim not in homology_dimensions
+            ]
+    assert total_lifetimes_in_dims(X_1, unfiltered_hom_dims) == \
+           total_lifetimes_in_dims(X_1_res, unfiltered_hom_dims)
+
+
 lifetimes_1 = X_1[:, :, 1] - X_1[:, :, 0]
 epsilons_1 = np.linspace(np.min(lifetimes_1), np.max(lifetimes_1), num=3)
 