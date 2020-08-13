a6ee6d1ab61cd651b046362410ff5f1af394612a
Umberto Lupo <umberto.lupo@gmail.com>
2020-08-13 11:32:05 +0200

    Refactor `_subdiagrams` to be able to throw informative errors on expected input properties

--- gtda/diagrams/_utils.py
+++ gtda/diagrams/_utils.py
@@ -15,15 +15,38 @@ def _subdiagrams(X, homology_dimensions, remove_dim=False):
     """For each diagram in a collection, extract the subdiagrams in a given
     list of homology dimensions. It is assumed that all diagrams in X contain
     the same number of points in each homology dimension."""
-    n = len(X)
+    n_samples = len(X)
+    X_0 = X[0]
+
+    def _subdiagrams_single_homology_dimension(homology_dimension):
+        n_features_in_dim = np.sum(X_0[:, 2] == homology_dimension)
+        try:
+            # In this case, reshape ensures copy
+            Xs = X[X[:, :, 2] == homology_dimension].\
+                reshape(n_samples, n_features_in_dim, 3)
+            return Xs
+        except ValueError as e:
+            if e.args[0].lower().startswith("cannot reshape array"):
+                raise ValueError(
+                    f"All persistence diagrams in the collection must have "
+                    f"the same number of birth-death-dimension triples in any "
+                    f"given homology dimension. This is not true in homology "
+                    f"dimension {dim}. Trivial triples for which birth = "
+                    f"death may be added or removed to fulfill this "
+                    f"requirement."
+                )
+            else:
+                raise e
+
     if len(homology_dimensions) == 1:
-        # Reshape ensures copy
-        Xs = X[X[:, :, 2] == homology_dimensions[0]].reshape(n, -1, 3)
+        Xs = _subdiagrams_single_homology_dimension(homology_dimensions[0])
     else:
         # np.concatenate will also create a copy
-        Xs = np.concatenate([X[X[:, :, 2] == dim].reshape(n, -1, 3)
-                             for dim in homology_dimensions],
-                            axis=1)
+        Xs = np.concatenate(
+            [_subdiagrams_single_homology_dimension(dim)
+             for dim in homology_dimensions],
+            axis=1
+            )
     if remove_dim:
         Xs = Xs[:, :, :2]
     return Xs
