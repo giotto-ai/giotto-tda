ae3ff6b0233db43490a4dfb997fcc76138b15cc0
Umberto Lupo <umberto.lupo@gmail.com>
2020-08-17 09:26:46 +0200

    Separate out tests to address mystery in lack of coverage

--- gtda/mapper/tests/test_visualization_interactive.py
+++ gtda/mapper/tests/test_visualization_interactive.py
@@ -10,6 +10,17 @@ N = 50
 d = 3
 X = np.random.randn(N, d)
 
+params = {
+    "cover": {
+        "initial": {"n_intervals": 10, "kind": "uniform",
+                    "overlap_frac": 0.1},
+        "final": {"n_intervals": 15, "kind": "balanced",
+                  "overlap_frac": 0.2}
+        },
+    "clusterer": {"initial": {"affinity": "euclidean"},
+                  "final": {"affinity": "manhattan"}}
+    }
+
 
 def _get_widget_by_trait(fig, key, val=None):
     for k, v in fig.widgets.items():
@@ -22,21 +33,11 @@ def _get_widget_by_trait(fig, key, val=None):
             pass
 
 
-@pytest.mark.parametrize("clone_pipeline", [True, False])
-@pytest.mark.parametrize("layout_dim", [2, 3])
-def test_pipeline_cloned(clone_pipeline, layout_dim):
-    """Verify that the pipeline is not changed on interaction if and only if
-    `clone_pipeline` is True."""
-    params = {
-        "cover": {
-            "initial": {"n_intervals": 10, "kind": "uniform",
-                        "overlap_frac": 0.1},
-            "final": {"n_intervals": 15, "kind": "balanced",
-                      "overlap_frac": 0.2}
-            },
-        "clusterer": {"initial": {"affinity": "euclidean"},
-                      "final": {"affinity": "manhattan"}}
-        }
+def test_pipeline_cloned_2D():
+    """Verify that the pipeline is not changed on interaction if
+    `clone_pipeline` is True (with `layout_dim` set to 2)."""
+    clone_pipeline = True
+    layout_dim = 2
 
     pipe = make_mapper_pipeline(
         cover=CubicalCover(**params["cover"]["initial"]),
@@ -52,7 +53,85 @@ def test_pipeline_cloned(clone_pipeline, layout_dim):
             w_text = _get_widget_by_trait(fig, 'description', param_name)
             final_param_value = values["final"][param_name]
             w_text.set_state({'value': final_param_value})
-            final_param = \
+            final_pipe_param_value = \
                 pipe.get_mapper_params()[f"{step}__{param_name}"]
-            assert final_param == initial_param_value \
+            assert final_pipe_param_value == initial_param_value \
+                if clone_pipeline else final_param_value
+
+
+def test_pipeline_not_cloned_2D():
+    """Verify that the pipeline is changed on interaction if `clone_pipeline`
+    is False (with `layout_dim` set to 2)."""
+    clone_pipeline = False
+    layout_dim = 2
+
+    pipe = make_mapper_pipeline(
+        cover=CubicalCover(**params["cover"]["initial"]),
+        clusterer=FirstSimpleGap(**params["clusterer"]["initial"])
+        )
+    fig = plot_interactive_mapper_graph(
+        pipe, X, clone_pipeline=clone_pipeline, layout_dim=layout_dim
+        )
+
+    # Get relevant widgets and change their states, then check final values
+    for step, values in params.items():
+        for param_name, initial_param_value in values["initial"].items():
+            w_text = _get_widget_by_trait(fig, 'description', param_name)
+            final_param_value = values["final"][param_name]
+            w_text.set_state({'value': final_param_value})
+            final_pipe_param_value = \
+                pipe.get_mapper_params()[f"{step}__{param_name}"]
+            assert final_pipe_param_value == initial_param_value \
+                if clone_pipeline else final_param_value
+
+
+def test_pipeline_cloned_3D():
+    """Verify that the pipeline is not changed on interaction if
+    `clone_pipeline` is True (with `layout_dim` set to 3)."""
+    clone_pipeline = True
+    layout_dim = 3
+
+    pipe = make_mapper_pipeline(
+        cover=CubicalCover(**params["cover"]["initial"]),
+        clusterer=FirstSimpleGap(**params["clusterer"]["initial"])
+        )
+    fig = plot_interactive_mapper_graph(
+        pipe, X, clone_pipeline=clone_pipeline, layout_dim=layout_dim
+        )
+
+    # Get relevant widgets and change their states, then check final values
+    for step, values in params.items():
+        for param_name, initial_param_value in values["initial"].items():
+            w_text = _get_widget_by_trait(fig, 'description', param_name)
+            final_param_value = values["final"][param_name]
+            w_text.set_state({'value': final_param_value})
+            final_pipe_param_value = \
+                pipe.get_mapper_params()[f"{step}__{param_name}"]
+            assert final_pipe_param_value == initial_param_value \
+                if clone_pipeline else final_param_value
+
+
+def test_pipeline_not_cloned_3D():
+    """Verify that the pipeline is changed on interaction if `clone_pipeline`
+    is False (with `layout_dim` set to 3)."""
+    clone_pipeline = False
+    layout_dim = 3
+
+    pipe = make_mapper_pipeline(
+        cover=CubicalCover(**params["cover"]["initial"]),
+        clusterer=FirstSimpleGap(**params["clusterer"]["initial"])
+        )
+    fig = plot_interactive_mapper_graph(
+        pipe, X, clone_pipeline=clone_pipeline, layout_dim=layout_dim
+        )
+
+    # Get relevant widgets and change their states, then check final values
+    for step, values in params.items():
+        for param_name, initial_param_value in values["initial"].items():
+            w_text = _get_widget_by_trait(fig, 'description', param_name)
+            final_param_value = values["final"][param_name]
+            w_text.set_state({'value': final_param_value})
+            final_pipe_param_value = \
+                pipe.get_mapper_params()[f"{step}__{param_name}"]
+            assert final_pipe_param_value == initial_param_value \
                 if clone_pipeline else final_param_value
