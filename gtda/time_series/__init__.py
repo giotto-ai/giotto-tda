15ca0c5b694d832bf8c310cbcefcbc5cb25f950b
Umberto Lupo <umberto.lupo@gmail.com>
2020-08-18 02:27:28 +0200

    Add time_delay_embedding to time_series __init__

--- gtda/time_series/__init__.py
+++ gtda/time_series/__init__.py
@@ -1,9 +1,8 @@
-"""The module :mod:`gtda.time_series` implements transformers to
-preprocess time series or embed them in a higher dimensional space for
-persistent homology.
-"""
+"""The module :mod:`gtda.time_series` implements transformers to preprocess
+time series or embed them in a higher dimensional space for persistent
+homology."""
 
-from .embedding import SlidingWindow, TakensEmbedding
+from .embedding import SlidingWindow, TakensEmbedding, time_delay_embedding
 from .features import PermutationEntropy
 from .preprocessing import Resampler, Stationarizer
 from .multivariate import PearsonDissimilarity
@@ -14,6 +13,7 @@ __all__ = [
     'Stationarizer',
     'PermutationEntropy',
     'TakensEmbedding',
+    'time_delay_embedding',
     'SlidingWindow',
     'Labeller',
     'PearsonDissimilarity'
