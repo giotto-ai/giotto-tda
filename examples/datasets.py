41f87b7c43d2f58a5121aa08940b8fe785b8e19b
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2020-08-23 12:42:57 +0200

    Add gravitation wave detection and general cleanup

--- examples/datasets.py
+++ examples/datasets.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pathlib import Path
 
 def generate_point_clouds(n_samples, n_points, eps):
     sphere_point_clouds = [
@@ -39,3 +40,59 @@ def generate_point_clouds(n_samples, n_points, eps):
     labels = np.concatenate((sphere_labels, torus_labels))
 
     return point_clouds, labels
+
+def make_gravitational_waves(
+    path_to_data: Path,
+    n_signals: int = 30,
+    downsample_factor: int = 2,
+    r_min: float = 0.075,
+    r_max: float = 0.65,
+    n_snr_values: int = 10,
+):
+    def padrand(V, n, kr):
+        cut = np.random.randint(n)
+        rand1 = np.random.randn(cut)
+        rand2 = np.random.randn(n - cut)
+        out = np.concatenate((rand1 * kr, V, rand2 * kr))
+        return out
+
+    Rcoef = np.linspace(r_min, r_max, n_snr_values)
+    Npad = 500  # number of padding points on either side of the vector
+    gw = np.load(path_to_data / "gravitational-wave-signals.npy")
+    Norig = len(gw["data"][0])
+    Ndat = len(gw["signal_present"])
+    ri = []
+    k = 0
+    N = int(Norig / downsample_factor)
+
+    ncoeff = []
+    Rcoeflist = []
+
+    for j in range(n_signals):
+        ncoeff.append(10 ** (-19) * (1 / Rcoef[j % n_snr_values]))
+        Rcoeflist.append(Rcoef[j % n_snr_values])
+
+    x = []
+    noisy_signals = []
+    gw_signals = []
+    y = []
+    pcloud = []
+    maxs = 0.0
+    k = 0
+    labels = np.zeros(n_signals)
+
+    for j in range(n_signals):
+        signal = gw["data"][j % Ndat][range(0, Norig, downsample_factor)]
+        sigp = int((np.random.randn() < 0))
+        noise = ncoeff[j] * np.random.randn(N)
+        labels[j] = sigp
+        if sigp == 1:
+            rawsig = padrand(signal + noise, Npad, ncoeff[j])
+            if k == 0:
+                k = 1
+        else:
+            rawsig = padrand(noise, Npad, ncoeff[j])
+        noisy_signals.append(rawsig.copy())
+        gw_signals.append(signal)
+
+    return noisy_signals, gw_signals, labels
