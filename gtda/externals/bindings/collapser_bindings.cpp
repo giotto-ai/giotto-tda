085d9b95723aa3cdb486178aa6afe0c2440898f4
Umberto Lupo <umberto.lupo@gmail.com>
2020-08-30 21:51:14 +0200

    Make more collapser bindings which compile

--- gtda/externals/bindings/collapser_bindings.cpp
+++ gtda/externals/bindings/collapser_bindings.cpp
@@ -22,10 +22,16 @@ using Filtered_edge_list = std::vector<Filtered_edge>;
 using Sparse_matrix = Eigen::SparseMatrix<Filtration_value>;
 using triplet_vec = Eigen::Triplet<Filtration_value>;
 
+/* COO data input types */
+using Row_idx = std::vector<uint32_t>;
+using Col_idx = std::vector<uint32_t>;
+using Filtration_values = std::vector<float>;
+using COO_data = std::tuple<Row_idx, Col_idx, Filtration_values>;
+
 /* Dense matrix input types */
 using Distance_matrix = std::vector<std::vector<Filtration_value>>;
 
-/* Generates a sparse matrix from a filtered edges list
+/* Generates a sparse matrix from a filtered edge list
  * This function is called after computing edge collapse
  */
 static Sparse_matrix generate_sparse_matrix(
@@ -43,10 +49,27 @@ static Sparse_matrix generate_sparse_matrix(
   return mat;
 }
 
+/* Generates COO sparse matrix data from a filtered edge list
+ * This function is called after computing edge collapse
+ */
+static COO_data coo_data_from_filtered_edge_list(
+    Filtered_edge_list& collapsed_edges) {
+  Row_idx row;
+  Col_idx col;
+  Filtration_values data;
+  for (auto& t : collapsed_edges) {
+    row.push_back(std::get<0>(t));
+    col.push_back(std::get<1>(t));
+    data.push_back(std::get<2>(t));
+  }
+
+  return COO_data(row, col, data);
+}
+
 PYBIND11_MODULE(gtda_collapser, m) {
   using namespace pybind11::literals;
 
-  m.doc() = "Collapser bindings for Gudhi  implementation";
+  m.doc() = "Collapser bindings for GUDHI implementation";
   m.def("flag_complex_collapse_edges_sparse",
         [](Sparse_matrix& graph_) {
           Filtered_edge_list graph;
@@ -69,10 +92,26 @@ PYBIND11_MODULE(gtda_collapser, m) {
         "Implicitly constructs a flag complex from edges, "
         "collapses edges while preserving the persistent homology");
 
+  m.def("flag_complex_collapse_edges_from_coo_data_to_coo_data",
+        [](Row_idx& row, Col_idx& col, Filtration_values& data) {
+          Filtered_edge_list graph;
+
+          /* Convert from COO input format to Filtered_edge_list */
+          int size = data.size();
+          for (size_t k = 0; k < size; ++k) graph.push_back(Filtered_edge(row[k], col[k], data[k]));
+
+          /* Start collapser */
+          auto vec_triples =
+              Gudhi::collapse::flag_complex_collapse_edges(graph);
+          return coo_data_from_filtered_edge_list(vec_triples);
+        },
+        "row"_a, "column"_a, "data"_a,
+        "Implicitly constructs a flag complex from edges, "
+        "collapses edges while preserving the persistent homology");
+
   m.def("flag_complex_collapse_edges_dense",
         [](Distance_matrix& dm) {
           Filtered_edge_list graph;
-          std::vector<triplet_vec> triplets;
 
           /* Convert from dense format to Filtered edge list */
           for (size_t i = 0; i < dm.size(); i++)
@@ -88,4 +127,24 @@ PYBIND11_MODULE(gtda_collapser, m) {
         "dense_matrix"_a,
         "Implicitly constructs a flag complex from edges, "
         "collapses edges while preserving the persistent homology");
+
+  m.def("flag_complex_collapse_edges_from_dense_to_coo_data",
+        [](Distance_matrix& dm) {
+          Filtered_edge_list graph;
+
+          /* Convert from dense format to Filtered edge list */
+          for (size_t i = 0; i < dm.size(); i++)
+            for (size_t j = 0; j < dm[i].size(); j++)
+              if (j > i) graph.push_back(Filtered_edge(i, j, dm[i][j]));
+
+          /* Start collapser */
+          auto vec_triples =
+              Gudhi::collapse::flag_complex_collapse_edges(graph);
+
+          return coo_data_from_filtered_edge_list(vec_triples);
+        },
+        "dense_matrix"_a,
+        "Implicitly constructs a flag complex from edges, "
+        "collapses edges while preserving the persistent homology");
+
 }
