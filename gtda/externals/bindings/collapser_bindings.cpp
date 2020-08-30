88bfbfc11a007d335147a8aeae8d578d448f7564
Umberto Lupo <umberto.lupo@gmail.com>
2020-08-30 11:22:48 +0200

    Split flag_complex_collapse_edges into a _sparse and a _dense version

--- gtda/externals/bindings/collapser_bindings.cpp
+++ gtda/externals/bindings/collapser_bindings.cpp
@@ -11,22 +11,22 @@
 
 namespace py = pybind11;
 
-/* GUDHI Collapser require types */
+/* GUDHI Collapser required types */
 using Filtration_value = float;
 using Vertex_handle = uint32_t;
 using Filtered_edge =
     std::tuple<Vertex_handle, Vertex_handle, Filtration_value>;
 using Filtered_edge_list = std::vector<Filtered_edge>;
 
-/* sparse matrix input types */
+/* Sparse matrix input types */
 using Sparse_matrix = Eigen::SparseMatrix<Filtration_value>;
 using triplet_vec = Eigen::Triplet<Filtration_value>;
 
-/* dense matrix input types */
+/* Dense matrix input types */
 using Distance_matrix = std::vector<std::vector<Filtration_value>>;
 
-/* generates a sparse matrix from a filtered edges list
- * This function is called after computing edges collapsing
+/* Generates a sparse matrix from a filtered edges list
+ * This function is called after computing edge collapse
  */
 static Sparse_matrix generate_sparse_matrix(
     Filtered_edge_list& collapsed_edges, int size) {
@@ -47,11 +47,11 @@ PYBIND11_MODULE(gtda_collapser, m) {
   using namespace pybind11::literals;
 
   m.doc() = "Collapser bindings for Gudhi  implementation";
-  m.def("flag_complex_collapse_edges",
+  m.def("flag_complex_collapse_edges_sparse",
         [](Sparse_matrix& graph_) {
           Filtered_edge_list graph;
 
-          /* Convert from Sparse format to Filtered edge list */
+          /* Convert from sparse format to Filtered_edge_list */
           int size = graph_.outerSize();
           for (size_t k = 0; k < size; ++k)
             for (Eigen::SparseMatrix<Filtration_value>::InnerIterator it(graph_,
@@ -69,12 +69,12 @@ PYBIND11_MODULE(gtda_collapser, m) {
         "Implicitly constructs a flag complex from edges, "
         "collapses edges while preserving the persistent homology");
 
-  m.def("flag_complex_collapse_edges",
+  m.def("flag_complex_collapse_edges_dense",
         [](Distance_matrix& dm) {
           Filtered_edge_list graph;
           std::vector<triplet_vec> triplets;
 
-          /* Convert from Sparse format to Filtered edge list */
+          /* Convert from dense format to Filtered edge list */
           for (size_t i = 0; i < dm.size(); i++)
             for (size_t j = 0; j < dm[i].size(); j++)
               if (j > i) graph.push_back(Filtered_edge(i, j, dm[i][j]));
