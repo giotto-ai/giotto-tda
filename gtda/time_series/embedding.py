59155f6bafcb492ca9598744b57ccc987f4bc862
Umberto Lupo <umberto.lupo@gmail.com>
2020-09-01 17:15:42 +0200

    Support list of time series input in time_delay_embedding

--- gtda/time_series/embedding.py
+++ gtda/time_series/embedding.py
@@ -1,6 +1,8 @@
 """Time series embedding."""
 # License: GNU AGPLv3
 
+from functools import partial
+
 import numpy as np
 from joblib import Parallel, delayed
 from sklearn.base import BaseEstimator
@@ -295,27 +297,36 @@ def time_delay_embedding(X, time_delay=1, dimension=2, stride=1, flatten=False,
                         'ensure_last_value': ensure_last_value}
         validate_params(input_params, expected_params)
 
-    n_timestamps = X.shape[-1]
-    n_points, offset = \
-        divmod(n_timestamps - time_delay * (dimension - 1) - 1, stride)
-    n_points += 1
-    if n_points <= 0:
-        raise ValueError(
-            f"Not enough time stamps ({n_timestamps}) to produce at least "
-            f"one {dimension}-dimensional vector under the current choice "
-            f"of time delay ({time_delay})."
-            )
-    indices = np.tile(np.arange(0, time_delay * dimension, time_delay),
-                      (n_points, 1))
-    indices += np.arange(n_points)[:, None] * stride
-    if ensure_last_value:
-        indices += offset
+    if hasattr(X, 'shape'):  # ndarray input
+        n_timestamps = X.shape[-1]
+        n_points, offset = \
+            divmod(n_timestamps - time_delay * (dimension - 1) - 1, stride)
+        n_points += 1
+        if n_points <= 0:
+            raise ValueError(
+                f"Not enough time stamps ({n_timestamps}) to produce at least "
+                f"one {dimension}-dimensional vector under the current choice "
+                f"of time delay ({time_delay})."
+                )
+        indices = np.tile(np.arange(0, time_delay * dimension, time_delay),
+                          (n_points, 1))
+        indices += np.arange(n_points)[:, None] * stride
+        if ensure_last_value:
+            indices += offset
 
-    X_embedded = X[..., indices]
-    if flatten:
-        transpose_axes = (0, *range(1, X.ndim)[::-1], X.ndim)
-        X_embedded = np.transpose(X_embedded, axes=transpose_axes).\
-            reshape(len(X), -1, dimension * np.prod(X.shape[1:-1]))
+        X_embedded = X[..., indices]
+        if flatten:
+            transpose_axes = (0, *range(1, X.ndim)[::-1], X.ndim)
+            X_embedded = np.transpose(X_embedded, axes=transpose_axes).\
+                reshape(len(X), -1, dimension * np.prod(X.shape[1:-1]))
+    else:  # list of ndarray input
+        func = partial(time_delay_embedding, time_delay=time_delay,
+                       dimension=dimension, stride=stride, flatten=flatten,
+                       ensure_last_value=ensure_last_value, validate=False)
+        X_embedded = []
+        for x in X:
+            x_embedded = func(x.reshape(1, *x.shape))[0]
+            X_embedded.append(x_embedded)
 
     return X_embedded
 
