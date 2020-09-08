584a83ad1208888994fc94fcd56bc8b300ebf7e4
Umberto Lupo <umberto.lupo@gmail.com>
2020-09-08 23:21:00 +0200

    Fix tests for KNeighborsGraph following change to non-symmetric output

--- gtda/graphs/tests/test_kneighbors.py
+++ gtda/graphs/tests/test_kneighbors.py
@@ -7,17 +7,19 @@ from sklearn.exceptions import NotFittedError
 
 from gtda.graphs import KNeighborsGraph
 
-X_kng = np.array([[[0, 0], [1, 2], [4, 3], [6, 2]]])
+X_kng = np.array([[[0, 0],
+                   [1, 2],
+                   [4, 3],
+                   [6, 2]]])
 
-X_kng_res = np.array([
-    csr_matrix((np.array([1] * 4),
-                (np.array([0, 1, 2, 3]),
-                 np.array([1, 0, 3, 2]))), shape=(4, 4))])
+X_kng_res = [csr_matrix((np.array([1] * 4),
+                         (np.array([0, 1, 2, 3]), np.array([1, 0, 3, 2]))),
+                        shape=(4, 4))]
 
-X_kng_res_k2 = np.array([csr_matrix(np.array([[0., 1., 1., 0.],
-                                              [1., 0., 1., 1.],
-                                              [1., 1., 0., 1.],
-                                              [0., 1., 1., 0.]]))])
+X_kng_res_k2 = [csr_matrix(np.array([[0, 1, 1, 0],
+                                     [1, 0, 1, 0],
+                                     [0, 1, 0, 1],
+                                     [0, 1, 1, 0]]))]
 
 
 def test_kng_not_fitted():
@@ -31,7 +33,8 @@ def test_kng_not_fitted():
                          [(1, X_kng_res), (2, X_kng_res_k2)])
 def test_kng_transform(n_neighbors, expected):
     kn_graph = KNeighborsGraph(n_neighbors=n_neighbors)
-
+    print(kn_graph.fit_transform(X_kng)[0].toarray())
+    print(expected[0])
     assert (kn_graph.fit_transform(X_kng)[0] != expected[0]).nnz == 0
 
 
@@ -41,11 +44,3 @@ def test_parallel_kng_transform():
 
     assert (kn_graph.fit_transform(X_kng)[0] !=
             kn_graph_parallel.fit_transform(X_kng)[0]).nnz == 0
-
-
-def test_symmetric():
-    kn_graph = KNeighborsGraph(n_neighbors=2)
-    X_kng_transformed = kn_graph.fit_transform(X_kng)
-
-    assert (X_kng_transformed[0] !=
-            X_kng_transformed[0].transpose()).nnz == 0
