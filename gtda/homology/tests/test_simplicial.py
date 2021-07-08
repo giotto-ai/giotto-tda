b8b22e6433e6d25f9b8d63d721f6012453c122cf
Umberto Lupo <46537483+ulupo@users.noreply.github.com>
2021-07-08 15:43:16 +0200

    Fix linting

--- gtda/homology/tests/test_simplicial.py
+++ gtda/homology/tests/test_simplicial.py
@@ -134,7 +134,7 @@ def test_wrp_params():
 def test_wrp_metric_params():
     def metric(x, y, parameter):
         return np.linalg.norm(x-y)
-    
+
     metric_params = {"parameter": 0.}
     wrp = WeightedRipsPersistence(metric=metric, metric_params=metric_params)
     wrp.fit_transform(X_pc)
