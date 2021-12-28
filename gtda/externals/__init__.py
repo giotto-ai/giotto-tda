f5495b483ae9d2fd4153968d3ce2607aa913706c
Umberto Lupo <umberto.lupo@gmail.com>
2021-12-28 14:43:53 +0100

    Replace ripser.py and robinhood submodules with giotto-ph dependency

--- gtda/externals/__init__.py
+++ gtda/externals/__init__.py
@@ -5,13 +5,12 @@ from .modules.gtda_bottleneck import bottleneck_distance
 from .modules.gtda_wasserstein import wasserstein_distance
 from .modules.gtda_collapser import flag_complex_collapse_edges_dense, \
     flag_complex_collapse_edges_sparse, flag_complex_collapse_edges_coo
-from .python import ripser, SparseRipsComplex, CechComplex, CubicalComplex, \
+from .python import SparseRipsComplex, CechComplex, CubicalComplex, \
     PeriodicCubicalComplex, SimplexTree, WitnessComplex, StrongWitnessComplex
 
 __all__ = [
     'bottleneck_distance',
     'wasserstein_distance',
-    'ripser',
     'SparseRipsComplex',
     'CechComplex',
     'CubicalComplex',
